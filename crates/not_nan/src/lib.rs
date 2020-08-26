b4a8e91d5e048a1ff3642de76635dce034b3c872
ngtkana <ngtkana@gmail.com>
2020-08-27 02:01:34 +0900

    Add not_nan

---
+++ crates/not_nan/src/lib.rs
@@ -0,0 +1,40 @@
+//! ordered_float::NotNan の劣化版です。
+//!
+use std::cmp::{Ord, Ordering};
+
+pub trait Float: PartialEq + PartialOrd {
+    fn is_nan(&self) -> bool;
+}
+
+impl Float for f64 {
+    fn is_nan(&self) -> bool {
+        *self == std::f64::NAN
+    }
+}
+
+#[derive(PartialOrd, PartialEq, Debug, Default, Clone, Copy)]
+pub struct NotNaN<T: Float>(T);
+
+impl<T: Float> NotNaN<T> {
+    pub fn from_float(value: T) -> Self {
+        match value {
+            ref value if value.is_nan() => panic!("これはなんですか？"),
+            value => NotNaN(value),
+        }
+    }
+
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl<T: Float> Eq for NotNaN<T> {}
+
+impl<T: Float> Ord for NotNaN<T> {
+    fn cmp(&self, other: &NotNaN<T>) -> Ordering {
+        match self.partial_cmp(&other) {
+            Some(ord) => ord,
+            None => unreachable!(),
+        }
+    }
+}
