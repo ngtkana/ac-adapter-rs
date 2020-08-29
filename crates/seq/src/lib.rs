d15fd5f6a08ae6cd87825467021241ad2ba278d0
ngtkana <ngtkana@gmail.com>
2020-08-29 13:11:43 +0900

    Add aoj_copied, cartesian_product

--- crates/seq/src/lib.rs
+++ crates/seq/src/lib.rs
@@ -7,6 +7,8 @@
 //!
 
 pub use self::adjacent::{adjacent, Adjacent};
+pub use self::aoj_copied::AojCopied;
+pub use self::cartesian_product::{cartesian_product, CartesianProduct};
 pub use self::grid_next::{grid_next, GridNext};
 pub use self::mul_step::{mul_step, MulStep};
 pub use self::repeat_with::{repeat_with, RepeatWith};
@@ -71,6 +73,101 @@ pub trait Seq: Iterator + Sized {
         Self: Iterator<Item = (i64, i64)>,
     {
         grid_next(self, ij, h, w)
+    }
+
+    /// `std::iter::copied` とほぼ同じ機能です。
+    fn aoj_copied<'a, T: 'a>(self) -> AojCopied<Self>
+    where
+        Self: Sized + Iterator<Item = &'a T>,
+        T: Copy,
+    {
+        AojCopied::new(self)
+    }
+
+    /// `itertools::Itertools::cartesian_product` とほぼ同じ機能です。
+    fn cartesian_product<J>(self, other: J) -> CartesianProduct<Self, J::IntoIter>
+    where
+        Self: Sized,
+        Self::Item: Clone,
+        J: IntoIterator,
+        J::IntoIter: Clone,
+    {
+        cartesian_product::cartesian_product(self, other.into_iter())
+    }
+}
+
+mod aoj_copied {
+    use std::iter::DoubleEndedIterator;
+
+    #[allow(missing_docs)]
+    #[derive(Debug, Clone)]
+    pub struct AojCopied<I> {
+        it: I,
+    }
+
+    impl<I> AojCopied<I> {
+        pub(super) fn new(it: I) -> AojCopied<I> {
+            AojCopied { it }
+        }
+    }
+
+    fn copy_fold<T: Copy, Acc>(mut f: impl FnMut(Acc, T) -> Acc) -> impl FnMut(Acc, &T) -> Acc {
+        move |acc, &elt| f(acc, elt)
+    }
+
+    impl<'a, I, T: 'a> Iterator for AojCopied<I>
+    where
+        I: Iterator<Item = &'a T>,
+        T: Copy,
+    {
+        type Item = T;
+
+        fn next(&mut self) -> Option<T> {
+            self.it.next().copied()
+        }
+
+        fn size_hint(&self) -> (usize, Option<usize>) {
+            self.it.size_hint()
+        }
+
+        fn fold<Acc, F>(self, init: Acc, f: F) -> Acc
+        where
+            F: FnMut(Acc, Self::Item) -> Acc,
+        {
+            self.it.fold(init, copy_fold(f))
+        }
+
+        fn nth(&mut self, n: usize) -> Option<T> {
+            self.it.nth(n).copied()
+        }
+
+        fn last(self) -> Option<T> {
+            self.it.last().copied()
+        }
+
+        fn count(self) -> usize {
+            self.it.count()
+        }
+    }
+
+    impl<'a, I, T: 'a> DoubleEndedIterator for AojCopied<I>
+    where
+        I: DoubleEndedIterator<Item = &'a T>,
+        T: Copy,
+    {
+        fn next_back(&mut self) -> Option<T> {
+            self.it.next_back().copied()
+        }
+    }
+
+    impl<'a, I, T: 'a> ExactSizeIterator for AojCopied<I>
+    where
+        I: ExactSizeIterator<Item = &'a T>,
+        T: Copy,
+    {
+        fn len(&self) -> usize {
+            self.it.len()
+        }
     }
 }
 
@@ -254,5 +351,207 @@ mod repeat_with {
         fn size_hint(&self) -> (usize, Option<usize>) {
             (::std::usize::MAX, None)
         }
+    }
+}
+
+mod cartesian_product {
+    #[allow(missing_docs)]
+    #[derive(Debug, Clone)]
+    pub struct CartesianProduct<I, J>
+    where
+        I: Iterator,
+    {
+        a: I,
+        a_cur: Option<I::Item>,
+        b: J,
+        b_orig: J,
+    }
+
+    #[allow(missing_docs)]
+    pub fn cartesian_product<I, J>(mut i: I, j: J) -> CartesianProduct<I, J>
+    where
+        I: Iterator,
+        J: Clone + Iterator,
+        I::Item: Clone,
+    {
+        CartesianProduct {
+            a_cur: i.next(),
+            a: i,
+            b_orig: j.clone(),
+            b: j,
+        }
+    }
+
+    impl<I, J> Iterator for CartesianProduct<I, J>
+    where
+        I: Iterator,
+        J: Clone + Iterator,
+        I::Item: Clone,
+    {
+        type Item = (I::Item, J::Item);
+
+        fn next(&mut self) -> Option<(I::Item, J::Item)> {
+            let elt_b = match self.b.next() {
+                None => {
+                    self.b = self.b_orig.clone();
+                    match self.b.next() {
+                        None => return None,
+                        Some(x) => {
+                            self.a_cur = self.a.next();
+                            x
+                        }
+                    }
+                }
+                Some(x) => x,
+            };
+            match self.a_cur {
+                None => None,
+                Some(ref a) => Some((a.clone(), elt_b)),
+            }
+        }
+
+        fn size_hint(&self) -> (usize, Option<usize>) {
+            let has_cur = self.a_cur.is_some() as usize;
+            // Not ExactSizeIterator because size may be larger than usize
+            let (b_min, b_max) = self.b.size_hint();
+
+            // Compute a * b_orig + b for both lower and upper bound
+            super::size_hint::add(
+                super::size_hint::mul(self.a.size_hint(), self.b_orig.size_hint()),
+                (b_min * has_cur, b_max.map(move |x| x * has_cur)),
+            )
+        }
+
+        fn fold<Acc, G>(mut self, mut accum: Acc, mut f: G) -> Acc
+        where
+            G: FnMut(Acc, Self::Item) -> Acc,
+        {
+            if let Some(mut a) = self.a_cur.take() {
+                let mut b = self.b;
+                loop {
+                    accum = b.fold(accum, |acc, elt| f(acc, (a.clone(), elt)));
+
+                    // we can only continue iterating a if we had a first element;
+                    if let Some(next_a) = self.a.next() {
+                        b = self.b_orig.clone();
+                        a = next_a;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            accum
+        }
+    }
+}
+
+mod size_hint {
+    //! Arithmetic on **Iterator** *.size_hint()* values.
+    //!
+
+    use std::cmp;
+    use std::usize;
+
+    /// **SizeHint** is the return type of **Iterator::size_hint()**.
+    pub type SizeHint = (usize, Option<usize>);
+
+    /// Add **SizeHint** correctly.
+    #[inline]
+    pub fn add(a: SizeHint, b: SizeHint) -> SizeHint {
+        let min = a.0.saturating_add(b.0);
+        let max = match (a.1, b.1) {
+            (Some(x), Some(y)) => x.checked_add(y),
+            _ => None,
+        };
+
+        (min, max)
+    }
+
+    /// Add **x** correctly to a **SizeHint**.
+    #[inline]
+    #[allow(dead_code)]
+    pub fn add_scalar(sh: SizeHint, x: usize) -> SizeHint {
+        let (mut low, mut hi) = sh;
+        low = low.saturating_add(x);
+        hi = hi.and_then(|elt| elt.checked_add(x));
+        (low, hi)
+    }
+
+    /// Sub **x** correctly to a **SizeHint**.
+    #[inline]
+    #[allow(dead_code)]
+    pub fn sub_scalar(sh: SizeHint, x: usize) -> SizeHint {
+        let (mut low, mut hi) = sh;
+        low = low.saturating_sub(x);
+        hi = hi.map(|elt| elt.saturating_sub(x));
+        (low, hi)
+    }
+
+    /// Multiply **SizeHint** correctly
+    ///
+    /// ```ignore
+    /// use std::usize;
+    /// use itertools::size_hint;
+    ///
+    /// assert_eq!(size_hint::mul((3, Some(4)), (3, Some(4))),
+    ///            (9, Some(16)));
+    ///
+    /// assert_eq!(size_hint::mul((3, Some(4)), (usize::MAX, None)),
+    ///            (usize::MAX, None));
+    ///
+    /// assert_eq!(size_hint::mul((3, None), (0, Some(0))),
+    ///            (0, Some(0)));
+    /// ```
+    #[inline]
+    #[allow(dead_code)]
+    pub fn mul(a: SizeHint, b: SizeHint) -> SizeHint {
+        let low = a.0.saturating_mul(b.0);
+        let hi = match (a.1, b.1) {
+            (Some(x), Some(y)) => x.checked_mul(y),
+            (Some(0), None) | (None, Some(0)) => Some(0),
+            _ => None,
+        };
+        (low, hi)
+    }
+
+    /// Multiply **x** correctly with a **SizeHint**.
+    #[inline]
+    #[allow(dead_code)]
+    pub fn mul_scalar(sh: SizeHint, x: usize) -> SizeHint {
+        let (mut low, mut hi) = sh;
+        low = low.saturating_mul(x);
+        hi = hi.and_then(|elt| elt.checked_mul(x));
+        (low, hi)
+    }
+
+    /// Return the maximum
+    #[inline]
+    #[allow(dead_code)]
+    pub fn max(a: SizeHint, b: SizeHint) -> SizeHint {
+        let (a_lower, a_upper) = a;
+        let (b_lower, b_upper) = b;
+
+        let lower = cmp::max(a_lower, b_lower);
+
+        let upper = match (a_upper, b_upper) {
+            (Some(x), Some(y)) => Some(cmp::max(x, y)),
+            _ => None,
+        };
+
+        (lower, upper)
+    }
+
+    /// Return the minimum
+    #[inline]
+    #[allow(dead_code)]
+    pub fn min(a: SizeHint, b: SizeHint) -> SizeHint {
+        let (a_lower, a_upper) = a;
+        let (b_lower, b_upper) = b;
+        let lower = cmp::min(a_lower, b_lower);
+        let upper = match (a_upper, b_upper) {
+            (Some(u1), Some(u2)) => Some(cmp::min(u1, u2)),
+            _ => a_upper.or(b_upper),
+        };
+        (lower, upper)
     }
 }
