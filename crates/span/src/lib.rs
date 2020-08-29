6bd36963ca2d89a35b0b11e73350753493f538da
ngtkana <ngtkana@gmail.com>
2020-08-29 14:24:20 +0900

    Rename and update slicetools->span

---
+++ crates/span/src/lib.rs
@@ -0,0 +1,226 @@
+#![warn(missing_docs)]
+#![warn(missing_doc_code_examples)]
+
+//! slice の機能を増やします。
+//! 詳しくは [`Span`] をご覧ください。
+//!
+//! [`Span`]: trait.Span.html
+
+use std::{cmp, ops};
+
+impl<T> Span<T> for [T] {
+    #[inline]
+    fn len(&self) -> usize {
+        self.len()
+    }
+
+    #[inline]
+    fn is_empty(&self) -> bool {
+        self.is_empty()
+    }
+}
+
+/// slice の機能を増やします。
+pub trait Span<T>: ops::Index<usize> {
+    /// 長さです。
+    fn len(&self) -> usize;
+
+    /// 空であるです。
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// 二分探索をします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use span::Span;
+    ///
+    /// let s = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    ///
+    /// let seek = 13;
+    /// assert_eq!(s.lower_bound(&seek), 9);
+    /// let seek = 4;
+    /// assert_eq!(s.lower_bound(&seek), 7);
+    /// let seek = 100;
+    /// assert_eq!(s.lower_bound(&seek), 13);
+    /// let seek = 1;
+    /// assert_eq!(s.lower_bound(&seek), 1);
+    /// ```
+    #[inline]
+    fn lower_bound<'a>(&'a self, x: &Self::Output) -> usize
+    where
+        Self::Output: Ord,
+    {
+        self.lower_bound_by(|p| p.cmp(x))
+    }
+
+    /// キー抽出関数を用いて二分探索をします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use span::Span;
+    /// use std::clone::Clone;
+    ///
+    /// let s = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    ///
+    /// let seek = 13;
+    /// assert_eq!(s.lower_bound_by_key(&seek, Clone::clone), 9);
+    /// let seek = 4;
+    /// assert_eq!(s.lower_bound_by_key(&seek, Clone::clone), 7);
+    /// let seek = 100;
+    /// assert_eq!(s.lower_bound_by_key(&seek, Clone::clone), 13);
+    /// let seek = 1;
+    /// assert_eq!(s.lower_bound_by_key(&seek, Clone::clone), 1);
+    /// ```
+    #[inline]
+    fn lower_bound_by_key<'a, B, F>(&'a self, b: &B, mut f: F) -> usize
+    where
+        F: FnMut(&'a Self::Output) -> B,
+        B: Ord,
+    {
+        self.lower_bound_by(|x| f(x).cmp(b))
+    }
+
+    /// 比較関数を用いて二分探索をします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use span::Span;
+    ///
+    /// let s = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    ///
+    /// let seek = 13;
+    /// assert_eq!(s.lower_bound_by(|probe| probe.cmp(&seek)), 9);
+    /// let seek = 4;
+    /// assert_eq!(s.lower_bound_by(|probe| probe.cmp(&seek)), 7);
+    /// let seek = 100;
+    /// assert_eq!(s.lower_bound_by(|probe| probe.cmp(&seek)), 13);
+    /// let seek = 1;
+    /// assert_eq!(s.lower_bound_by(|probe| probe.cmp(&seek)), 1);
+    /// ```
+    #[inline]
+    fn lower_bound_by<'a, F>(&'a self, mut f: F) -> usize
+    where
+        F: FnMut(&'a Self::Output) -> cmp::Ordering,
+    {
+        self.span_partition_point(|x| f(x) == cmp::Ordering::Less)
+    }
+
+    /// 二分探索をします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use span::Span;
+    ///
+    /// let s = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    ///
+    /// let seek = 13;
+    /// assert_eq!(s.upper_bound(&seek), 10);
+    /// let seek = 4;
+    /// assert_eq!(s.upper_bound(&seek), 7);
+    /// let seek = 100;
+    /// assert_eq!(s.upper_bound(&seek), 13);
+    /// let seek = 1;
+    /// assert_eq!(s.upper_bound(&seek), 5);
+    /// ```
+    #[inline]
+    fn upper_bound<'a>(&'a self, x: &Self::Output) -> usize
+    where
+        Self::Output: Ord,
+    {
+        self.upper_bound_by(|p| p.cmp(x))
+    }
+
+    /// キー抽出関数を用いて二分探索をします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use span::Span;
+    /// use std::clone::Clone;
+    ///
+    /// let s = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    ///
+    /// let seek = 13;
+    /// assert_eq!(s.upper_bound_by_key(&seek, Clone::clone), 10);
+    /// let seek = 4;
+    /// assert_eq!(s.upper_bound_by_key(&seek, Clone::clone), 7);
+    /// let seek = 100;
+    /// assert_eq!(s.upper_bound_by_key(&seek, Clone::clone), 13);
+    /// let seek = 1;
+    /// assert_eq!(s.upper_bound_by_key(&seek, Clone::clone), 5);
+    /// ```
+    #[inline]
+    fn upper_bound_by_key<'a, B, F>(&'a self, b: &B, mut f: F) -> usize
+    where
+        F: FnMut(&'a Self::Output) -> B,
+        B: Ord,
+    {
+        self.upper_bound_by(|x| f(x).cmp(b))
+    }
+
+    /// 比較関数を用いて二分探索をします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use span::Span;
+    ///
+    /// let s = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    ///
+    /// let seek = 13;
+    /// assert_eq!(s.upper_bound_by(|probe| probe.cmp(&seek)), 10);
+    /// let seek = 4;
+    /// assert_eq!(s.upper_bound_by(|probe| probe.cmp(&seek)), 7);
+    /// let seek = 100;
+    /// assert_eq!(s.upper_bound_by(|probe| probe.cmp(&seek)), 13);
+    /// let seek = 1;
+    /// assert_eq!(s.upper_bound_by(|probe| probe.cmp(&seek)), 5);
+    /// ```
+    #[inline]
+    fn upper_bound_by<'a, F>(&'a self, mut f: F) -> usize
+    where
+        F: FnMut(&'a Self::Output) -> cmp::Ordering,
+    {
+        self.span_partition_point(|x| f(x) != cmp::Ordering::Greater)
+    }
+
+    /// [`slice::partition_point`] と同じです。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use span::Span;
+    ///
+    /// let v = [1, 2, 3, 3, 5, 6, 7];
+    /// let i = v.span_partition_point(|&x| x < 5);
+    ///
+    /// assert_eq!(i, 4);
+    /// assert!(v[..i].iter().all(|&x| x < 5));
+    /// assert!(v[i..].iter().all(|&x| !(x < 5)));
+    /// ```
+    /// [`slice::partition_point`]: https://doc.rust-lang.org/std/primitive.slice.html#method.partition_point
+    fn span_partition_point<'a, F>(&'a self, mut pred: F) -> usize
+    where
+        F: FnMut(&'a Self::Output) -> bool,
+    {
+        let mut left = 0;
+        let mut right = self.len();
+        while left != right {
+            let mid = left + (right - left) / 2;
+            let value = &self[mid];
+            if pred(value) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        left
+    }
+}
