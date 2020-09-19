8531bbcbc8f1bb88061ad84dce1707eaa9ce623f
ngtkana <ngtkana@gmail.com>
2020-09-19 20:33:26 +0900

    Add wrappers Affine and Cat

--- crates/utils/type_traits/src/wrappers.rs
+++ crates/utils/type_traits/src/wrappers.rs
@@ -11,7 +11,6 @@ impl<T> Assoc for Add<T>
 where
     T: ops::Add<Output = T> + Element,
 {
-    #[inline]
     fn op(self, rhs: Self) -> Self {
         Add(self.0 + rhs.0)
     }
@@ -20,7 +19,6 @@ impl<T> Identity for Add<T>
 where
     T: Zero,
 {
-    #[inline]
     fn identity() -> Self {
         Add(T::zero())
     }
@@ -37,7 +35,6 @@ impl<T> Assoc for Mul<T>
 where
     T: ops::Mul<Output = T> + Element,
 {
-    #[inline]
     fn op(self, rhs: Self) -> Self {
         Mul(self.0 * rhs.0)
     }
@@ -46,8 +43,63 @@ impl<T> Identity for Mul<T>
 where
     T: One,
 {
-    #[inline]
     fn identity() -> Self {
         Mul(T::one())
+    }
+}
+
+/// [`ops::Add`], [`ops::Mul`] によるアフィン変換の合成で [`Assoc`], [`Ideneity`]
+/// を実装するラッパーです。
+///
+/// [`ops::Add`]: http://doc.rust-lang.org/std/ops/trait.Add.html
+/// [`ops::Mul`]: http://doc.rust-lang.org/std/ops/trait.Mul.html
+/// [`Assoc`]: traits.Assoc.html
+/// [`Identity`]: traits.Identity.html
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Affine<T> {
+    pub a: T,
+    pub b: T,
+}
+
+impl<T> Assoc for Affine<T>
+where
+    T: ops::Add<Output = T> + Element,
+    T: ops::Mul<Output = T> + Element,
+{
+    fn op(self, rhs: Self) -> Self {
+        Self {
+            a: self.a.clone() * rhs.a,
+            b: self.b + self.a * rhs.b,
+        }
+    }
+}
+impl<T> Identity for Affine<T>
+where
+    T: Zero + One,
+{
+    fn identity() -> Self {
+        Self {
+            a: T::one(),
+            b: T::zero(),
+        }
+    }
+}
+
+/// 文字列結合を演算として [`String`] に [`Assoc`], [`Identity`] を実装するラッパーです。
+///
+/// [`String`]: http://doc.rust-lang.org/std/str/struct.String.html
+/// [`Assoc`]: traits.Assoc.html
+/// [`Identity`]: traits.Identity.html
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Cat(pub String);
+
+impl Assoc for Cat {
+    fn op(self, rhs: Self) -> Self {
+        Cat(self.0.chars().chain(rhs.0.chars()).collect())
+    }
+}
+impl Identity for Cat {
+    fn identity() -> Self {
+        Cat(String::new())
     }
 }
