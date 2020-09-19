8a9134206a612ab1a333688e7f611b40072a48e4
ngtkana <ngtkana@gmail.com>
2020-09-19 20:35:26 +0900

    Add segtree

---
+++ crates/algolib/segtree2/src/lib.rs
@@ -0,0 +1,236 @@
+use std::{iter, ops};
+use type_traits::*;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Segtree<T> {
+    head: usize,
+    len: usize,
+    table: Vec<T>,
+}
+
+impl<T: Assoc> Segtree<T> {
+    pub fn from_slice(src: &[T]) -> Self {
+        if src.is_empty() {
+            Self {
+                head: 0,
+                len: 0,
+                table: Vec::new(),
+            }
+        } else {
+            let head = src.len() - 1;
+            let mut me = Self {
+                head,
+                len: src.len(),
+                table: src[1..].iter().chain(src).cloned().collect::<Vec<_>>(),
+            };
+            for i in (0..head).rev() {
+                me.update(i);
+            }
+            me
+        }
+    }
+
+    pub fn set(&mut self, i: usize, x: T) {
+        assert!(i < self.len);
+        self.modify(i, |y| y.clone_from(&x));
+    }
+
+    pub fn get(&mut self, i: usize) -> &T {
+        assert!(i < self.len);
+        &self.table[self.head + i]
+    }
+
+    pub fn modify(&mut self, mut i: usize, f: impl Fn(&mut T)) {
+        assert!(i < self.len);
+        i += self.head;
+        f(&mut self.table[i]);
+        for i in iter::successors(Some((i - 1) / 2), |&x| {
+            if x == 0 {
+                None
+            } else {
+                Some((x - 1) / 2)
+            }
+        }) {
+            self.update(i);
+        }
+    }
+
+    pub fn fold(&self, range: impl ops::RangeBounds<usize>) -> Option<T> {
+        let (mut start, mut end) = open(self.len, range);
+        assert!(start <= end, "変な区間を渡すのをやめませんか？");
+        assert!(end <= self.len, "範囲外です！");
+        start += self.head;
+        end += self.head;
+
+        if start == end {
+            None
+        } else if start + 1 == end {
+            Some(self.table[start].clone())
+        } else {
+            let mut left = self.table[start].clone();
+            start += 1;
+            end -= 1;
+            let mut right = self.table[end].clone();
+            while start != end {
+                if start % 2 == 0 {
+                    left.op_from_right(&self.table[start]);
+                    start += 1;
+                }
+                if end % 2 == 0 {
+                    end -= 1;
+                    right.op_from_left(&self.table[end]);
+                }
+                start = (start - 1) / 2;
+                end = (end - 1) / 2;
+            }
+            Some(T::op(left, right))
+        }
+    }
+
+    fn update(&mut self, i: usize) {
+        let x = T::op(self.table[2 * i + 1].clone(), self.table[2 * i + 2].clone());
+        self.table[i] = x;
+    }
+}
+
+fn open(len: usize, range: impl ops::RangeBounds<usize>) -> (usize, usize) {
+    use ops::Bound::*;
+    (
+        match range.start_bound() {
+            Unbounded => 0,
+            Included(&x) => x,
+            Excluded(&x) => x + 1,
+        },
+        match range.end_bound() {
+            Excluded(&x) => x,
+            Included(&x) => x + 1,
+            Unbounded => len,
+        },
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    mod tester;
+
+    use super::*;
+    use query_test::query;
+    use rand::prelude::*;
+    use std::marker::PhantomData;
+    use tester::{gen_instance, SegQuery};
+    use type_traits::wrappers::{Add, Affine, Cat};
+
+    fn gen_fp<Mod>(rng: &mut impl Rng) -> fp::Fp<Mod>
+    where
+        Mod: fp::Modable<Output = i64>,
+    {
+        fp::Fp::new(rng.gen_range(0, Mod::VALUE))
+    }
+
+    macro_rules! query_struct {
+        (struct $name:ident;) => {
+            struct $name<'a> {
+                pub len: usize,
+                pub rng: &'a mut StdRng,
+            }
+        };
+    }
+    macro_rules! triv_mtds {
+        ($self:ident: $Self:ident) => {
+            type Rng = StdRng;
+            fn new(len: usize, rng: &'a mut Self::Rng) -> $Self {
+                $Self { len, rng }
+            }
+            fn rng(&mut $self) -> &mut Self::Rng {
+                &mut $self.rng
+            }
+            fn len(&$self) -> usize {
+                $self.len
+            }
+        };
+    }
+
+    #[test]
+    fn test_add_fp_add() {
+        use rand::prelude::*;
+        type Fp = fp::F998244353;
+
+        query_struct! { struct Query; }
+        impl<'a> SegQuery<'a> for Query<'a> {
+            type Value = Add<Fp>;
+            triv_mtds! {self: Self}
+            fn gen_value(&mut self) -> Self::Value {
+                Add(gen_fp(&mut self.rng))
+            }
+        }
+
+        let mut rng = StdRng::seed_from_u64(42);
+        let mut instance = gen_instance(1..30, &mut rng, PhantomData::<Query>);
+        for _ in 0..20 {
+            match instance.query.rng().gen_range(0, 100) {
+                0..=29 => instance.apply(query!(set, i, x)),
+                20..=39 => instance.apply(query!(get, i)),
+                40..=99 => instance.apply(query!(fold, range)),
+                _ => panic!(),
+            }
+        }
+    }
+
+    #[test]
+    fn test_add_fp_affine() {
+        use rand::prelude::*;
+        type Fp = fp::F998244353;
+
+        query_struct! { struct Query; }
+        impl<'a> SegQuery<'a> for Query<'a> {
+            type Value = Affine<Fp>;
+            triv_mtds! {self: Self}
+            fn gen_value(&mut self) -> Self::Value {
+                Affine {
+                    a: gen_fp(&mut self.rng()),
+                    b: gen_fp(&mut self.rng()),
+                }
+            }
+        }
+
+        let mut rng = StdRng::seed_from_u64(42);
+        let mut instance = gen_instance(1..30, &mut rng, PhantomData::<Query>);
+        for _ in 0..20 {
+            match instance.query.rng().gen_range(0, 100) {
+                0..=29 => instance.apply(query!(set, i, x)),
+                20..=39 => instance.apply(query!(get, i)),
+                40..=99 => instance.apply(query!(fold, range)),
+                _ => panic!(),
+            }
+        }
+    }
+
+    #[test]
+    fn test_cat() {
+        use rand::prelude::*;
+
+        query_struct! { struct Query; }
+        impl<'a> SegQuery<'a> for Query<'a> {
+            type Value = Cat;
+            triv_mtds! {self: Self}
+            fn gen_value(&mut self) -> Self::Value {
+                Cat(
+                    iter::repeat_with(|| self.rng().sample(rand::distributions::Alphanumeric))
+                        .take(3)
+                        .collect::<String>(),
+                )
+            }
+        }
+
+        let mut rng = StdRng::seed_from_u64(42);
+        let mut instance = gen_instance(1..30, &mut rng, PhantomData::<Query>);
+        for _ in 0..20 {
+            match instance.query.rng().gen_range(0, 100) {
+                0..=29 => instance.apply(query!(set, i, x)),
+                20..=39 => instance.apply(query!(get, i)),
+                40..=99 => instance.apply(query!(fold, range)),
+                _ => panic!(),
+            }
+        }
+    }
+}
