fd6a38936aaa968535bac46824b4e7c7734b875a
ngtkana <ngtkana@gmail.com>
2020-09-21 12:03:29 +0900

    Add InversionNumber to wrappers

--- crates/utils/type_traits/src/wrappers.rs
+++ crates/utils/type_traits/src/wrappers.rs
@@ -48,6 +48,103 @@ where
     }
 }
 
+/// 転倒数を演算として、[`Assoc`], [`Identity`] を実装するラッパーです。
+///
+/// [`Assoc`]: traits.Assoc.html
+/// [`Identity`]: traits.Identity.html
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub struct InvertionNumber {
+    zero: u64,
+    one: u64,
+    invertion: u64,
+}
+impl Assoc for InvertionNumber {
+    fn op(self, rhs: Self) -> Self {
+        InvertionNumber {
+            zero: self.zero + rhs.zero,
+            one: self.one + rhs.one,
+            invertion: self.invertion + self.one * rhs.zero + rhs.invertion,
+        }
+    }
+}
+impl Identity for InvertionNumber {
+    fn identity() -> Self {
+        InvertionNumber::new()
+    }
+}
+impl InvertionNumber {
+    /// 空文字列に対応する転倒数です。
+    pub fn new() -> Self {
+        InvertionNumber {
+            zero: 0,
+            one: 0,
+            invertion: 0,
+        }
+    }
+    /// ビットならば 0 か 1 を、そうでなければ `None` を返します。
+    pub fn try_get_u8(&self) -> Option<u8> {
+        self.try_get_bool().map(|b| if b { 1 } else { 0 })
+    }
+    /// ビットならば 0 は `false`、1 は `ture` にして、そうでなければなければ `None` を返します。
+    pub fn try_get_bool(&self) -> Option<bool> {
+        if *self == Self::bit_zero() {
+            Some(false)
+        } else if *self == Self::bit_one() {
+            Some(true)
+        } else {
+            None
+        }
+    }
+    /// 0 の個数を返します。
+    pub fn count_zeros(&self) -> u64 {
+        self.zero
+    }
+    /// 1 の個数を返します。
+    pub fn count_ones(&self) -> u64 {
+        self.one
+    }
+    /// 転倒数を返します。
+    pub fn inversion_number(&self) -> u64 {
+        self.invertion
+    }
+    /// ビット 0 に対応する転倒数です。
+    pub fn bit_zero() -> Self {
+        InvertionNumber {
+            zero: 1,
+            one: 0,
+            invertion: 0,
+        }
+    }
+    /// ビット 1 に対応する転倒数です。
+    pub fn bit_one() -> Self {
+        InvertionNumber {
+            zero: 0,
+            one: 1,
+            invertion: 0,
+        }
+    }
+    /// `false` ならばビット 0、`true` ならばビット 1 を構築します。
+    pub fn from_bool(b: bool) -> Self {
+        if b {
+            Self::bit_one()
+        } else {
+            Self::bit_zero()
+        }
+    }
+    /// 0 ならばビット 0、1 ならばビット 1 を構築します。
+    ///
+    /// # Panics
+    ///
+    /// 0 でも 1 でもないものを渡すとパニックです。
+    pub fn from_u8(x: u8) -> Self {
+        match x {
+            0 => Self::bit_zero(),
+            1 => Self::bit_one(),
+            _ => panic!("0 でも 1 でもないものを渡すとパニックです。"),
+        }
+    }
+}
+
 /// [`ops::Add`], [`ops::Mul`] によるアフィン変換の合成で [`Assoc`], [`Ideneity`]
 /// を実装するラッパーです。
 ///
@@ -57,7 +154,9 @@ where
 /// [`Identity`]: traits.Identity.html
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Affine<T> {
+    /// 1 次の係数です。
     pub a: T,
+    /// 0 次の係数です。
     pub b: T,
 }
 
