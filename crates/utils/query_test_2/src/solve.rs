e4a8516396384ba75824a0da971db1b511859d76
ngtkana <ngtkana@gmail.com>
2020-09-29 02:41:50 +0900

    Refactor query_test_2

--- crates/utils/query_test_2/src/solve.rs
+++ crates/utils/query_test_2/src/solve.rs
@@ -1,21 +1,15 @@
 use crate::Query;
 
 /// 値で回答します。
-pub trait SolveValue2Value<Q: Query> {
+pub trait Solve<Q: Query> {
     #[allow(missing_docs)]
-    fn solve_value_to_value(&self, param: Q::Param) -> Q::Output;
+    fn solve(&self, param: Q::Param) -> Q::Output;
 }
 /// 参照で回答します。
 pub trait SolveGet<Q: Query> {
     #[allow(missing_docs)]
     fn solve_get(&self, param: Q::Param) -> &Q::Output;
 }
-/// 参照で回答します。
-pub trait SolveRef2Value<Q: Query> {
-    #[allow(missing_docs)]
-    fn solve_ref_to_value(&self, param: Q::Param) -> &Q::Output;
-}
-
 /// 自分を書き換えます。
 pub trait SolveMut<Q: Query> {
     #[allow(missing_docs)]
