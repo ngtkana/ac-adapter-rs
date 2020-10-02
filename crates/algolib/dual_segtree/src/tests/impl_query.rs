970e3e5379ca92189b3584b738f74f1c74d1e02e
ngtkana <ngtkana@gmail.com>
2020-10-02 18:37:36 +0900

    Add adapters for DualSegtreeWith

--- crates/algolib/dual_segtree/src/tests/impl_query.rs
+++ crates/algolib/dual_segtree/src/tests/impl_query.rs
@@ -1,7 +1,7 @@
-use crate::DualSegtree;
+use crate::{DualSegtree, DualSegtreeWith};
 use query_test_2::{query, solve, FromBrute, Vector};
 use std::ops::Range;
-use type_traits::{actions::Adj, Identity};
+use type_traits::{actions::Adj, Action, Identity};
 
 impl<T: Identity> FromBrute for DualSegtree<T> {
     type Brute = Vector<T>;
@@ -17,5 +17,22 @@ impl<T: Identity> solve::SolveMut<query::Get<T>> for DualSegtree<T> {
 impl<T: Identity> solve::Mutate<query::RangeApply<Adj<T>>> for DualSegtree<T> {
     fn mutate(&mut self, (range, action): (Range<usize>, Adj<T>)) {
         self.apply(range, action.0)
+    }
+}
+
+impl<T: Action + Identity> FromBrute for DualSegtreeWith<T> {
+    type Brute = Vector<T::Space>;
+    fn from_brute(_brute: &Self::Brute) -> Self {
+        todo!()
+    }
+}
+impl<T: Action + Identity> solve::SolveMut<query::Get<T::Space>> for DualSegtreeWith<T> {
+    fn solve_mut(&mut self, _i: usize) -> T::Space {
+        todo!()
+    }
+}
+impl<T: Action + Identity> solve::Mutate<query::RangeApply<T>> for DualSegtree<T> {
+    fn mutate(&mut self, (_range, _action): (Range<usize>, T)) {
+        todo!()
     }
 }
