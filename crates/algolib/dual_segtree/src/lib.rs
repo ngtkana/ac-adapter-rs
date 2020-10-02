96f0edc144d1b73a2745a4bbb70e5709b6925985
ngtkana <ngtkana@gmail.com>
2020-10-02 18:32:55 +0900

    Impl DualSegtree

--- crates/algolib/dual_segtree/src/lib.rs
+++ crates/algolib/dual_segtree/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeBounds;
+use std::ops::{self, Range, RangeBounds};
 use type_traits::{Action, Identity};
 
 #[derive(Debug, Clone, PartialEq)]
@@ -35,17 +35,58 @@ impl<T: Identity> DualSegtree<T> {
                 .collect::<Vec<_>>(),
         }
     }
-    pub fn apply(&mut self, _range: impl RangeBounds<usize>, _x: T) {
-        todo!()
+    pub fn apply(&mut self, range: impl RangeBounds<usize>, x: T) {
+        let Range { mut start, mut end } = open(self.len, range);
+        if start < end {
+            start += self.len;
+            end += self.len;
+            self.thrust(start);
+            self.thrust(end - 1);
+            while start != end {
+                if start % 2 == 1 {
+                    self.table[start].op_from_left(&x);
+                    start += 1;
+                }
+                if end % 2 == 1 {
+                    end -= 1;
+                    self.table[end].op_from_left(&x);
+                }
+                start >>= 1;
+                end >>= 1;
+            }
+        }
     }
     pub fn get(&mut self, mut i: usize) -> &T {
         i += self.len;
         self.thrust(i);
         &self.table[i]
     }
-    fn thrust(&mut self, _i: usize) {
-        todo!()
+    fn thrust(&mut self, i: usize) {
+        let lg = self.lg;
+        (1..=lg)
+            .rev()
+            .map(|p| i >> p)
+            .for_each(|j| self.propagate(j));
     }
+    fn propagate(&mut self, i: usize) {
+        if self.table[i] != T::identity() {
+            let x = std::mem::replace(&mut self.table[i], T::identity());
+            self.table[2 * i].op_from_left(&x);
+            self.table[2 * i + 1].op_from_left(&x);
+        }
+    }
+}
+fn open(len: usize, range: impl RangeBounds<usize>) -> Range<usize> {
+    use ops::Bound::*;
+    (match range.start_bound() {
+        Unbounded => 0,
+        Included(&x) => x,
+        Excluded(&x) => x + 1,
+    })..(match range.end_bound() {
+        Excluded(&x) => x,
+        Included(&x) => x + 1,
+        Unbounded => len,
+    })
 }
 
 #[cfg(test)]
@@ -59,7 +100,7 @@ mod tests {
 
     #[test]
     fn test_add_fp() {
-        use type_traits::{actions::Adj, binary::Add};
+        use type_traits::{actions::Adj, binary::Add, Constant};
         type Node = Add<Fp>;
         type Action = Adj<Node>;
 
@@ -70,13 +111,13 @@ mod tests {
             }
         }
         impl gen::GenValue<Node> for G {
-            fn gen_value<R: Rng>(rng: &mut R) -> Node {
-                Add(Fp::new(rng.gen_range(0, 1)))
+            fn gen_value<R: Rng>(_rng: &mut R) -> Node {
+                Add(Fp::new(0))
             }
         }
         impl gen::GenAction<Action> for G {
             fn gen_action<R: Rng>(rng: &mut R) -> Action {
-                Adj(<G as gen::GenValue<Node>>::gen_value(rng))
+                Adj(Add(Fp::new(rng.gen_range(0, fp::Mod998244353::VALUE))))
             }
         }
 
@@ -84,7 +125,7 @@ mod tests {
         for _ in 0..4 {
             tester.initialize();
             for _ in 0..100 {
-                let command = tester.rng_mut().gen_range(0, 5);
+                let command = tester.rng_mut().gen_range(0, 2);
                 match command {
                     0 => tester.compare_mut::<query::Get<_>>(),
                     1 => tester.mutate::<query::RangeApply<_>>(),
