cb4cfcdfa9685e126b3720aefdb5db2b67d29436
ngtkana <ngtkana@gmail.com>
2020-10-02 17:23:43 +0900

    Add SolveMut

--- crates/utils/query_test_2/src/solve.rs
+++ crates/utils/query_test_2/src/solve.rs
@@ -5,6 +5,11 @@ pub trait Solve<Q: Query> {
     #[allow(missing_docs)]
     fn solve(&self, param: Q::Param) -> Q::Output;
 }
+/// 自分を書き換えて、値も返します。
+pub trait SolveMut<Q: Query> {
+    #[allow(missing_docs)]
+    fn solve_mut(&mut self, param: Q::Param) -> Q::Output;
+}
 /// 自分を書き換えます。
 pub trait Mutate<Q: Query<Output = ()>> {
     #[allow(missing_docs)]
@@ -13,4 +18,10 @@ pub trait Mutate<Q: Query<Output = ()>> {
 /// 結果が正しければ `true`, 誤っていれば `false` を返します。
 pub trait Judge<Q: Query> {
     fn judge(&self, param: Q::Param, output: Q::Output) -> bool;
+}
+
+impl<Q: Query, T: Solve<Q>> SolveMut<Q> for T {
+    fn solve_mut(&mut self, param: Q::Param) -> Q::Output {
+        self.solve(param)
+    }
 }
