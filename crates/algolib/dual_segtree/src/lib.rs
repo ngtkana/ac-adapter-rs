f24c8c07a5e3c82221dacf7ccf0d10f1ddee9708
ngtkana <ngtkana@gmail.com>
2020-10-02 16:34:33 +0900

    Add a mock of dual_segtree

---
+++ crates/algolib/dual_segtree/src/lib.rs
@@ -0,0 +1,45 @@
+use std::ops::RangeBounds;
+use type_traits::{Action, Identity};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct DualSegtreeWith<T: Action> {
+    dual: DualSegtree<T>,
+    table: Vec<T::Space>,
+}
+impl<T: Action> DualSegtreeWith<T> {
+    pub fn from_slice(_src: &[T], _table: &[T::Space]) -> Self {
+        todo!()
+    }
+    pub fn apply(&mut self, _range: impl RangeBounds<usize>, _x: T) {
+        todo!()
+    }
+    pub fn get(&mut self, _i: usize) -> &T::Space {
+        todo!()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct DualSegtree<T> {
+    len: usize,
+    lg: u32,
+    lazy: Vec<T>,
+}
+impl<T: Identity> DualSegtree<T> {
+    pub fn from_slice(_src: &[T]) -> Self {
+        todo!()
+    }
+    pub fn apply(&mut self, _range: impl RangeBounds<usize>, _x: T) {
+        todo!()
+    }
+    pub fn get(&mut self, _i: usize) -> T {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
