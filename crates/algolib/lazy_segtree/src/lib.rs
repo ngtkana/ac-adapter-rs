a00aeb994deb90dfe0fc8363a0c19e01991137bf
ngtkana <ngtkana@gmail.com>
2020-10-10 02:54:08 +0900

    Implement Set, Fold, RangeApply

--- crates/algolib/lazy_segtree/src/lib.rs
+++ crates/algolib/lazy_segtree/src/lib.rs
@@ -1,33 +1,125 @@
 #![allow(dead_code, unused_variables, unused_imports)]
 use alg_traits::{Action, Identity};
-use std::ops::{Range, RangeBounds};
+use std::{
+    cell::RefCell,
+    ops::{DerefMut, Range, RangeBounds},
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct LazySegtree<A: Action, T: Identity> {
+    lg: u32,
     len: usize,
-    lazy: std::cell::RefCell<Vec<A::Value>>,
-    table: std::cell::RefCell<Vec<T::Value>>,
+    lazy: RefCell<Vec<A::Value>>,
+    table: RefCell<Vec<T::Value>>,
 }
 
 impl<A, T> LazySegtree<A, T>
 where
-    A: Action<Point = T::Value>,
+    A: Action<Point = T::Value> + Identity,
     T: Identity,
 {
     fn from_slice(src: &[T::Value]) -> Self {
-        todo!()
+        let len = src.len();
+        let mut table = vec![T::identity(); 2 * len];
+        table[len..].clone_from_slice(src);
+        for i in (1..len).rev() {
+            table[i] = T::op(table[2 * i].clone(), table[2 * i + 1].clone());
+        }
+        Self {
+            lg: len.next_power_of_two().trailing_zeros(),
+            len,
+            table: RefCell::new(table),
+            lazy: RefCell::new(std::iter::repeat(A::identity()).take(len).collect()),
+        }
     }
 
-    fn set(&mut self, i: usize, x: T::Value) {
-        todo!()
+    fn set(&mut self, mut i: usize, x: T::Value) {
+        i += self.len;
+        for p in (1..=self.lg).rev() {
+            self.push(i >> p);
+        }
+        self.table.borrow_mut()[i] = x;
+        for p in 1..=self.lg {
+            self.update(i >> p);
+        }
     }
 
     fn fold(&self, range: impl RangeBounds<usize>) -> T::Value {
-        todo!()
+        let Range { mut start, mut end } = open::open(self.len, range);
+        assert!(start <= end, "変な区間");
+        assert!(end <= self.len, "範囲外");
+        if start == end {
+            T::identity()
+        } else {
+            start += self.len;
+            end += self.len;
+            for p in (1..=self.lg).rev() {
+                if ((start >> p) << p) != start {
+                    self.push(start >> p);
+                }
+                if ((end >> p) << p) != end {
+                    self.push(end >> p);
+                }
+            }
+
+            let mut left = T::identity();
+            let mut right = T::identity();
+            while start != end {
+                if start % 2 == 1 {
+                    T::op_left(&mut left, self.table.borrow()[start].clone());
+                    start += 1;
+                }
+                if end % 2 == 1 {
+                    end -= 1;
+                    T::op_right(self.table.borrow()[end].clone(), &mut right);
+                }
+                start >>= 1;
+                end >>= 1;
+            }
+            T::op(left, right)
+        }
     }
 
     fn apply(&mut self, range: impl RangeBounds<usize>, a: A::Value) {
-        todo!()
+        let Range { mut start, mut end } = open::open(self.len, range);
+        if start != end {
+            start += self.len;
+            end += self.len;
+            for p in (1..=self.lg).rev() {
+                if ((start >> p) << p) != start {
+                    self.push(start >> p);
+                }
+                if ((end >> p) << p) != end {
+                    self.push((end - 1) >> p);
+                }
+            }
+            {
+                let orig_start = start;
+                let orig_end = end;
+                while start != end {
+                    if start % 2 == 1 {
+                        self.apply_subtree(start, a.clone());
+                        start += 1;
+                    }
+                    if end % 2 == 1 {
+                        end -= 1;
+                        self.apply_subtree(end, a.clone());
+                    }
+                    start >>= 1;
+                    end >>= 1;
+                }
+                start = orig_start;
+                end = orig_end;
+            }
+            for p in 1..=self.lg {
+                if ((start >> p) << p) != start {
+                    self.update(start >> p);
+                }
+                if ((end >> p) << p) != end {
+                    self.update((end - 1) >> p);
+                }
+            }
+        }
     }
 
     fn search_forward<R, F>(&self, range: R, pred: F) -> usize
@@ -44,6 +136,36 @@ where
         F: FnMut(&T::Value) -> bool,
     {
         todo!()
+    }
+
+    fn peek_one(&self, mut i: usize) -> T::Value {
+        i += self.len;
+        let mut x = self.table.borrow()[i].clone();
+        for p in (1..=self.lg).rev() {
+            A::act_assign(self.lazy.borrow()[i >> p].clone(), &mut x);
+        }
+        x
+    }
+    fn peek(&self) -> Vec<T::Value> {
+        (0..self.len).map(|i| self.peek_one(i)).collect::<Vec<_>>()
+    }
+    fn update(&self, i: usize) {
+        let x = T::op(
+            self.table.borrow()[2 * i].clone(),
+            self.table.borrow()[2 * i + 1].clone(),
+        );
+        self.table.borrow_mut()[i] = x;
+    }
+    fn push(&self, i: usize) {
+        let x = std::mem::replace(&mut self.lazy.borrow_mut()[i], A::identity());
+        self.apply_subtree(2 * i, x.clone());
+        self.apply_subtree(2 * i + 1, x);
+    }
+    fn apply_subtree(&self, i: usize, a: A::Value) {
+        A::act_assign(a.clone(), &mut self.table.borrow_mut()[i]);
+        if i < self.len {
+            A::op_right(a, &mut self.lazy.borrow_mut()[i]);
+        }
     }
 }
 
@@ -78,13 +200,8 @@ mod tests {
         struct A {}
         impl Assoc for A {
             type Value = u32;
-            fn op(a: u32, b: u32) -> u32 {
-                a + b
-            }
-        }
-        impl Identity for A {
-            fn identity() -> u32 {
-                0
+            fn op(a: u32, _b: u32) -> u32 {
+                a
             }
         }
         impl Action for A {
@@ -111,6 +228,11 @@ mod tests {
                 rng.gen_range(0, 100)
             }
         }
+        impl test_vector2::GenActor<Option<A>> for G {
+            fn gen_actor(rng: &mut impl Rng) -> Option<u32> {
+                Some(rng.gen_range(0, 100))
+            }
+        }
 
         struct P {}
         impl queries::Pred<(u32, u32), u32> for P {
@@ -119,16 +241,15 @@ mod tests {
             }
         }
 
-        let mut tester = Tester::<A, X, G>::new(StdRng::seed_from_u64(42));
+        let mut tester = Tester::<Option<A>, X, G>::new(StdRng::seed_from_u64(42));
         for _ in 0..4 {
             tester.initialize();
             for _ in 0..100 {
-                let command = tester.rng_mut().gen_range(0, 4);
+                let command = tester.rng_mut().gen_range(0, 3);
                 match command {
                     0 => tester.mutate::<queries::Set<_>>(),
                     1 => tester.compare::<queries::Fold<_>>(),
-                    2 => tester.judge::<queries::SearchForward<_, _, P>>(),
-                    3 => tester.judge::<queries::SearchBackward<_, _, P>>(),
+                    2 => tester.mutate::<queries::RangeApply<_>>(),
                     _ => unreachable!(),
                 }
             }
