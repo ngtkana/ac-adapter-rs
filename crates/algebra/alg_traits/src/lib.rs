3f450d368ccf24ff3b64bf1c4f3e3802a72c088f
ngtkana <ngtkana@gmail.com>
2020-10-10 01:05:22 +0900

    Add impl traits for Option

--- crates/algebra/alg_traits/src/lib.rs
+++ crates/algebra/alg_traits/src/lib.rs
@@ -27,3 +27,29 @@ pub trait Action: Assoc {
         *x = Self::act(a, x.clone());
     }
 }
+
+impl<T: Assoc> Assoc for Option<T> {
+    type Value = Option<T::Value>;
+    fn op(lhs: Option<T::Value>, rhs: Option<T::Value>) -> Self::Value {
+        match (lhs, rhs) {
+            (Some(lhs), Some(rhs)) => Some(T::op(lhs, rhs)),
+            (x, None) => x,
+            (None, y) => y,
+        }
+    }
+}
+impl<T: Assoc> Identity for Option<T> {
+    fn identity() -> Option<T::Value> {
+        None
+    }
+}
+impl<A: Action> Action for Option<A> {
+    type Point = A::Point;
+    fn act(a: Option<A::Value>, x: A::Point) -> A::Point {
+        if let Some(a) = a {
+            A::act(a, x)
+        } else {
+            x
+        }
+    }
+}
