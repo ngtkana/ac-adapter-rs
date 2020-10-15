561e58804ba6cb96eee2b23dd17c84574f00f307
ngtkana <ngtkana@gmail.com>
2020-10-16 03:01:30 +0900

    Rename segbeats_task1 -> segbeats

--- crates/algolib/segbeats/src/tests/impl_query.rs
+++ crates/algolib/segbeats/src/tests/impl_query.rs
@@ -2,7 +2,7 @@ use super::{
     queries::{ChangeMax, ChangeMin, QueryMax, QueryMin, QuerySum},
     vector::Vector,
 };
-use crate::{Elm, SegbeatsTask1};
+use crate::{Elm, Segbeats};
 use query_test::{
     solve::{Mutate, Solve},
     FromBrute,
@@ -10,34 +10,34 @@ use query_test::{
 use std::ops::Range;
 
 // -- solve
-impl<T: Elm> Mutate<ChangeMin<T>> for SegbeatsTask1<T> {
+impl<T: Elm> Mutate<ChangeMin<T>> for Segbeats<T> {
     fn mutate(&mut self, (range, x): (Range<usize>, T)) {
         self.change_min(range, x);
     }
 }
-impl<T: Elm> Mutate<ChangeMax<T>> for SegbeatsTask1<T> {
+impl<T: Elm> Mutate<ChangeMax<T>> for Segbeats<T> {
     fn mutate(&mut self, (range, x): (Range<usize>, T)) {
         self.change_max(range, x);
     }
 }
-impl<T: Elm> Solve<QueryMin<T>> for SegbeatsTask1<T> {
+impl<T: Elm> Solve<QueryMin<T>> for Segbeats<T> {
     fn solve(&self, range: Range<usize>) -> T {
         self.query_min(range)
     }
 }
-impl<T: Elm> Solve<QueryMax<T>> for SegbeatsTask1<T> {
+impl<T: Elm> Solve<QueryMax<T>> for Segbeats<T> {
     fn solve(&self, range: Range<usize>) -> T {
         self.query_max(range)
     }
 }
-impl<T: Elm> Solve<QuerySum<T>> for SegbeatsTask1<T> {
+impl<T: Elm> Solve<QuerySum<T>> for Segbeats<T> {
     fn solve(&self, range: Range<usize>) -> T {
         self.query_sum(range)
     }
 }
 
 // -- from brute
-impl<T: Elm> FromBrute for SegbeatsTask1<T> {
+impl<T: Elm> FromBrute for Segbeats<T> {
     type Brute = Vector<T>;
     fn from_brute(src: &Vector<T>) -> Self {
         Self::new(&src.0)
