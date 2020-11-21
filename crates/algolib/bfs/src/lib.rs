8505b2ab8709a5f2313947e8b55bc03c9251d352
ngtkana <ngtkana@gmail.com>
2020-11-21 23:36:51 +0900

    Add diameter to bfs

--- crates/algolib/bfs/src/lib.rs
+++ crates/algolib/bfs/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-pub fn sssp(g: &[Vec<usize>], s: usize) -> Vec<u32> {
+pub fn calc_dist(s: usize, g: &[Vec<usize>]) -> Vec<u32> {
     let mut dist = vec![std::u32::MAX; g.len()];
     dist[s] = 0;
     let mut queue = VecDeque::from(vec![s]);
@@ -15,7 +15,18 @@ pub fn sssp(g: &[Vec<usize>], s: usize) -> Vec<u32> {
     dist
 }
 
-pub fn sssp_restore(g: &[Vec<usize>], s: usize) -> (Vec<u32>, Vec<usize>) {
+pub fn tree_diamter(g: &[Vec<usize>]) -> ([usize; 2], u32) {
+    assert_eq!(g.iter().flatten().count(), (g.len() - 1) * 2);
+    let dist = calc_dist(0, &g);
+    let &diam = dist.iter().max().unwrap();
+    let x = dist.iter().position(|&d| d == diam).unwrap();
+    let dist = calc_dist(x, &g);
+    let &diam = dist.iter().max().unwrap();
+    let y = dist.iter().position(|&d| d == diam).unwrap();
+    ([x, y], diam)
+}
+
+pub fn calc_dist_restore(s: usize, g: &[Vec<usize>]) -> (Vec<u32>, Vec<usize>) {
     let mut dist = vec![std::u32::MAX; g.len()];
     let mut prv = vec![std::usize::MAX; g.len()];
     dist[s] = 0;
@@ -33,8 +44,8 @@ pub fn sssp_restore(g: &[Vec<usize>], s: usize) -> (Vec<u32>, Vec<usize>) {
     (dist, prv)
 }
 
-pub fn psp_path(g: &[Vec<usize>], s: usize, t: usize) -> Option<Vec<usize>> {
-    let (dist, prv) = sssp_restore(&g, s);
+pub fn find_path(s: usize, t: usize, g: &[Vec<usize>]) -> Option<Vec<usize>> {
+    let (dist, prv) = calc_dist_restore(s, &g);
     if dist[t] == std::u32::MAX {
         None
     } else {
@@ -52,12 +63,13 @@ pub fn psp_path(g: &[Vec<usize>], s: usize, t: usize) -> Option<Vec<usize>> {
 
 #[cfg(test)]
 mod tests {
-    use super::{psp_path, sssp, sssp_restore};
+    use super::{calc_dist, calc_dist_restore, find_path};
     use rand::prelude::*;
-    use randtools::{LogUniform, SimpleGraph};
+    use randtools::{LogUniform, SimpleGraph, Tree};
 
+    // -- graph
     #[test]
-    fn test_sssp() {
+    fn test_graph() {
         let mut rng = StdRng::seed_from_u64(42);
         for test_id in 0..100 {
             let n = rng.sample(LogUniform(2, 3000));
@@ -67,20 +79,20 @@ mod tests {
             let s = rng.gen_range(0, n);
             let t = rng.gen_range(0, n);
 
-            // sssp
-            let dist = sssp(&g, s);
+            // calc_dist
+            let dist = calc_dist(s, &g);
             validate_dist(&g, &dist, s);
 
-            // sssp_restore
+            // calc_dist_restore
             let prv = {
-                let (dist1, prv) = sssp_restore(&g, s);
+                let (dist1, prv) = calc_dist_restore(s, &g);
                 assert_eq!(dist, dist1);
                 prv
             };
             validate_prv(&dist, &prv, s);
 
-            // psp_path
-            let path = psp_path(&g, s, t);
+            // find_path
+            let path = find_path(s, t, &g);
             if let Some(path) = path {
                 validate_path(&dist, &path, s, t);
             } else {
@@ -114,5 +126,49 @@ mod tests {
         path.iter()
             .enumerate()
             .for_each(|(i, &x)| assert_eq!(dist[x], i as u32));
+    }
+
+    // -- tree
+    #[test]
+    fn test_tree() {
+        let mut rng = StdRng::seed_from_u64(42);
+        for test_id in 0..100 {
+            let n = rng.sample(LogUniform(2, 20));
+            println!("Test {}, n = {}", test_id, n);
+            let g = rng.sample(Tree(n));
+            let (_, result) = super::tree_diamter(&g);
+            let (_, expected) = brute_tree_diameter(&g);
+            assert_eq!(result, expected);
+        }
+    }
+
+    fn brute_tree_diameter(g: &[Vec<usize>]) -> ([usize; 2], u32) {
+        let n = g.len();
+        let mut dist = vec![vec![std::u32::MAX; n]; n];
+        (0..n).for_each(|i| dist[i][i] = 0);
+        g.iter()
+            .enumerate()
+            .map(|(i, v)| v.iter().copied().map(move |j| (i, j)))
+            .flatten()
+            .for_each(|(i, j)| {
+                dist[i][j] = 1;
+                dist[j][i] = 1
+            });
+        for k in 0..n {
+            for i in 0..n {
+                for j in 0..n {
+                    let x = dist[i][k].saturating_add(dist[k][j]).min(dist[i][j]);
+                    dist[i][j] = x;
+                }
+            }
+        }
+        assert!(dist.iter().flatten().all(|&x| x != u32::MAX));
+        dist.iter()
+            .enumerate()
+            .map(|(i, v)| v.iter().copied().enumerate().map(move |(j, x)| (i, j, x)))
+            .flatten()
+            .map(|(i, j, x)| ([i, j], x))
+            .max_by_key(|&(_, x)| x)
+            .unwrap()
     }
 }
