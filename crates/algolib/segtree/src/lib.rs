631ec1942169caabf9973b55bfafd73a812d6e64
ngtkana <ngtkana@gmail.com>
2020-12-13 23:51:58 +0900

    Add binary searching to segtree

--- crates/algolib/segtree/src/lib.rs
+++ crates/algolib/segtree/src/lib.rs
@@ -57,6 +57,121 @@ where
         }
         (self.op)(fl, fr)
     }
+
+    pub fn search_forward(
+        &self,
+        range: impl RangeBounds<usize>,
+        mut pred: impl FnMut(&T) -> bool,
+    ) -> usize {
+        let Range { mut start, mut end } = open(range, self.len);
+        if start == end {
+            start
+        } else {
+            start += self.len;
+            end += self.len;
+            let orig_end = end;
+            let mut crr = (self.identity)();
+            let mut shift = 0;
+            while start != end {
+                if start % 2 == 1 {
+                    let nxt = (self.op)(crr.clone(), self.table[start].clone());
+                    if !pred(&nxt) {
+                        return self.search_subtree_forward(crr, start, pred);
+                    }
+                    crr = nxt;
+                    start += 1;
+                }
+                start >>= 1;
+                end >>= 1;
+                shift += 1;
+            }
+            for p in (0..shift).rev() {
+                let end = (orig_end >> p) - 1;
+                if end % 2 == 0 {
+                    let nxt = (self.op)(crr.clone(), self.table[end].clone());
+                    if !pred(&nxt) {
+                        return self.search_subtree_forward(crr, end, pred);
+                    }
+                    crr = nxt;
+                }
+            }
+            orig_end - self.len
+        }
+    }
+    pub fn search_backward(
+        &self,
+        range: impl RangeBounds<usize>,
+        mut pred: impl FnMut(&T) -> bool,
+    ) -> usize {
+        let Range { mut start, mut end } = open(range, self.len);
+        if start == end {
+            start
+        } else {
+            start += self.len;
+            end += self.len;
+            let orig_start_m1 = start - 1;
+            let mut crr = (self.identity)();
+            let mut shift = 0;
+            while start != end {
+                if end % 2 == 1 {
+                    end -= 1;
+                    let nxt = (self.op)(self.table[end].clone(), crr.clone());
+                    if !pred(&nxt) {
+                        return self.search_subtree_backward(crr, end, pred);
+                    }
+                    crr = nxt;
+                }
+                start = (start + 1) >> 1;
+                end >>= 1;
+                shift += 1;
+            }
+            for p in (0..shift).rev() {
+                let start = (orig_start_m1 >> p) + 1;
+                if start % 2 == 1 {
+                    let nxt = (self.op)(self.table[start].clone(), crr.clone());
+                    if !pred(&nxt) {
+                        return self.search_subtree_backward(crr, start, pred);
+                    }
+                    crr = nxt;
+                }
+            }
+            orig_start_m1 + 1 - self.len
+        }
+    }
+    fn search_subtree_forward(
+        &self,
+        mut crr: T,
+        mut root: usize,
+        mut pred: impl FnMut(&T) -> bool,
+    ) -> usize {
+        while root < self.len {
+            let nxt = (self.op)(crr.clone(), self.table[root * 2].clone());
+            root = if pred(&nxt) {
+                crr = nxt;
+                root * 2 + 1
+            } else {
+                root * 2
+            };
+        }
+        root - self.len
+    }
+    fn search_subtree_backward(
+        &self,
+        mut crr: T,
+        mut root: usize,
+        mut pred: impl FnMut(&T) -> bool,
+    ) -> usize {
+        while root < self.len {
+            let nxt = (self.op)(self.table[root * 2 + 1].clone(), crr.clone());
+            root = if pred(&nxt) {
+                crr = nxt;
+                root * 2
+            } else {
+                root * 2 + 1
+            };
+        }
+        root + 1 - self.len
+    }
 }
 
 fn open(range: impl RangeBounds<usize>, len: usize) -> Range<usize> {
@@ -195,7 +310,7 @@ mod tests {
             println!("a = {:?}", &a);
             println!("seg = {:?}", &seg);
             for _ in 0..200 {
-                match rng.gen_range(0, 2) {
+                match rng.gen_range(0, 4) {
                     0 => {
                         let i = rng.gen_range(0, n);
                         let s = iter::once(rng.sample(Alphanumeric)).collect::<String>();
@@ -206,6 +321,20 @@ mod tests {
                         let range = rng.sample(SubRange(0..n));
                         let result = seg.fold(range.clone());
                         let expected = a[range].iter().cloned().fold(String::new(), strcat);
+                        assert_eq!(result, expected);
+                    }
+                    2 => {
+                        let range = rng.sample(SubRange(0..n));
+                        let d = rng.gen_range(0, n);
+                        let result = seg.search_forward(range.clone(), |s| s.len() <= d);
+                        let expected = (range.start + d).min(range.end);
+                        assert_eq!(result, expected);
+                    }
+                    3 => {
+                        let range = rng.sample(SubRange(0..n));
+                        let d = rng.gen_range(0, n);
+                        let result = seg.search_backward(range.clone(), |s| s.len() <= d);
+                        let expected = (range.end.saturating_sub(d)).max(range.start);
                         assert_eq!(result, expected);
                     }
                     _ => panic!(),
