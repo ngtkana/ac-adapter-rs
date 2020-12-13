8e5cde7b806ae58cacad6364e781a523e84951b2
ngtkana <ngtkana@gmail.com>
2020-12-13 23:39:38 +0900

    Add trait impls of segtree

--- crates/algolib/segtree/src/lib.rs
+++ crates/algolib/segtree/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{
     fmt::{self, Debug, Formatter},
-    ops::{Bound, Range, RangeBounds},
+    ops::{Bound, Deref, DerefMut, Drop, Index, Range, RangeBounds},
+    slice::SliceIndex,
 };
 
 pub struct Segtree<T, Op, Ideneity> {
@@ -30,13 +31,11 @@ where
             identity,
         }
     }
-    pub fn set(&mut self, i: usize, x: T) {
-        let mut i = i + self.len;
-        self.table[i] = x;
-        while i != 0 {
-            i /= 2;
-            self.table[i] = (self.op)(self.table[i * 2].clone(), self.table[i * 2 + 1].clone());
-        }
+    pub fn as_slice(&self) -> &[T] {
+        &self.table[self.len..]
+    }
+    pub fn entry(&mut self, index: usize) -> Entry<'_, T, Op, Identity> {
+        Entry { seg: self, index }
     }
     pub fn fold(&self, range: impl RangeBounds<usize>) -> T {
         let Range { mut start, mut end } = open(range, self.len);
@@ -72,11 +71,81 @@ fn open(range: impl RangeBounds<usize>, len: usize) -> Range<usize> {
     }
 }
 
-impl<T: Debug, Op, Identity> Debug for Segtree<T, Op, Identity> {
+impl<T: Debug, Op, Identity> Debug for Segtree<T, Op, Identity>
+where
+    T: Clone,
+    Op: Fn(T, T) -> T,
+    Identity: Fn() -> T,
+{
     fn fmt(&self, w: &mut Formatter<'_>) -> fmt::Result {
         w.debug_tuple("Segtree")
             .field(&&self.table[self.len..])
             .finish()
+    }
+}
+
+impl<I: SliceIndex<[T]>, T, Op, Identity> Index<I> for Segtree<T, Op, Identity>
+where
+    T: Clone,
+    Op: Fn(T, T) -> T,
+    Identity: Fn() -> T,
+{
+    type Output = I::Output;
+    fn index(&self, index: I) -> &I::Output {
+        &self.as_slice()[index]
+    }
+}
+
+pub struct Entry<'a, T, Op, Identity>
+where
+    T: Clone,
+    Op: Fn(T, T) -> T,
+    Identity: Fn() -> T,
+{
+    seg: &'a mut Segtree<T, Op, Identity>,
+    index: usize,
+}
+
+impl<'a, T, Op, Identity> Drop for Entry<'a, T, Op, Identity>
+where
+    T: Clone,
+    Op: Fn(T, T) -> T,
+    Identity: Fn() -> T,
+{
+    fn drop(&mut self) {
+        let mut index = self.index + self.seg.len;
+        while index != 0 {
+            index /= 2;
+            self.seg.table[index] = (self.seg.op)(
+                self.seg.table[index * 2].clone(),
+                self.seg.table[index * 2 + 1].clone(),
+            );
+        }
+    }
+}
+
+impl<'a, T, Op, Identity> Deref for Entry<'a, T, Op, Identity>
+where
+    T: Clone,
+    Op: Fn(T, T) -> T,
+    Identity: Fn() -> T,
+{
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.seg[self.index.clone()]
+    }
+}
+
+impl<'a, T, Op, Identity> DerefMut for Entry<'a, T, Op, Identity>
+where
+    T: Clone,
+    Op: Fn(T, T) -> T,
+    Identity: Fn() -> T,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        let index = self.index;
+        let len = self.seg.len;
+        &mut (self.seg.table[len..])[index]
     }
 }
 
@@ -90,9 +159,34 @@ mod tests {
     };
 
     #[test]
+    fn test_index() {
+        let seg = Segtree::new(&[0, 1], |x, _y| x, || 0);
+        assert_eq!(seg[0], 0);
+        assert_eq!(seg[1], 1);
+        assert_eq!(&seg[..], &[0, 1]);
+    }
+
+    #[test]
+    fn test_as_slice() {
+        let seg = Segtree::new(&[0, 1], |x, _y| x, || 0);
+        assert_eq!(seg.as_slice(), &[0, 1]);
+    }
+
+    #[test]
+    fn test_entry() {
+        let mut seg = Segtree::new(&[0, 1], |x, _y| x, || 0);
+        *seg.entry(0) = 10;
+        *seg.entry(1) = 11;
+        assert_eq!(seg.as_slice(), &[10, 11]);
+        *seg.entry(0) = 20;
+        *seg.entry(1) = 21;
+        assert_eq!(seg.as_slice(), &[20, 21]);
+    }
+
+    #[test]
     fn test_strcat() {
         let mut rng = StdRng::seed_from_u64(42);
-        for _ in 0..200 {
+        for _ in 0..20 {
             let n = rng.gen_range(1, 40);
             let mut a = iter::repeat_with(|| iter::once(rng.sample(Alphanumeric)).collect())
                 .take(n)
@@ -106,7 +200,7 @@ mod tests {
                         let i = rng.gen_range(0, n);
                         let s = iter::once(rng.sample(Alphanumeric)).collect::<String>();
                         a[i] = s.clone();
-                        seg.set(i, s);
+                        *seg.entry(i) = s;
                     }
                     1 => {
                         let range = rng.sample(SubRange(0..n));
