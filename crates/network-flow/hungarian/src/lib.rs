d855bb0c7599a97cc23aecfff7020c188cbab760
Kana <ngtkana@gmail.com>
2021-01-31 03:06:37 +0900

    Hungarian supports unsized integers

--- crates/network-flow/hungarian/src/lib.rs
+++ crates/network-flow/hungarian/src/lib.rs
@@ -1,4 +1,9 @@
-pub fn hungarian(cost_matrix: &[Vec<i64>]) -> HungarianResult {
+use std::{
+    iter::Sum,
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
+
+pub fn hungarian<T: Value>(cost_matrix: &[Vec<T>]) -> HungarianResult<T> {
     let h = cost_matrix.len();
     let w = cost_matrix[0].len();
     let mut m = vec![std::usize::MAX; w + 1];
@@ -8,7 +13,7 @@ pub fn hungarian(cost_matrix: &[Vec<i64>]) -> HungarianResult {
         .map(|v| *v.iter().min().unwrap() - all_min)
         .collect::<Vec<_>>()
         .into_boxed_slice();
-    let mut right = vec![0; w].into_boxed_slice();
+    let mut right = vec![T::zero(); w].into_boxed_slice();
 
     for s in 0..h {
         // dijkstra
@@ -16,7 +21,7 @@ pub fn hungarian(cost_matrix: &[Vec<i64>]) -> HungarianResult {
             let mut used_l = vec![false; h];
             let mut used_r = vec![false; w + 1];
             let mut pi = vec![w; w];
-            let mut dist = vec![std::i64::MAX; w];
+            let mut dist = vec![T::infinity(); w];
             m[w] = s;
             let mut x0 = s;
             let mut y0 = w;
@@ -26,9 +31,9 @@ pub fn hungarian(cost_matrix: &[Vec<i64>]) -> HungarianResult {
                     let mut swap = w;
                     used_r[y0] = true;
                     used_l[x0] = true;
-                    let mut delta = std::i64::MAX;
+                    let mut delta = T::infinity();
                     for y in (0..w).filter(|&y| !used_r[y]) {
-                        let cost = cost_matrix[x0][y] - (right[y] - left[x0]);
+                        let cost = cost_matrix[x0][y] + left[x0] - right[y];
                         if cost < dist[y] {
                             pi[y] = y0;
                             dist[y] = cost;
@@ -66,7 +71,7 @@ pub fn hungarian(cost_matrix: &[Vec<i64>]) -> HungarianResult {
 
     let backward = m;
     let mut forward = vec![std::usize::MAX; h].into_boxed_slice();
-    let mut value = 0;
+    let mut value = T::zero();
     for (y, &x) in backward.iter().enumerate() {
         if x != std::usize::MAX {
             forward[x] = y;
@@ -88,20 +93,47 @@ pub fn hungarian(cost_matrix: &[Vec<i64>]) -> HungarianResult {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct HungarianResult {
+pub struct HungarianResult<T: Value> {
     pub forward: Box<[usize]>,
     pub backward: Box<[Option<usize>]>,
-    pub left: Box<[i64]>,
-    pub right: Box<[i64]>,
-    pub value: i64,
+    pub left: Box<[T]>,
+    pub right: Box<[T]>,
+    pub value: T,
+}
+
+pub trait Value:
+    Sized + Copy + Add<Output = Self> + AddAssign + Sub<Output = Self> + SubAssign + Sum + Ord
+{
+    fn zero() -> Self;
+    fn infinity() -> Self;
+}
+
+macro_rules! impl_value {
+    ($($T:ident),* $(,)?) => {$(
+        impl Value for $T {
+            fn zero() -> Self {
+                0
+            }
+            fn infinity() -> Self {
+                std::$T::MAX
+            }
+        }
+    )*}
+}
+
+impl_value! {
+    u8, u16, u32, u64, u128, usize,
+    i8, i16, i32, i64, i128, isize,
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{assert_eq, mem::swap};
+    use std::{assert_eq, fmt::Debug, mem::swap, ops::RangeInclusive};
+
+    use rand::distributions::uniform::SampleUniform;
 
     use {
-        super::{hungarian, HungarianResult},
+        super::{hungarian, HungarianResult, Value},
         dbg::{lg, tabular},
         itertools::Itertools,
         next_permutation::permutations_map,
@@ -115,7 +147,7 @@ mod tests {
 
     #[test_case(vec![vec![4, 3, 5], vec![3, 5, 9], vec![4, 1, 4]] => (vec![2, 0, 1], 9); "yosupo sample")]
     #[test_case(vec![vec![4, 3, 5], vec![3, 5, 0], vec![4, 1, 4]] => (vec![0, 2, 1], 5); "handmade")]
-    fn test_hand(cost_matrix: Vec<Vec<i64>>) -> (Vec<usize>, i64) {
+    fn test_hand(cost_matrix: Vec<Vec<u8>>) -> (Vec<usize>, u8) {
         let HungarianResult { forward, value, .. } = hungarian(&cost_matrix);
         (forward.into_vec(), value)
     }
@@ -123,7 +155,24 @@ mod tests {
     #[test_case(5, 5, 100, true)]
     #[test_case(5, 5, 1000, false)]
     #[test_case(30, 500, 30, false)]
-    fn test_rand(h: usize, w: usize, iter: usize, brute: bool) {
+    fn test_rand_u32(h: usize, w: usize, iter: usize, brute: bool) {
+        test_rand_impl::<i32>(h, w, iter, brute, 0..=100);
+    }
+
+    #[test_case(5, 5, 100, true)]
+    #[test_case(5, 5, 1000, false)]
+    #[test_case(30, 500, 30, false)]
+    fn test_rand_i32(h: usize, w: usize, iter: usize, brute: bool) {
+        test_rand_impl::<i32>(h, w, iter, brute, -100..=100);
+    }
+
+    fn test_rand_impl<T: Value + Debug + SampleUniform>(
+        h: usize,
+        w: usize,
+        iter: usize,
+        brute: bool,
+        value_range: RangeInclusive<T>,
+    ) {
         let mut rng = StdRng::seed_from_u64(42);
         for _ in 0..iter {
             let mut h = rng.gen_range(1..=h);
@@ -131,10 +180,13 @@ mod tests {
             if h > w {
                 swap(&mut h, &mut w);
             }
-            let cost_matrix =
-                repeat_with(|| repeat_with(|| rng.gen_range(0..=1)).take(w).collect_vec())
-                    .take(h)
-                    .collect_vec();
+            let cost_matrix = repeat_with(|| {
+                repeat_with(|| rng.gen_range(value_range.clone()))
+                    .take(w)
+                    .collect_vec()
+            })
+            .take(h)
+            .collect_vec();
             tabular!(&cost_matrix);
             let result = hungarian(&cost_matrix);
             if brute {
@@ -144,7 +196,7 @@ mod tests {
         }
     }
 
-    fn validate(cost_matrix: &[Vec<i64>], result: &HungarianResult) {
+    fn validate<T: Value + Debug>(cost_matrix: &[Vec<T>], result: &HungarianResult<T>) {
         let h = cost_matrix.len();
         let w = cost_matrix[0].len();
         let HungarianResult {
@@ -176,8 +228,8 @@ mod tests {
             .flatten()
         {
             assert!(
-                right[j] - left[i] <= x,
-                "left[{i}] = {left}, right[{j}] = {right}, cost_matrix[{i}][{j}] = {cost}",
+                right[j] <= x + left[i],
+                "left[{i}] = {left:?}, right[{j}] = {right:?}, cost_matrix[{i}][{j}] = {cost:?}",
                 i = i,
                 left = left[i],
                 j = j,
@@ -198,11 +250,11 @@ mod tests {
                 .iter()
                 .enumerate()
                 .map(|(i, &j)| cost_matrix[i][j])
-                .sum::<i64>()
+                .sum::<T>()
         );
     }
 
-    fn compare_with_brute(cost_matrix: &[Vec<i64>], result: &HungarianResult) {
+    fn compare_with_brute<T: Value + Debug>(cost_matrix: &[Vec<T>], result: &HungarianResult<T>) {
         let h = cost_matrix.len();
         let w = cost_matrix[0].len();
         let brute_value = permutations_map((0..w).collect_vec(), |v| {
@@ -213,10 +265,13 @@ mod tests {
         assert_eq!(brute_value, result.value);
     }
 
-    fn calculate_score(cost_matrix: &[Vec<i64>], perm: impl IntoIterator<Item = usize>) -> i64 {
+    fn calculate_score<T: Value + Debug>(
+        cost_matrix: &[Vec<T>],
+        perm: impl IntoIterator<Item = usize>,
+    ) -> T {
         perm.into_iter()
             .enumerate()
             .map(|(i, j)| cost_matrix[i][j])
-            .sum::<i64>()
+            .sum::<T>()
     }
 }
