a0b4ff49a103dadfec87f9ba52c8682b262dd773
Kana <ngtkana@gmail.com>
2021-02-03 00:05:03 +0900

    Add dbg/bitset

--- crates/utils/dbg/src/lib.rs
+++ crates/utils/dbg/src/lib.rs
@@ -1,3 +1,23 @@
+//! Provides print-debugging utilities.
+//!
+//! # Macros
+//!
+//! Prints arguments to STDERR with code position information.
+//!
+//! - [`lg`]
+//! - [`msg`]
+//!
+//!
+//! # Wrappers
+//!
+//! Provides various formats via `Display` trait.
+//!
+//! - [`BitSlice`]
+
+mod bitslice;
+
+pub use bitslice::BitSlice;
+
 #[macro_export]
 macro_rules! lg {
     () => {
@@ -15,21 +35,6 @@ macro_rules! lg {
     ($val:expr,) => { $crate::lg!($val) };
     ($($val:expr),+ $(,)?) => {
         ($($crate::lg!($val)),+,)
-    };
-}
-
-#[macro_export]
-macro_rules! lg_nl {
-    () => {
-        eprintln!("[{}:{}]", $crate::file!(), $crate::line!());
-    };
-    ($val:expr) => {
-        match $val {
-            tmp => {
-                eprintln!("[{}:{}] {}:\n{:?}", file!(), line!(), stringify!($val), tmp);
-                tmp
-            }
-        };
     };
 }
 
@@ -54,65 +59,4 @@ macro_rules! msg {
     ($msg:expr, $($val:expr),+ $(,)?) => {
         ($(msg!($msg, $val)),+,)
     };
-}
-
-#[macro_export]
-macro_rules! tabular {
-    ($val:expr) => {
-        $crate::lg_nl!($crate::Tabular($val))
-    };
-}
-
-#[macro_export]
-macro_rules! boolean_table {
-    ($val:expr) => {
-        $crate::lg_nl!($crate::BooleanTable($val));
-    };
-}
-
-#[macro_export]
-macro_rules! boolean_slice {
-    ($val:expr) => {
-        $crate::lg!($crate::BooleanSlice($val));
-    };
-}
-
-use std::fmt::{Debug, Formatter};
-
-#[derive(Clone)]
-pub struct Tabular<'a, T: Debug>(pub &'a [T]);
-impl<'a, T: Debug> Debug for Tabular<'a, T> {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        for i in 0..self.0.len() {
-            writeln!(f, "{:2} | {:?}", i, &self.0[i])?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Clone)]
-pub struct BooleanTable<'a>(pub &'a [Vec<bool>]);
-impl<'a> Debug for BooleanTable<'a> {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        for i in 0..self.0.len() {
-            writeln!(f, "{:2} | {:?}", i, BooleanSlice(&self.0[i]))?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Clone)]
-pub struct BooleanSlice<'a>(pub &'a [bool]);
-impl<'a> Debug for BooleanSlice<'a> {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            self.0
-                .iter()
-                .map(|&b| if b { "1 " } else { "0 " })
-                .collect::<String>()
-        )?;
-        Ok(())
-    }
 }
