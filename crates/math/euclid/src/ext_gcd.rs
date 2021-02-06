330b709c215acd70bb8bc5a412f7cbb4dfaf624f
Kana <ngtkana@gmail.com>
2021-02-07 05:53:59 +0900

    Add euclid

---
+++ crates/math/euclid/src/ext_gcd.rs
@@ -0,0 +1,53 @@
+use {super::Signed, std::mem::swap};
+
+/// Takes two integers `x, y` and returns `a, b, g` satisfying `ax + by = g, 0 < g`
+///
+/// # Panics
+///
+/// Panics if `x == 0 || y == 0`
+pub fn ext_gcd<T: Signed>(x: T, y: T) -> (T, T, T) {
+    assert_ne!(x, T::zero());
+    assert_ne!(y, T::zero());
+    let (a, g) = {
+        let mut x = x;
+        let mut y = y;
+        let mut u = T::one();
+        let mut v = T::zero();
+        while x != T::zero() {
+            let q = y / x;
+            y -= q * x;
+            v -= q * u;
+            swap(&mut x, &mut y);
+            swap(&mut u, &mut v);
+        }
+        if y < T::zero() {
+            (-v, -y)
+        } else {
+            (v, y)
+        }
+    };
+    dbg!(x, y, a, g);
+    assert_eq!((g - a * x) % y, T::zero());
+    let b = (g - a * x) / y;
+    (a, b, g)
+}
+
+#[cfg(test)]
+mod tests {
+    use {crate::ext_gcd, crate::gcd, test_case::test_case};
+
+    #[test_case(1, 1)]
+    #[test_case(1, 18)]
+    #[test_case(18, 1)]
+    #[test_case(42, 48)]
+    #[test_case(55, 89)]
+    #[test_case(420, 1200)]
+    fn test_gcd(x: i32, y: i32) {
+        let g = gcd(x, y);
+        for (x, y) in [x, -x].iter().copied().zip([y, -y].iter().copied()) {
+            let (a, b, d) = ext_gcd(x, y);
+            assert_eq!(d, g);
+            assert_eq!(a * x + b * y, g);
+        }
+    }
+}
---
+++ crates/math/euclid/src/lib.rs
@@ -0,0 +1,105 @@
+mod ext_gcd;
+mod gcd;
+
+pub use {ext_gcd::ext_gcd, gcd::gcd};
+
+use std::{
+    fmt::Debug,
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, RemAssign, Sub, SubAssign},
+};
+
+/// Abstraction of integers.
+pub trait Int:
+    Debug
+    + Copy
+    + Ord
+    + Add<Output = Self>
+    + AddAssign
+    + Sub<Output = Self>
+    + SubAssign
+    + Mul<Output = Self>
+    + MulAssign
+    + Div<Output = Self>
+    + DivAssign
+    + Rem<Output = Self>
+    + RemAssign
+{
+    /// Returns `0`.
+    fn zero() -> Self;
+    /// Returns `1`.
+    fn one() -> Self;
+    /// Increment `self`.
+    fn increment(&mut self);
+    /// Returns the absolute value.
+    fn abs(self) -> Self;
+    /// Calculates the quotient of Euclidean division of self by `rhs`.
+    fn div_euclid(self, rhs: Self) -> Self;
+    /// Calculates the least nonnegative remainder of `self (mod rhs)`.
+    fn rem_euclid(self, rhs: Self) -> Self;
+    /// Returns `true` if and only if `self` divides `n`.
+    fn divides(self, n: Self) -> bool {
+        n.rem_euclid(self) == Self::zero()
+    }
+}
+
+/// Abstraction of unsigned integers.
+pub trait Unsigned: Int {}
+/// Abstraction of signed integers.
+pub trait Signed: Int + Neg<Output = Self> {}
+
+macro_rules! impl_unsigned {
+    ($($t:ty),* $(,)?) => {$(
+        impl Int for $t {
+            fn zero() -> Self {
+                0
+            }
+            fn one() -> Self {
+                1
+            }
+            fn increment(&mut self) {
+                *self += 1;
+            }
+            fn abs(self) -> Self {
+                self
+            }
+            fn div_euclid(self, rhs: Self) -> Self {
+                self.div_euclid(rhs)
+            }
+            fn rem_euclid(self, rhs: Self) -> Self {
+                self.rem_euclid(rhs)
+            }
+        }
+        impl Unsigned for $t {}
+    )*}
+}
+impl_unsigned! {
+    usize, u8, u16, u32, u64, u128,
+}
+macro_rules! impl_signed {
+    ($($t:ty),* $(,)?) => {$(
+        impl Int for $t {
+            fn zero() -> Self {
+                0
+            }
+            fn one() -> Self {
+                1
+            }
+            fn increment(&mut self) {
+                *self += 1;
+            }
+            fn abs(self) -> Self {
+                self.abs()
+            }
+            fn div_euclid(self, rhs: Self) -> Self {
+                self.div_euclid(rhs)
+            }
+            fn rem_euclid(self, rhs: Self) -> Self {
+                self.rem_euclid(rhs)
+            }
+        }
+        impl Signed for $t {}
+    )*}
+}
+impl_signed! {
+    isize, i8, i16, i32, i64, i128,
+}
