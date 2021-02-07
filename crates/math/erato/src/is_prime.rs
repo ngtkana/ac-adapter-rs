08d99d585b5f8078f39b1746372cdc5384d60280
Kana <ngtkana@gmail.com>
2021-02-07 18:22:04 +0900

    [wip] Fix erato

--- crates/math/erato/src/is_prime.rs
+++ crates/math/erato/src/is_prime.rs
@@ -1,37 +1,53 @@
-use super::Int;
+use {super::Int, std::marker::PhantomData};
+
+pub type Sieve = SieveBase<needs_list::No>;
+pub type SieveWithList = SieveBase<needs_list::Yes>;
 
 /// Use a sieve of eratosthenes to query if an integer is prime.
-pub struct IsPrime {
-    is_prime: Vec<bool>,
+#[derive(Debug, Clone, PartialEq)]
+pub struct SieveBase<L: NeedsList> {
+    sieve: Vec<bool>,
+    list: L::PrimeList,
 }
 
-impl IsPrime {
+impl<L: NeedsList> SieveBase<L> {
     /// Construct a new empty sieve. No heap allocations is run via this method.
     ///
     /// # Examples
     ///
     /// ```
-    /// use erato::IsPrime;
-    /// let _ = IsPrime::new();
+    /// use erato::Sieve;
+    /// let _ = Sieve::new();
     /// ```
     pub fn new() -> Self {
         Self {
-            is_prime: Vec::new(),
+            sieve: Vec::new(),
+            list: L::new(),
         }
     }
 
-    /// Construct a new empty sieve. No heap allocations is run via this method.
+    pub fn len(&self) -> usize {
+        self.sieve.len()
+    }
+
+    pub fn reserve(&mut self, len: usize) {
+        if self.len() <= len {
+            *self = Self::with_capacity(len);
+        }
+    }
+
+    /// Construct a new empty sieve.
     ///
     /// # Examples
     ///
     /// ```
-    /// use erato::IsPrime;
-    /// let _ = IsPrime::with_capacity(10);
+    /// use erato::Sieve;
+    /// let _ = Sieve::with_capacity(10);
     /// ```
     pub fn with_capacity(n: usize) -> Self {
-        Self {
-            is_prime: construct_table(n.next_power_of_two()),
-        }
+        let sieve = construct_is_prime_table(n);
+        let list = L::construct(&sieve);
+        Self { sieve, list }
     }
 
     /// Returns `true` if `x` is a prime number.
@@ -40,26 +56,124 @@ impl IsPrime {
     ///
     /// if `x <= 0`.
     ///
+    ///
+    /// # Note
+    ///
+    /// If `self.len() <= x` sieve will extended the size to the next power of two of `x`.
+    ///
+    ///
     /// # Examples
     ///
     /// ```
-    /// use erato::IsPrime;
+    /// use erato::Sieve;
     ///
-    /// let mut sieve = IsPrime::new();
+    /// let mut sieve = Sieve::new();
     /// assert!(sieve.is_prime(2));
     /// assert!(!sieve.is_prime(6));
     /// ```
     pub fn is_prime<T: Int>(&mut self, x: T) -> bool {
         assert!(T::zero() <= x);
         let x = x.as_usize();
-        if self.is_prime.len() <= x {
+        if self.sieve.len() <= x {
             *self = Self::with_capacity(x + 1);
         }
-        self.is_prime[x.as_usize()]
+        self.sieve[x.as_usize()]
     }
 }
 
-fn construct_table(n: usize) -> Vec<bool> {
+impl SieveBase<needs_list::Yes> {
+    pub fn prime_numbers<T: Int>(&mut self) -> PrimeNumbers<T> {
+        PrimeNumbers {
+            sieve: self,
+            index: 0,
+            _marker: PhantomData,
+        }
+    }
+    pub fn prime_factors<T: Int>(&mut self, n: T) -> FactorizeByTrialDivision<T> {
+        assert!(T::zero() < n);
+        let mut prime_numbers = self.prime_numbers();
+        FactorizeByTrialDivision {
+            p: prime_numbers.next().unwrap(),
+            prime_numbers,
+            n,
+        }
+    }
+}
+
+/// [See the document of `SieveBase::prime_numbers`](SieveBase::prime_numbers)
+pub struct PrimeNumbers<'a, T> {
+    sieve: &'a mut SieveBase<needs_list::Yes>,
+    index: usize,
+    _marker: PhantomData<T>,
+}
+impl<'a, T: Int> Iterator for PrimeNumbers<'a, T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let Self { sieve, index, .. } = self;
+        let p = if let Some(&p) = sieve.list.get(*index) {
+            T::from_usize(p)
+        } else {
+            sieve.reserve((sieve.len() * 2).max(3));
+            T::from_usize(sieve.list[*index])
+        };
+        *index += 1;
+        Some(p)
+    }
+}
+
+/// [See the document of `SieveBase::factorize`](SieveBase::factorize)
+pub struct FactorizeByTrialDivision<'a, T> {
+    prime_numbers: PrimeNumbers<'a, T>,
+    p: T,
+    n: T,
+}
+impl<'a, T: Int> Iterator for FactorizeByTrialDivision<'a, T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let Self {
+            prime_numbers,
+            p,
+            n,
+        } = self;
+        if *n == T::one() {
+            None
+        } else {
+            while *n % *p != T::zero() {
+                *p = prime_numbers.next().unwrap();
+            }
+            *n /= *p;
+            Some(*p)
+        }
+    }
+}
+
+pub trait NeedsList {
+    type PrimeList;
+    fn new() -> Self::PrimeList;
+    fn construct(src: &[bool]) -> Self::PrimeList;
+}
+pub mod needs_list {
+    use super::{extract_prime_numbers_from_is_prime_table, NeedsList};
+    #[derive(Debug, Clone, Copy, PartialEq)]
+    pub enum Yes {}
+    pub enum No {}
+    impl NeedsList for Yes {
+        type PrimeList = Vec<usize>;
+        fn new() -> Self::PrimeList {
+            Vec::new()
+        }
+        fn construct(src: &[bool]) -> Self::PrimeList {
+            extract_prime_numbers_from_is_prime_table(src)
+        }
+    }
+    impl NeedsList for No {
+        type PrimeList = ();
+        fn new() -> Self::PrimeList {}
+        fn construct(_src: &[bool]) -> Self::PrimeList {}
+    }
+}
+
+fn construct_is_prime_table(n: usize) -> Vec<bool> {
     let mut is_prime = vec![true; n];
     (0..2.min(n)).for_each(|i| is_prime[i] = false);
     for p in (2..).take_while(|&p| p * p < n) {
@@ -75,10 +189,22 @@ fn construct_table(n: usize) -> Vec<bool> {
     is_prime
 }
 
+fn extract_prime_numbers_from_is_prime_table(is_prime: &[bool]) -> Vec<usize> {
+    is_prime
+        .iter()
+        .enumerate()
+        .filter(|&(_, &b)| b)
+        .map(|(p, _)| p)
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use {
-        super::{construct_table, IsPrime},
+        super::{
+            construct_is_prime_table, extract_prime_numbers_from_is_prime_table, Sieve,
+            SieveWithList,
+        },
         test_case::test_case,
     };
 
@@ -88,13 +214,25 @@ mod tests {
     #[test_case(3 => vec![false, false, true])]
     #[test_case(4 => vec![false, false, true, true])]
     #[test_case(5 => vec![false, false, true, true, false])]
-    fn test_construct_table(n: usize) -> Vec<bool> {
-        construct_table(n)
+    fn test_construct_is_prime_table(n: usize) -> Vec<bool> {
+        construct_is_prime_table(n)
+    }
+
+    #[test_case(0 => Vec::<usize>::new())]
+    #[test_case(1 => Vec::<usize>::new())]
+    #[test_case(2 => Vec::<usize>::new())]
+    #[test_case(3 => vec![2])]
+    #[test_case(4 => vec![2, 3])]
+    #[test_case(5 => vec![2, 3])]
+    #[test_case(6 => vec![2, 3, 5])]
+    #[test_case(16 => vec![2, 3, 5, 7, 11, 13])]
+    fn test_extract_prime_numbers_from_is_prime_table(n: usize) -> Vec<usize> {
+        extract_prime_numbers_from_is_prime_table(&construct_is_prime_table(n))
     }
 
     #[test]
     fn test_is_prime_via_new() {
-        let mut sieve = IsPrime::new();
+        let mut sieve = Sieve::new();
         assert!(sieve.is_prime(2));
         assert!(sieve.is_prime(3));
         assert!(!sieve.is_prime(9));
@@ -107,7 +245,7 @@ mod tests {
 
     #[test]
     fn test_is_prime_via_with_capacity() {
-        let mut sieve = IsPrime::with_capacity(10);
+        let mut sieve = Sieve::with_capacity(10);
         assert!(sieve.is_prime(2));
         assert!(sieve.is_prime(3));
         assert!(!sieve.is_prime(9));
@@ -116,5 +254,23 @@ mod tests {
         assert!(sieve.is_prime(7));
         assert!(sieve.is_prime(307));
         assert!(!sieve.is_prime(102));
+    }
+
+    #[test_case(0 => Vec::<i32>::new())]
+    #[test_case(1 => vec![2])]
+    #[test_case(2 => vec![2, 3])]
+    #[test_case(5 => vec![2, 3, 5, 7, 11])]
+    fn test_prime_numbers(len: usize) -> Vec<i32> {
+        let mut sieve = SieveWithList::new();
+        sieve.prime_numbers().take(len).collect()
+    }
+
+    #[test_case(1 => Vec::<i32>::new())]
+    #[test_case(2 => vec![2])]
+    #[test_case(15 => vec![3, 5])]
+    #[test_case(84 => vec![2, 2, 3, 7])]
+    fn test_prime_divisors(n: i32) -> Vec<i32> {
+        let mut sieve = SieveWithList::new();
+        sieve.prime_factors(n).collect()
     }
 }
--- crates/math/erato/src/lib.rs
+++ crates/math/erato/src/lib.rs
@@ -1,15 +1,36 @@
 mod is_prime;
 mod least;
 
-pub use is_prime::IsPrime;
+pub use is_prime::{Sieve, SieveWithList};
 pub use least::Least;
 
-use std::fmt::Debug;
+use std::{
+    fmt::Debug,
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign},
+};
 
 /// Abstraction of integers.
-pub trait Int: Debug + Copy + Ord {
+pub trait Int:
+    Debug
+    + Copy
+    + Ord
+    + Add<Output = Self>
+    + AddAssign
+    + Sub<Output = Self>
+    + SubAssign
+    + Mul<Output = Self>
+    + MulAssign
+    + Div<Output = Self>
+    + DivAssign
+    + Rem<Output = Self>
+    + RemAssign
+{
     /// Returns `0`.
     fn zero() -> Self;
+    /// Returns `1`.
+    fn one() -> Self;
+    /// Returns `2`.
+    fn two() -> Self;
     /// Converts into `usize`
     fn as_usize(self) -> usize;
     /// Converts an `usize` into `Self`
@@ -21,6 +42,12 @@ macro_rules! impl_int {
         impl Int for $t {
             fn zero() -> Self {
                 0
+            }
+            fn one() -> Self {
+                1
+            }
+            fn two() -> Self {
+                2
             }
             fn as_usize(self) -> usize {
                 self as usize
@@ -34,4 +61,39 @@ macro_rules! impl_int {
 impl_int! {
     usize, u8, u16, u32, u64, u128,
     isize, i8, i16, i32, i64, i128,
+}
+
+fn construct_least_table(n: usize) -> Vec<usize> {
+    let mut least = vec![std::usize::MAX; n];
+    for p in 2..n {
+        if least[p] != std::usize::MAX {
+            continue;
+        }
+        least[p] = p;
+        let mut i = p * p;
+        while i < n {
+            if least[i] == std::usize::MAX {
+                least[i] = p;
+            }
+            i += p;
+        }
+    }
+    least
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::construct_least_table, test_case::test_case};
+
+    #[test_case(0 => Vec::<usize>::new())]
+    #[test_case(1 => vec![std::usize::MAX])]
+    #[test_case(2 => vec![std::usize::MAX, std::usize::MAX])]
+    #[test_case(3 => vec![std::usize::MAX, std::usize::MAX, 2])]
+    #[test_case(4 => vec![std::usize::MAX, std::usize::MAX, 2, 3])]
+    #[test_case(5 => vec![std::usize::MAX, std::usize::MAX, 2, 3, 2])]
+    #[test_case(6 => vec![std::usize::MAX, std::usize::MAX, 2, 3, 2, 5])]
+    #[test_case(16 => vec![std::usize::MAX, std::usize::MAX, 2, 3, 2, 5, 2, 7, 2, 3, 2, 11, 2, 13, 2, 3])]
+    fn test_construct_least_table(n: usize) -> Vec<usize> {
+        construct_least_table(n)
+    }
 }
