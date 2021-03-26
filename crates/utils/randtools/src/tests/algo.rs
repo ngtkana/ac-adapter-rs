077f0c9fe58435900177209b480c9ebba571533a
Kana <ngtkana@gmail.com>
2021-03-26 20:16:55 +0900

    Apply automatic clippy::redundant-closure-for-method-calls.

--- crates/utils/randtools/src/tests/algo.rs
+++ crates/utils/randtools/src/tests/algo.rs
@@ -3,7 +3,7 @@ use std::mem;
 pub fn is_tree(g: &[Vec<usize>]) -> bool {
     // 頂点数 n, 辺数 2n - 2, 頂点番号 < n
     let n = g.len();
-    if g.iter().map(|v| v.len()).sum::<usize>() != 2 * n - 2 || g.iter().flatten().any(|&x| n <= x)
+    if g.iter().map(std::vec::Vec::len).sum::<usize>() != 2 * n - 2 || g.iter().flatten().any(|&x| n <= x)
     {
         return false;
     }
