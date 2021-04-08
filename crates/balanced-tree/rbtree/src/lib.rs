15943cb514891e887acabfe1e957b2c5c54758a3
Kana <ngtkana@gmail.com>
2021-04-09 01:48:51 +0900

    Add iter to rbtree

--- crates/balanced-tree/rbtree/src/lib.rs
+++ crates/balanced-tree/rbtree/src/lib.rs
@@ -1,6 +1,119 @@
-use std::cmp::Ordering;
+mod detail;
 
-// Non-empty RB-tree
+use std::{iter::FromIterator, mem::take};
+
+#[derive(Clone, Debug, Hash, PartialEq)]
+pub struct RbTree<T>(Option<Root<T>>);
+
+impl<T> Default for RbTree<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<A> FromIterator<A> for RbTree<A> {
+    fn from_iter<T: IntoIterator<Item = A>>(iter: T) -> Self {
+        let mut nodes = iter
+            .into_iter()
+            .map(|x| Self::singleton(x))
+            .collect::<Vec<_>>();
+        if nodes.is_empty() {
+            return Self::default();
+        }
+        let mut step = 1;
+        while step < nodes.len() {
+            for i in (0..nodes.len() - step).step_by(2 * step) {
+                nodes[i] = Self::merge(take(&mut nodes[i]), take(&mut nodes[i + step]));
+            }
+            step *= 2;
+        }
+        take(&mut nodes[0])
+    }
+}
+
+pub struct Iter<'a, T> {
+    stack: Vec<&'a Root<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.stack.pop() {
+                None => return None,
+                Some(Root::Nil(Nil(x))) => return Some(x),
+                Some(Root::Node(node)) => {
+                    self.stack.push(&node.right);
+                    self.stack.push(&node.left);
+                }
+            }
+        }
+    }
+}
+
+impl<T> RbTree<T> {
+    pub fn new() -> Self {
+        Self(None)
+    }
+    pub fn len(&self) -> usize {
+        match &self.0 {
+            None => 0,
+            Some(node) => node.len(),
+        }
+    }
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            stack: match &self.0 {
+                None => Vec::new(),
+                Some(node) => vec![node],
+            },
+        }
+    }
+    pub fn singleton(x: T) -> Self {
+        Self(Some(Root::singleton(x)))
+    }
+    pub fn push_front(&mut self, x: T) {
+        *self = Self::merge(Self::singleton(x), take(self));
+    }
+    pub fn push_back(&mut self, x: T) {
+        *self = Self::merge(take(self), Self::singleton(x));
+    }
+    pub fn insert(&mut self, i: usize, x: T) {
+        assert!((0..=self.len()).contains(&i));
+        let [l, r] = take(self).split(i);
+        *self = Self::merge(Self::merge(l, Self::singleton(x)), r);
+    }
+    pub fn delete(&mut self, i: usize) -> T {
+        assert!((0..self.len()).contains(&i));
+        let [l, cr] = take(self).split(i);
+        let [c, r] = cr.split(1);
+        *self = Self::merge(l, r);
+        match c.0 {
+            Some(Root::Node(_)) | None => unreachable!(),
+            Some(Root::Nil(Nil(x))) => x,
+        }
+    }
+    pub fn merge(lhs: Self, rhs: Self) -> Self {
+        match [lhs.0, rhs.0] {
+            [None, None] => Self(None),
+            [Some(l), None] => Self(Some(l)),
+            [None, Some(r)] => Self(Some(r)),
+            [Some(l), Some(r)] => Self(Some(Root::merge(l, r))),
+        }
+    }
+    pub fn split(self, i: usize) -> [Self; 2] {
+        assert!((0..=self.len()).contains(&i));
+        if i == 0 {
+            [Self(None), self]
+        } else if i == self.len() {
+            [self, Self(None)]
+        } else {
+            let [l, r] = self.0.unwrap().split(i);
+            [Self(Some(l)), Self(Some(r))]
+        }
+    }
+}
+
 #[derive(Clone, Debug, Hash, PartialEq)]
 enum Root<T> {
     Nil(Nil<T>),
@@ -17,131 +130,23 @@ struct Node<T> {
     len: usize,
 }
 
-impl<T> Node<T> {
-    fn new(lhs: Box<Root<T>>, rhs: Box<Root<T>>, height: usize) -> Self {
-        Self {
-            len: lhs.len() + rhs.len(),
-            height,
-            left: lhs,
-            right: rhs,
-        }
-    }
-    fn update(&mut self) {
-        self.len = self.left.len() + self.right.len();
-    }
-}
-
-impl<T> Root<T> {
-    fn split(self, i: usize) -> [Self; 2] {
-        let node = self.into_node().unwrap();
-        let left_len = node.left.len();
-        match i.cmp(&left_len) {
-            Ordering::Equal => [*node.left, *node.right],
-            Ordering::Less => {
-                let [l, r] = node.left.split(i);
-                [l, Self::merge(r, *node.right)]
-            }
-            Ordering::Greater => {
-                let [l, r] = node.right.split(i - left_len);
-                [Self::merge(*node.left, l), r]
-            }
-        }
-    }
-    fn merge(lhs: Self, rhs: Self) -> Self {
-        let h = lhs.height();
-        match h.cmp(&rhs.height()) {
-            Ordering::Equal => Self::Node(Node::new(Box::new(lhs), Box::new(rhs), h + 1)),
-            Ordering::Less => rhs.merge_front(lhs),
-            Ordering::Greater => lhs.merge_back(rhs),
-        }
-    }
-    fn merge_front(self, rhs: Self) -> Self {
-        let mut node = self.into_node().unwrap();
-        node.left = Box::new(if node.left.height() == rhs.height() {
-            Self::Node(Node::new(Box::new(rhs), node.left, node.height))
-        } else {
-            node.left.merge_front(rhs)
-        });
-        if node.height == node.left.node().unwrap().left.height() {
-            let Node {
-                left: l,
-                right: r,
-                height,
-                len: _,
-            } = node.left.into_node().unwrap();
-            node = Node::new(
-                l,
-                Box::new(Root::Node(Node::new(r, node.right, height))),
-                height + 1,
-            );
-        } else {
-            node.update();
-        }
-        Self::Node(node)
-    }
-    fn merge_back(self, rhs: Self) -> Self {
-        let mut node = self.into_node().unwrap();
-        node.right = Box::new(if node.right.height() == rhs.height() {
-            Self::Node(Node::new(node.right, Box::new(rhs), node.height))
-        } else {
-            node.right.merge_back(rhs)
-        });
-        if node.height == node.right.height() {
-            if node.height == node.left.height() {
-                node.height += 1;
-                node.update();
-            } else {
-                let Node {
-                    left: l,
-                    right: r,
-                    height,
-                    len: _,
-                } = node.right.into_node().unwrap();
-                node = Node::new(
-                    Box::new(Root::Node(Node::new(node.left, l, height))),
-                    r,
-                    height,
-                );
-            }
-        } else {
-            node.update();
-        }
-        Self::Node(node)
-    }
-    fn node(&self) -> Option<&Node<T>> {
-        match self {
-            Self::Nil(_) => None,
-            Self::Node(node) => Some(node),
-        }
-    }
-    fn into_node(self) -> Option<Node<T>> {
-        match self {
-            Self::Nil(_) => None,
-            Self::Node(node) => Some(node),
-        }
-    }
-    fn len(&self) -> usize {
-        match self {
-            Self::Nil(_) => 1,
-            Self::Node(node) => node.len,
-        }
-    }
-    fn height(&self) -> usize {
-        match self {
-            Self::Nil(_) => 0,
-            Self::Node(node) => node.height,
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use {
-        super::{Nil, Node, Root},
-        test_case::test_case,
-    };
+    use itertools::Itertools;
+    use std::iter::repeat_with;
 
-    fn validate(root: &Root<()>) {
+    use super::{RbTree, Root};
+    use rand::{prelude::StdRng, Rng, SeedableRng};
+    #[allow(unused_imports)]
+    use test_case::test_case;
+
+    fn validate<T>(tree: &RbTree<T>) {
+        match &tree.0 {
+            None => (),
+            Some(root) => validate_dfs(root),
+        }
+    }
+    fn validate_dfs<T>(root: &Root<T>) {
         if let Some(node) = root.node() {
             if let Some(left) = node.left.node() {
                 assert!(left.height == node.height || left.height == node.height - 1);
@@ -152,87 +157,22 @@ mod tests {
             if let Some(right) = node.right.node() {
                 assert_eq!(right.height + 1, node.height);
             }
+            validate_dfs(&node.left);
+            validate_dfs(&node.right);
         }
     }
 
-    fn to_structure_sring(root: &Root<()>) -> String {
-        fn dfs(root: &Root<()>, s: &mut String) {
-            s.push('(');
-            match root {
-                Root::Nil(_) => (),
-                Root::Node(node) => {
-                    dfs(&node.left, s);
-                    s.push_str(&node.height.to_string());
-                    s.push(',');
-                    s.push_str(&node.len.to_string());
-                    dfs(&node.right, s);
-                }
-            }
-            s.push(')');
+    #[test]
+    fn test_iter() {
+        let mut rng = StdRng::seed_from_u64(42);
+        for _ in 0..20 {
+            let n = rng.gen_range(0..20);
+            let a = repeat_with(|| rng.gen_range(0..100)).take(n).collect_vec();
+            println!("a = {:?}", &a);
+            let tree = a.iter().copied().collect::<RbTree<_>>();
+            validate(&tree);
+            let b = tree.iter().copied().collect_vec();
+            assert_eq!(&a, &b);
         }
-        let mut s = String::new();
-        dfs(root, &mut s);
-        s
-    }
-
-    fn one_node() -> Root<()> {
-        Root::Nil(Nil(()))
-    }
-    fn two_node() -> Root<()> {
-        Root::Node(Node::new(Box::new(one_node()), Box::new(one_node()), 1))
-    }
-    fn three_node() -> Root<()> {
-        Root::Node(Node::new(Box::new(two_node()), Box::new(one_node()), 1))
-    }
-    fn two_node_two_node() -> Root<()> {
-        Root::Node(Node::new(Box::new(two_node()), Box::new(two_node()), 2))
-    }
-    fn two_node_three_node() -> Root<()> {
-        Root::Node(Node::new(Box::new(two_node()), Box::new(three_node()), 2))
-    }
-    fn three_node_two_node() -> Root<()> {
-        Root::Node(Node::new(Box::new(three_node()), Box::new(two_node()), 2))
-    }
-    fn three_node_three_node() -> Root<()> {
-        Root::Node(Node::new(Box::new(three_node()), Box::new(three_node()), 2))
-    }
-
-    #[test_case(one_node() => "()".to_owned())]
-    #[test_case(two_node() => "(()1,2())".to_owned())]
-    #[test_case(three_node() => "((()1,2())1,3())".to_owned())]
-    fn test_to_structure_string(root: Root<()>) -> String {
-        to_structure_sring(&root)
-    }
-
-    #[test_case(one_node(), one_node() => to_structure_sring(&two_node()))]
-    #[test_case(one_node(), two_node() => to_structure_sring(&three_node()))]
-    #[test_case(one_node(), three_node() => to_structure_sring(&two_node_two_node()))]
-    #[test_case(two_node(), one_node() => to_structure_sring(&three_node()))]
-    #[test_case(two_node(), two_node() => to_structure_sring(&two_node_two_node()))]
-    #[test_case(two_node(), three_node() => to_structure_sring(&two_node_three_node()))]
-    #[test_case(three_node(), one_node() => to_structure_sring(&two_node_two_node()))]
-    #[test_case(three_node(), two_node() => to_structure_sring(&three_node_two_node()))]
-    #[test_case(three_node(), three_node() => to_structure_sring(&three_node_three_node()))]
-    fn test_merge(lhs: Root<()>, rhs: Root<()>) -> String {
-        let res = Root::merge(lhs, rhs);
-        validate(&res);
-        to_structure_sring(&res)
-    }
-
-    #[test_case(two_node(), 1 => [to_structure_sring(&one_node()), to_structure_sring(&one_node())])]
-    #[test_case(three_node(), 1 => [to_structure_sring(&one_node()), to_structure_sring(&two_node())])]
-    #[test_case(three_node(), 2 => [to_structure_sring(&two_node()), to_structure_sring(&one_node())])]
-    #[test_case(two_node_two_node(), 1 => [to_structure_sring(&one_node()), to_structure_sring(&three_node())])]
-    #[test_case(two_node_two_node(), 2 => [to_structure_sring(&two_node()), to_structure_sring(&two_node())])]
-    #[test_case(two_node_two_node(), 3 => [to_structure_sring(&three_node()), to_structure_sring(&one_node())])]
-    #[test_case(two_node_three_node(), 1 => [to_structure_sring(&one_node()), to_structure_sring(&two_node_two_node())])]
-    #[test_case(two_node_three_node(), 2 => [to_structure_sring(&two_node()), to_structure_sring(&three_node())])]
-    #[test_case(two_node_three_node(), 3 => [to_structure_sring(&three_node()), to_structure_sring(&two_node())])]
-    #[test_case(two_node_three_node(), 4 => [to_structure_sring(&two_node_two_node()), to_structure_sring(&one_node())])]
-    fn test_split(root: Root<()>, i: usize) -> [String; 2] {
-        let [l, r] = &Root::split(root, i);
-        validate(&l);
-        validate(&r);
-        [to_structure_sring(&l), to_structure_sring(&r)]
     }
 }
