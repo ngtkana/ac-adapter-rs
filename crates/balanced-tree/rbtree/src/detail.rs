8ab739f82a0b52e73b4ea9f177ce63477931530d
Kana <ngtkana@gmail.com>
2021-04-26 22:46:50 +0900

    Remove unnecessary lg(n) times allocations in rbtree merges

--- crates/balanced-tree/rbtree/src/detail.rs
+++ crates/balanced-tree/rbtree/src/detail.rs
@@ -4,6 +4,7 @@ use {
         cmp::Ordering,
         hash::{Hash, Hasher},
         marker::PhantomData,
+        mem::swap,
     },
 };
 
@@ -20,6 +21,167 @@ pub struct Node<T, O: Op<Value = T>> {
     pub len: usize,
     pub summary: O::Summary,
     pub __marker: PhantomData<fn(O) -> O>,
+}
+impl<T, O: Op<Value = T>> Node<T, O> {
+    pub fn new(lhs: Box<Root<T, O>>, rhs: Box<Root<T, O>>, height: usize) -> Self {
+        Self {
+            len: lhs.len() + rhs.len(),
+            summary: O::op(lhs.summary(), rhs.summary()),
+            height,
+            left: lhs,
+            right: rhs,
+            __marker: PhantomData,
+        }
+    }
+    pub fn update(&mut self) {
+        self.len = self.left.len() + self.right.len();
+        self.summary = O::op(self.left.summary(), self.right.summary());
+    }
+}
+impl<T, O: Op<Value = T>> Root<T, O> {
+    pub fn len(&self) -> usize {
+        match self {
+            Self::Nil(_) => 1,
+            Self::Node(node) => node.len,
+        }
+    }
+    pub fn fold(&self, start: usize, end: usize) -> O::Summary {
+        debug_assert!(start < end && end <= self.len());
+        match self {
+            Self::Nil(Nil(x)) => O::summarize(x),
+            Self::Node(node) => {
+                let lsize = node.left.len();
+                if start == 0 && end == self.len() {
+                    node.summary.clone()
+                } else if end <= lsize {
+                    node.left.fold(start, end)
+                } else if lsize <= start {
+                    node.right.fold(start - lsize, end - lsize)
+                } else {
+                    O::op(
+                        node.left.fold(start, lsize),
+                        node.right.fold(0, end - lsize),
+                    )
+                }
+            }
+        }
+    }
+    pub fn split(self, i: usize) -> [Self; 2] {
+        let node = self.into_node().unwrap();
+        let left_len = node.left.len();
+        match i.cmp(&left_len) {
+            Ordering::Equal => [*node.left, *node.right],
+            Ordering::Less => {
+                let [l, r] = node.left.split(i);
+                [l, Self::merge(r, *node.right)]
+            }
+            Ordering::Greater => {
+                let [l, r] = node.right.split(i - left_len);
+                [Self::merge(*node.left, l), r]
+            }
+        }
+    }
+    pub fn singleton(x: T) -> Self {
+        Self::Nil(Nil(x))
+    }
+    pub fn merge(mut lhs: Self, mut rhs: Self) -> Self {
+        let h = lhs.height();
+        match h.cmp(&rhs.height()) {
+            Ordering::Equal => Self::Node(Node::new(Box::new(lhs), Box::new(rhs), h + 1)),
+            Ordering::Less => {
+                rhs.merge_front(lhs);
+                rhs
+            }
+            Ordering::Greater => {
+                lhs.merge_back(rhs);
+                lhs
+            }
+        }
+    }
+    pub fn merge_front(&mut self, other: Self) {
+        let h = self.height();
+        if other.height() == self.node().unwrap().left.height() {
+            let Node { left, .. } = self.node_mut().unwrap();
+            replace_with(left, |left| {
+                Box::new(Self::Node(Node::new(Box::new(other), left, h)))
+            });
+        } else {
+            self.node_mut().unwrap().left.merge_front(other);
+        }
+        if self.node().unwrap().left.node().unwrap().left.height() == h {
+            if self.node().unwrap().right.height() == h {
+                self.node_mut().unwrap().height += 1;
+            } else {
+                let Node { left, right, .. } = self.node_mut().unwrap();
+                swap(left, right);
+                let l = left;
+                let Node { left, right, .. } = right.node_mut().unwrap();
+                swap(l, left);
+                swap(left, right);
+                self.node_mut().unwrap().right.node_mut().unwrap().update();
+            }
+        }
+        self.node_mut().unwrap().update();
+    }
+    pub fn merge_back(&mut self, other: Self) {
+        let h = self.height();
+        if other.height() == self.node().unwrap().right.height() {
+            let Node { right, .. } = self.node_mut().unwrap();
+            replace_with(right, |right| {
+                Box::new(Self::Node(Node::new(right, Box::new(other), h)))
+            });
+        } else {
+            self.node_mut().unwrap().right.merge_back(other);
+        }
+        if self.node().unwrap().right.node().unwrap().right.height() == h {
+            if self.node().unwrap().left.height() == h {
+                self.node_mut().unwrap().height += 1;
+            } else {
+                let Node { left, right, .. } = self.node_mut().unwrap();
+                swap(left, right);
+                let r = right;
+                let Node { left, right, .. } = left.node_mut().unwrap();
+                swap(right, r);
+                swap(left, right);
+                self.node_mut().unwrap().left.node_mut().unwrap().update();
+            }
+        }
+        self.node_mut().unwrap().update();
+    }
+    pub fn node(&self) -> Option<&Node<T, O>> {
+        match self {
+            Self::Nil(_) => None,
+            Self::Node(node) => Some(node),
+        }
+    }
+    pub fn node_mut(&mut self) -> Option<&mut Node<T, O>> {
+        match self {
+            Self::Nil(_) => None,
+            Self::Node(node) => Some(node),
+        }
+    }
+    fn into_node(self) -> Option<Node<T, O>> {
+        match self {
+            Self::Nil(_) => None,
+            Self::Node(node) => Some(node),
+        }
+    }
+    fn height(&self) -> usize {
+        match self {
+            Self::Nil(_) => 0,
+            Self::Node(node) => node.height,
+        }
+    }
+    pub fn summary(&self) -> O::Summary {
+        match self {
+            Self::Nil(Nil(x)) => O::summarize(x),
+            Self::Node(node) => node.summary.clone(),
+        }
+    }
+}
+
+fn replace_with<T, F: FnOnce(T) -> T>(dest: &mut T, f: F) {
+    unsafe { std::ptr::write(dest, f(std::ptr::read(dest))) }
 }
 
 impl<T: Clone, O: Op<Value = T>> Clone for Root<T, O>
@@ -96,154 +258,32 @@ where
     }
 }
 
-impl<T, O: Op<Value = T>> Node<T, O> {
-    pub fn new(lhs: Box<Root<T, O>>, rhs: Box<Root<T, O>>, height: usize) -> Self {
-        Self {
-            len: lhs.len() + rhs.len(),
-            summary: O::op(lhs.summary(), rhs.summary()),
-            height,
-            left: lhs,
-            right: rhs,
-            __marker: PhantomData,
-        }
-    }
-    pub fn update(&mut self) {
-        self.len = self.left.len() + self.right.len();
-        self.summary = O::op(self.left.summary(), self.right.summary());
-    }
-}
-impl<T, O: Op<Value = T>> Root<T, O> {
-    pub fn len(&self) -> usize {
-        match self {
-            Self::Nil(_) => 1,
-            Self::Node(node) => node.len,
-        }
-    }
-    pub fn fold(&self, start: usize, end: usize) -> O::Summary {
-        debug_assert!(start < end && end <= self.len());
-        match self {
-            Self::Nil(Nil(x)) => O::summarize(x),
-            Self::Node(node) => {
-                let lsize = node.left.len();
-                if start == 0 && end == self.len() {
-                    node.summary.clone()
-                } else if end <= lsize {
-                    node.left.fold(start, end)
-                } else if lsize <= start {
-                    node.right.fold(start - lsize, end - lsize)
-                } else {
-                    O::op(
-                        node.left.fold(start, lsize),
-                        node.right.fold(0, end - lsize),
-                    )
-                }
-            }
-        }
-    }
-    pub fn split(self, i: usize) -> [Self; 2] {
-        let node = self.into_node().unwrap();
-        let left_len = node.left.len();
-        match i.cmp(&left_len) {
-            Ordering::Equal => [*node.left, *node.right],
-            Ordering::Less => {
-                let [l, r] = node.left.split(i);
-                [l, Self::merge(r, *node.right)]
-            }
-            Ordering::Greater => {
-                let [l, r] = node.right.split(i - left_len);
-                [Self::merge(*node.left, l), r]
-            }
-        }
-    }
-    pub fn singleton(x: T) -> Self {
-        Self::Nil(Nil(x))
-    }
-    pub fn merge(lhs: Self, rhs: Self) -> Self {
-        let h = lhs.height();
-        match h.cmp(&rhs.height()) {
-            Ordering::Equal => Self::Node(Node::new(Box::new(lhs), Box::new(rhs), h + 1)),
-            Ordering::Less => rhs.merge_front(lhs),
-            Ordering::Greater => lhs.merge_back(rhs),
-        }
-    }
-    pub fn merge_front(self, other: Self) -> Self {
-        let mut node = self.into_node().unwrap();
-        let h = node.height;
-        node.left = Box::new(if node.left.height() == other.height() {
-            Self::Node(Node::new(Box::new(other), node.left, h))
-        } else {
-            node.left.merge_front(other)
-        });
-        if node.left.node().unwrap().left.height() == h {
-            if node.right.height() == h {
-                node.height += 1;
-            } else {
-                let left = node.left.into_node().unwrap();
-                node = Node::new(
-                    left.left,
-                    Box::new(Self::Node(Node::new(left.right, node.right, h))),
-                    h,
-                );
-            }
-        }
-        node.update();
-        Self::Node(node)
-    }
-    pub fn merge_back(self, other: Self) -> Self {
-        let mut node = self.into_node().unwrap();
-        let h = node.height;
-        node.right = Box::new(if node.right.height() == other.height() {
-            Self::Node(Node::new(node.right, Box::new(other), h))
-        } else {
-            node.right.merge_back(other)
-        });
-        if node.right.node().unwrap().right.height() == h {
-            if node.left.height() == h {
-                node.height += 1;
-            } else {
-                let right = node.right.into_node().unwrap();
-                node = Node::new(
-                    Box::new(Self::Node(Node::new(node.left, right.left, h))),
-                    right.right,
-                    h,
-                );
-            }
-        }
-        node.update();
-        Self::Node(node)
-    }
-    pub fn node(&self) -> Option<&Node<T, O>> {
-        match self {
-            Self::Nil(_) => None,
-            Self::Node(node) => Some(node),
-        }
-    }
-    fn into_node(self) -> Option<Node<T, O>> {
-        match self {
-            Self::Nil(_) => None,
-            Self::Node(node) => Some(node),
-        }
-    }
-    fn height(&self) -> usize {
-        match self {
-            Self::Nil(_) => 0,
-            Self::Node(node) => node.height,
-        }
-    }
-    pub fn summary(&self) -> O::Summary {
-        match self {
-            Self::Nil(Nil(x)) => O::summarize(x),
-            Self::Node(node) => node.summary.clone(),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use {
-        super::{Nil, Node, Root},
+        super::{Nil, Node, Op, Root},
         test_case::test_case,
     };
+
+    fn to_structure_sring<T, O: Op<Value = T>>(root: &Root<T, O>) -> String {
+        let mut s = String::new();
+        to_structure_sring_dfs(root, &mut s);
+        s
+    }
+    fn to_structure_sring_dfs<T, O: Op<Value = T>>(root: &Root<T, O>, s: &mut String) {
+        s.push('(');
+        match root {
+            Root::Nil(_) => (),
+            Root::Node(node) => {
+                to_structure_sring_dfs(&node.left, s);
+                s.push_str(&node.height.to_string());
+                s.push(',');
+                s.push_str(&node.len.to_string());
+                to_structure_sring_dfs(&node.right, s);
+            }
+        }
+        s.push(')');
+    }
 
     fn validate(root: &Root<()>) {
         if let Some(node) = root.node() {
@@ -258,26 +298,6 @@ mod tests {
             validate(&node.left);
             validate(&node.right);
         }
-    }
-
-    fn to_structure_sring(root: &Root<()>) -> String {
-        fn dfs(root: &Root<()>, s: &mut String) {
-            s.push('(');
-            match root {
-                Root::Nil(_) => (),
-                Root::Node(node) => {
-                    dfs(&node.left, s);
-                    s.push_str(&node.height.to_string());
-                    s.push(',');
-                    s.push_str(&node.len.to_string());
-                    dfs(&node.right, s);
-                }
-            }
-            s.push(')');
-        }
-        let mut s = String::new();
-        dfs(root, &mut s);
-        s
     }
 
     fn nil() -> Root<()> {
@@ -307,9 +327,9 @@ mod tests {
         y: impl Fn() -> Root<()>,
         z: impl Fn() -> Root<()>,
         w: impl Fn() -> Root<()>,
-        h: usize,
+        height: usize,
     ) -> impl Fn() -> Root<()> {
-        n2(n2(x, y, h), n2(z, w, h), h)
+        n2(n2(x, y, height), n2(z, w, height), height)
     }
     fn n2nil() -> Root<()> {
         n2(nil, nil, 1)()
