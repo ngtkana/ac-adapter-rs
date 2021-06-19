d34604109c9e80db8014c24106237d56d341e861
Kana <ngtkana@gmail.com>
2021-06-19 19:20:42 +0900

    Add docs to binary.

--- crates/utils/binary/src/lib.rs
+++ crates/utils/binary/src/lib.rs
@@ -1,9 +1,26 @@
+//! 二分法をします。
+
+/// 指数部分に使うためのトレイトです。すべての符号なし整数型に実装されています。
 pub trait Pow {
+    /// `*x != 0`
     fn is_nonzero(&self) -> bool;
+    /// `x & 1 == 1`
     fn is_odd(&self) -> bool;
-    fn div2(&mut self);
+    /// `self >>= 1`
+    fn shr1(&mut self);
 }
 
+/// 二分法により、`a` を `b` 個 `f` したものを計算します。
+///
+/// # Requirements
+///
+/// * `f` が結合的で `init` がその単位元（もしくは `a` の非負冪の上で成り立っていればよいです。）
+///
+///
+/// # Returns
+///
+/// * `a` を `b` 個 `f` したもの。
+///
 pub fn binary<T>(mut a: T, mut b: impl Pow, init: T, f: impl Fn(&T, &T) -> T) -> T {
     let mut ans = init;
     while b.is_nonzero() {
@@ -11,7 +28,7 @@ pub fn binary<T>(mut a: T, mut b: impl Pow, init: T, f: impl Fn(&T, &T) -> T) ->
             ans = f(&ans, &a);
         }
         a = f(&a, &a);
-        b.div2();
+        b.shr1();
     }
     ans
 }
@@ -23,10 +40,10 @@ macro_rules! impl_pow {
                 *self != 0
             }
             fn is_odd(&self) -> bool {
-                self.rem_euclid(2) == 1
+                self & 1 == 1
             }
-            fn div2(&mut self) {
-                *self = self.div_euclid(2);
+            fn shr1(&mut self) {
+                *self >>= 1;
             }
         }
     )*}
@@ -34,7 +51,6 @@ macro_rules! impl_pow {
 
 impl_pow! {
     u8, u16, u32, u64, u128, usize,
-    i8, i16, i32, i64, i128, isize,
 }
 
 #[cfg(test)]
@@ -47,7 +63,7 @@ mod tests {
     #[test_case(3 => "abcabcabc".to_owned())]
     #[test_case(4 => "abcabcabcabc".to_owned())]
     #[test_case(5 => "abcabcabcabcabc".to_owned())]
-    fn test_cat(n: i32) -> String {
+    fn test_cat(n: u32) -> String {
         binary("abc".to_owned(), n, String::new(), |s, t| {
             s.chars().chain(t.chars()).collect::<String>()
         })
