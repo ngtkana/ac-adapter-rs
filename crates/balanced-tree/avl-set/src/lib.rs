1fd6039337eb18cf0638903920bb320b0b10a755
Kana <ngtkana@gmail.com>
2021-07-11 15:10:10 +0900

    Add balancing algorithm to avlttree.

--- crates/balanced-tree/avl-set/src/lib.rs
+++ crates/balanced-tree/avl-set/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     cmp::Ordering,
     fmt::{Debug, DebugSet},
-    mem::{replace, take},
+    mem::{replace, swap, take},
 };
 
 #[derive(Clone, Hash, PartialEq)]
@@ -100,8 +100,35 @@ impl<T> Avltree<T> {
     }
     fn update(&mut self) {
         if let Some(node) = &mut self.0 {
+            let d = node.child[0].ht() as isize - node.child[1].ht() as isize;
+            if 1 < d {
+                let [a, b] = take(&mut node.child[0].0.as_mut().unwrap().child);
+                let c = take(&mut node.child[1]);
+                node.child.swap(0, 1);
+                swap(
+                    &mut node.value,
+                    &mut node.child[1].0.as_mut().unwrap().value,
+                );
+                node.child[0] = a;
+                node.child[1].0.as_mut().unwrap().child = [b, c];
+                node.child[1].update();
+            } else if d < -1 {
+                let a = take(&mut node.child[0]);
+                let [b, c] = take(&mut node.child[1].0.as_mut().unwrap().child);
+                node.child.swap(0, 1);
+                swap(
+                    &mut node.value,
+                    &mut node.child[0].0.as_mut().unwrap().value,
+                );
+                node.child[0].0.as_mut().unwrap().child = [a, b];
+                node.child[1] = c;
+                node.child[0].update();
+            }
             node.update();
         }
+    }
+    fn ht(&self) -> usize {
+        self.0.as_ref().map_or(0, |node| node.ht)
     }
     fn fmt_impl(&self, debug_set: &mut DebugSet<'_, '_>)
     where
@@ -143,6 +170,7 @@ impl<T: Debug> Debug for Avltree<T> {
 
 #[derive(Clone, Hash, PartialEq)]
 pub struct Node<T> {
+    ht: usize,
     len: usize,
     value: T,
     child: [Box<Avltree<T>>; 2],
@@ -150,12 +178,14 @@ pub struct Node<T> {
 impl<T> Node<T> {
     fn new(value: T) -> Self {
         Self {
+            ht: 1,
             len: 1,
             value,
             child: [Box::new(Avltree::new()), Box::new(Avltree::new())],
         }
     }
     fn update(&mut self) {
+        self.ht = self.child.iter().map(|child| child.ht()).max().unwrap() + 1;
         self.len = self.child.iter().map(|child| child.len()).sum::<usize>() + 1;
     }
 }
@@ -165,8 +195,118 @@ mod tests {
     use {
         super::Avltree,
         rand::{prelude::StdRng, Rng, SeedableRng},
+        std::fmt::Debug,
         superslice::Ext,
     };
+
+    fn describe<T: Debug>(avl: &Avltree<T>) -> String {
+        fn dfs<T: Debug>(avl: &Avltree<T>, s: &mut String) {
+            if let Some(node) = avl.0.as_ref() {
+                s.push('(');
+                dfs(&node.child[0], s);
+                s.push_str(&format!("{:?}", &node.value));
+                dfs(&node.child[1], s);
+                s.push(')');
+            }
+        }
+        let mut s = String::new();
+        dfs(avl, &mut s);
+        s
+    }
+
+    #[test]
+    fn test_structure_biased_ascending() {
+        let mut avl = Avltree::<u32>::new();
+        assert_eq!(describe(&avl).as_str(), "");
+        avl.insert(0);
+        assert_eq!(describe(&avl).as_str(), "(0)");
+        avl.insert(1);
+        assert_eq!(describe(&avl).as_str(), "(0(1))");
+        avl.insert(2);
+        assert_eq!(describe(&avl).as_str(), "((0)1(2))");
+        avl.insert(3);
+        assert_eq!(describe(&avl).as_str(), "((0)1(2(3)))");
+        avl.insert(4);
+        assert_eq!(describe(&avl).as_str(), "((0)1((2)3(4)))");
+        avl.insert(5);
+        assert_eq!(describe(&avl).as_str(), "(((0)1(2))3(4(5)))");
+        avl.insert(6);
+        assert_eq!(describe(&avl).as_str(), "(((0)1(2))3((4)5(6)))");
+        avl.insert(7);
+        assert_eq!(describe(&avl).as_str(), "(((0)1(2))3((4)5(6(7))))");
+        avl.insert(8);
+        assert_eq!(describe(&avl).as_str(), "(((0)1(2))3((4)5((6)7(8))))");
+        avl.insert(9);
+        assert_eq!(describe(&avl).as_str(), "(((0)1(2))3(((4)5(6))7(8(9))))");
+        avl.insert(10);
+        assert_eq!(
+            describe(&avl).as_str(),
+            "(((0)1(2))3(((4)5(6))7((8)9(10))))"
+        );
+        avl.insert(11);
+        assert_eq!(
+            describe(&avl).as_str(),
+            "((((0)1(2))3((4)5(6)))7((8)9(10(11))))"
+        );
+    }
+
+    #[test]
+    fn test_structure_biased_descending() {
+        struct Reverse(u32);
+        impl PartialEq for Reverse {
+            fn eq(&self, other: &Self) -> bool {
+                self.0 == other.0
+            }
+        }
+        impl Eq for Reverse {}
+        impl PartialOrd for Reverse {
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                Some(self.cmp(&other))
+            }
+        }
+        impl Ord for Reverse {
+            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+                self.0.cmp(&other.0).reverse()
+            }
+        }
+        impl Debug for Reverse {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                self.0.fmt(f)
+            }
+        }
+        let mut avl = Avltree::<Reverse>::new();
+        assert_eq!(describe(&avl).as_str(), "");
+        avl.insert(Reverse(0));
+        assert_eq!(describe(&avl).as_str(), "(0)");
+        avl.insert(Reverse(1));
+        assert_eq!(describe(&avl).as_str(), "((1)0)");
+        avl.insert(Reverse(2));
+        assert_eq!(describe(&avl).as_str(), "((2)1(0))");
+        avl.insert(Reverse(3));
+        assert_eq!(describe(&avl).as_str(), "(((3)2)1(0))");
+        avl.insert(Reverse(4));
+        assert_eq!(describe(&avl).as_str(), "(((4)3(2))1(0))");
+        avl.insert(Reverse(5));
+        assert_eq!(describe(&avl).as_str(), "(((5)4)3((2)1(0)))");
+        avl.insert(Reverse(6));
+        assert_eq!(describe(&avl).as_str(), "(((6)5(4))3((2)1(0)))");
+        avl.insert(Reverse(7));
+        assert_eq!(describe(&avl).as_str(), "((((7)6)5(4))3((2)1(0)))");
+        avl.insert(Reverse(8));
+        assert_eq!(describe(&avl).as_str(), "((((8)7(6))5(4))3((2)1(0)))");
+        avl.insert(Reverse(9));
+        assert_eq!(describe(&avl).as_str(), "((((9)8)7((6)5(4)))3((2)1(0)))");
+        avl.insert(Reverse(10));
+        assert_eq!(
+            describe(&avl).as_str(),
+            "((((10)9(8))7((6)5(4)))3((2)1(0)))"
+        );
+        avl.insert(Reverse(11));
+        assert_eq!(
+            describe(&avl).as_str(),
+            "((((11)10)9(8))7(((6)5(4))3((2)1(0))))"
+        );
+    }
 
     #[test]
     fn test_rand() {
@@ -175,6 +315,9 @@ mod tests {
         impl Brute {
             fn new() -> Self {
                 Self(Vec::new())
+            }
+            fn len(&self) -> usize {
+                self.0.len()
             }
             fn insert(&mut self, x: i32) -> Option<usize> {
                 if self.0.binary_search(&x).is_err() {
@@ -207,47 +350,58 @@ mod tests {
 
         let mut rng = StdRng::seed_from_u64(42);
         for _ in 0..20 {
+            const A: i32 = 40;
             const Q: usize = 200;
             let mut fast = Avltree::new();
             let mut brute = Brute::new();
             for _ in 0..Q {
-                match rng.gen_range(0..5) {
+                match rng.gen_range(0..6) {
                     // insert
                     0 => {
-                        let x = rng.gen_range(0..20);
+                        let result = fast.len();
+                        let expected = brute.len();
+                        assert_eq!(result, expected);
+                    }
+                    // insert
+                    1 => {
+                        let x = rng.gen_range(0..A);
                         let result = fast.insert(x);
                         let expected = brute.insert(x);
                         assert_eq!(result, expected);
                     }
                     // delete
-                    1 => {
-                        let x = rng.gen_range(0..20);
+                    2 => {
+                        let x = rng.gen_range(0..A);
                         let result = fast.delete(&x);
                         let expected = brute.delete(&x);
                         assert_eq!(result, expected);
                     }
                     // lower_bound
-                    2 => {
-                        let x = rng.gen_range(0..20);
+                    3 => {
+                        let x = rng.gen_range(0..=A);
                         let result = fast.lower_bound(&x);
                         let expected = brute.lower_bound(&x);
                         assert_eq!(result, expected);
                     }
                     // upper_bound
-                    3 => {
-                        let x = rng.gen_range(0..20);
+                    4 => {
+                        let x = rng.gen_range(0..=A);
                         let result = fast.upper_bound(&x);
                         let expected = brute.upper_bound(&x);
                         assert_eq!(result, expected);
                     }
                     // collect_vec
-                    4 => {
+                    5 => {
                         let result = fast.collect_vec();
                         let expected = brute.collect_vec();
                         assert_eq!(result, expected);
                     }
                     _ => unreachable!(),
                 }
+                println!("{:?}", &fast);
+                let ht = fast.ht();
+                let ht_lim = fast.len().next_power_of_two().trailing_zeros() as usize * 2 + 1;
+                assert!(ht <= ht_lim);
             }
         }
     }
