8ba837fff17d2ad5879be7a790c4ef7082135519
Kana <ngtkana@gmail.com>
2021-12-14 18:34:05 +0900

    add from_iter to new_avl_tree

--- crates/new_avl_tree/src/lib.rs
+++ crates/new_avl_tree/src/lib.rs
@@ -1,10 +1,83 @@
 use std::{fmt::Debug, mem::swap};
 
-struct Node<T: Debug> {
+pub struct AvlTree<T> {
+    root: Option<Box<Node<T>>>,
+}
+impl<T: Debug> AvlTree<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    // TODO: iterator
+    pub fn collect_vec(&self) -> Vec<T>
+    where
+        T: Clone,
+    {
+        fn collect_vec<T: Debug + Clone>(node: &Node<T>, out: &mut Vec<T>) {
+            if let Some(c) = node.child[0].as_ref() {
+                collect_vec(c, out);
+            }
+            out.push(node.value.clone());
+            if let Some(c) = node.child[1].as_ref() {
+                collect_vec(c, out);
+            }
+        }
+        let mut out = Vec::new();
+        if let Some(node) = self.root.as_ref() {
+            collect_vec(node, &mut out);
+        }
+        out
+    }
+}
+
+impl<T: Debug> Default for AvlTree<T> {
+    fn default() -> Self {
+        Self { root: None }
+    }
+}
+// TODO: not using vector
+impl<T: Debug> FromIterator<T> for AvlTree<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        fn from_slice_of_nodes<T: Debug>(
+            nodes: &mut [Option<Box<Node<T>>>],
+        ) -> Option<Box<Node<T>>> {
+            if nodes.is_empty() {
+                None
+            } else {
+                let i = nodes.len() / 2;
+                Some(merge_with_root(
+                    [
+                        from_slice_of_nodes(&mut nodes[..i]),
+                        from_slice_of_nodes(&mut nodes[i + 1..]),
+                    ],
+                    nodes[i].take().unwrap(),
+                ))
+            }
+        }
+        Self {
+            root: from_slice_of_nodes(
+                iter.into_iter()
+                    .map(new)
+                    .map(Some)
+                    .collect::<Vec<_>>()
+                    .as_mut_slice(),
+            ),
+        }
+    }
+}
+
+struct Node<T> {
     child: [Option<Box<Self>>; 2],
     value: T,
     len: usize,
     ht: u8,
+}
+fn new<T: Debug>(value: T) -> Box<Node<T>> {
+    Box::new(Node {
+        child: [None, None],
+        value,
+        len: 1,
+        ht: 1,
+    })
 }
 impl<T: Debug> Node<T> {
     fn update(&mut self) {
@@ -77,5 +150,19 @@ fn split_delete_by<T: Debug>(
         tmp[1 - e] = sub[e].take();
         sub[e] = Some(merge_with_root(tmp, root));
         (sub, center, res)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_iter() {
+        for n in 0..=10 {
+            let result = (0..n).collect::<AvlTree<_>>().collect_vec();
+            let expected = (0..n).collect::<Vec<_>>();
+            assert_eq!(result, expected);
+        }
     }
 }
