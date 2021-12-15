878cfbc0cfc57301c2780a0af640a9811aa6d3c1
Kana <ngtkana@gmail.com>
2021-12-15 17:40:15 +0900

    add binary_search_remove to new_avl_tree

--- crates/new_avl_tree/src/lib.rs
+++ crates/new_avl_tree/src/lib.rs
@@ -1,9 +1,9 @@
-use std::{borrow::Borrow, cmp::Ordering, fmt::Debug, iter::successors, mem::swap, ops::Index};
+use std::{borrow::Borrow, cmp::Ordering, iter::successors, mem::swap, ops::Index};
 
 pub struct AvlTree<T> {
     root: Option<Box<Node<T>>>,
 }
-impl<T: Debug> AvlTree<T> {
+impl<T> AvlTree<T> {
     pub fn new() -> Self {
         Self::default()
     }
@@ -13,8 +13,31 @@ impl<T: Debug> AvlTree<T> {
     pub fn len(&self) -> usize {
         len(&self.root)
     }
+    pub fn push_back(&mut self, value: T) {
+        self.append(&mut Self {
+            root: Some(new(value)),
+        })
+    }
+    pub fn push_front(&mut self, value: T) {
+        self.prepend(&mut Self {
+            root: Some(new(value)),
+        })
+    }
+    pub fn pop_back(&mut self) -> Option<T> {
+        let (mut sub, center, _res) = split_delete_by(self.root.take()?, |_| false);
+        self.root = sub[0].take();
+        Some(center.value)
+    }
+    pub fn pop_front(&mut self) -> Option<T> {
+        let (mut sub, center, _res) = split_delete_by(self.root.take()?, |_| true);
+        self.root = sub[1].take();
+        Some(center.value)
+    }
     pub fn append(&mut self, other: &mut Self) {
         self.root = merge([self.root.take(), other.root.take()]);
+    }
+    pub fn prepend(&mut self, other: &mut Self) {
+        self.root = merge([other.root.take(), self.root.take()]);
     }
     pub fn split_off_by(&mut self, mut is_right: impl FnMut(&T) -> bool) -> Self {
         let [left, right] = split_by(self.root.take(), |node| is_right(&node.value));
@@ -35,6 +58,11 @@ impl<T: Debug> AvlTree<T> {
             new(value),
         ));
     }
+    pub fn partition_point_insert(&mut self, is_right: impl FnMut(&T) -> bool, value: T) {
+        let mut right = self.split_off_by(is_right);
+        self.push_back(value);
+        self.append(&mut right);
+    }
     pub fn remove_at(&mut self, index: usize) -> T {
         assert!(index < self.len());
         let mut right = self.split_off_at(index + 1);
@@ -48,10 +76,11 @@ impl<T: Debug> AvlTree<T> {
     pub fn binary_search_by(&self, mut f: impl FnMut(&T) -> Ordering) -> Result<usize, usize> {
         binary_search_by(&self.root, |node| f(&node.value)).map(|(_node, index)| index)
     }
-    pub fn binary_search_by_key<B>(&self, b: &B, mut f: impl FnMut(&T) -> B) -> Result<usize, usize>
-    where
-        B: Ord,
-    {
+    pub fn binary_search_by_key<B: Ord>(
+        &self,
+        b: &B,
+        mut f: impl FnMut(&T) -> B,
+    ) -> Result<usize, usize> {
         self.binary_search_by(|x| f(x).cmp(&b))
     }
     pub fn binary_search<Q>(&self, value: &Q) -> Result<usize, usize>
@@ -60,6 +89,38 @@ impl<T: Debug> AvlTree<T> {
         Q: Ord,
     {
         self.binary_search_by(|x| x.borrow().cmp(&value))
+    }
+    pub fn binary_search_remove_by(
+        &mut self,
+        mut f: impl FnMut(&T) -> Ordering,
+    ) -> Result<(T, usize), usize> {
+        let mut right = self.split_off_by(|x| f(x) != Ordering::Less);
+        let index = self.len();
+        let (mut sub, center, _res) = split_delete_by(right.root.take().ok_or(index)?, |_| true);
+        if f(&center.value) == Ordering::Equal {
+            self.append(&mut Self {
+                root: sub[1].take(),
+            });
+            Ok((center.value, index))
+        } else {
+            sub[0] = self.root.take();
+            self.root = Some(merge_with_root(sub, center));
+            Err(index)
+        }
+    }
+    pub fn binary_search_remove_by_key<B: Ord>(
+        &mut self,
+        b: &B,
+        mut f: impl FnMut(&T) -> B,
+    ) -> Result<(T, usize), usize> {
+        self.binary_search_remove_by(|x| f(x).cmp(&b))
+    }
+    pub fn binary_search_remove<Q>(&mut self, value: &Q) -> Result<(T, usize), usize>
+    where
+        T: Borrow<Q>,
+        Q: Ord,
+    {
+        self.binary_search_remove_by(|x| x.borrow().cmp(&value))
     }
     pub fn iter(&self) -> Iter<'_, T> {
         Iter {
@@ -71,23 +132,20 @@ impl<T: Debug> AvlTree<T> {
     }
 }
 
-impl<T: Debug> Default for AvlTree<T> {
+impl<T> Default for AvlTree<T> {
     fn default() -> Self {
         Self { root: None }
     }
 }
-impl<T: Debug> Index<usize> for AvlTree<T> {
+impl<T> Index<usize> for AvlTree<T> {
     type Output = T;
     fn index(&self, index: usize) -> &Self::Output {
         self.get(index).unwrap()
     }
 }
-// TODO: not using vector
-impl<T: Debug> FromIterator<T> for AvlTree<T> {
+impl<T> FromIterator<T> for AvlTree<T> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-        fn from_slice_of_nodes<T: Debug>(
-            nodes: &mut [Option<Box<Node<T>>>],
-        ) -> Option<Box<Node<T>>> {
+        fn from_slice_of_nodes<T>(nodes: &mut [Option<Box<Node<T>>>]) -> Option<Box<Node<T>>> {
             if nodes.is_empty() {
                 None
             } else {
@@ -116,7 +174,7 @@ impl<T: Debug> FromIterator<T> for AvlTree<T> {
 pub struct Iter<'a, T> {
     stack: Vec<&'a Node<T>>,
 }
-impl<'a, T: Debug> Iterator for Iter<'a, T> {
+impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
         let last = self.stack.pop()?;
@@ -137,7 +195,7 @@ struct Node<T> {
     len: usize,
     ht: u8,
 }
-fn new<T: Debug>(value: T) -> Box<Node<T>> {
+fn new<T>(value: T) -> Box<Node<T>> {
     Box::new(Node {
         child: [None, None],
         value,
@@ -145,7 +203,7 @@ fn new<T: Debug>(value: T) -> Box<Node<T>> {
         ht: 1,
     })
 }
-impl<T: Debug> Node<T> {
+impl<T> Node<T> {
     fn update(&mut self) {
         self.len = len(&self.child[0]) + 1 + len(&self.child[1]);
         self.ht = 1 + ht(&self.child[0]).max(ht(&self.child[1]));
@@ -154,14 +212,14 @@ impl<T: Debug> Node<T> {
         ht(&self.child[e]) as isize - ht(&self.child[1 - e]) as isize
     }
 }
-fn len<T: Debug>(tree: &Option<Box<Node<T>>>) -> usize {
+fn len<T>(tree: &Option<Box<Node<T>>>) -> usize {
     tree.as_ref().map_or(0, |node| node.len)
 }
-fn ht<T: Debug>(tree: &Option<Box<Node<T>>>) -> u8 {
+fn ht<T>(tree: &Option<Box<Node<T>>>) -> u8 {
     tree.as_ref().map_or(0, |node| node.ht)
 }
-fn balance<T: Debug>(tree: &mut Box<Node<T>>) {
-    fn rotate<T: Debug>(tree: &mut Box<Node<T>>, e: usize) {
+fn balance<T>(tree: &mut Box<Node<T>>) {
+    fn rotate<T>(tree: &mut Box<Node<T>>, e: usize) {
         let mut x = tree.child[e].take().unwrap();
         let y = x.child[1 - e].take();
         swap(tree, &mut x);
@@ -181,7 +239,7 @@ fn balance<T: Debug>(tree: &mut Box<Node<T>>) {
         }
     }
 }
-fn merge_with_root<T: Debug>(
+fn merge_with_root<T>(
     mut sub: [Option<Box<Node<T>>>; 2],
     mut center: Box<Node<T>>,
 ) -> Box<Node<T>> {
@@ -200,7 +258,7 @@ fn merge_with_root<T: Debug>(
     center.update();
     center
 }
-fn merge<T: Debug>(mut sub: [Option<Box<Node<T>>>; 2]) -> Option<Box<Node<T>>> {
+fn merge<T>(mut sub: [Option<Box<Node<T>>>; 2]) -> Option<Box<Node<T>>> {
     match sub[1].take() {
         None => sub[0].take(),
         Some(sub1) => {
@@ -211,7 +269,7 @@ fn merge<T: Debug>(mut sub: [Option<Box<Node<T>>>; 2]) -> Option<Box<Node<T>>> {
         }
     }
 }
-fn split_delete_by<T: Debug>(
+fn split_delete_by<T>(
     mut root: Box<Node<T>>,
     mut is_right: impl FnMut(&Node<T>) -> bool,
 ) -> ([Option<Box<Node<T>>>; 2], Box<Node<T>>, bool) {
@@ -230,7 +288,7 @@ fn split_delete_by<T: Debug>(
         (sub, center, res)
     }
 }
-fn split_by<T: Debug>(
+fn split_by<T>(
     mut tree: Option<Box<Node<T>>>,
     is_right: impl FnMut(&Node<T>) -> bool,
 ) -> [Option<Box<Node<T>>>; 2] {
@@ -247,7 +305,7 @@ fn split_by<T: Debug>(
         None => [None, None],
     }
 }
-fn split_at<T: Debug>(tree: Option<Box<Node<T>>>, mut index: usize) -> [Option<Box<Node<T>>>; 2] {
+fn split_at<T>(tree: Option<Box<Node<T>>>, mut index: usize) -> [Option<Box<Node<T>>>; 2] {
     split_by(tree, |node| {
         let current = len(&node.child[0]);
         let res = index <= current;
@@ -257,7 +315,7 @@ fn split_at<T: Debug>(tree: Option<Box<Node<T>>>, mut index: usize) -> [Option<B
         res
     })
 }
-fn binary_search_by<T: Debug>(
+fn binary_search_by<T>(
     tree: &Option<Box<Node<T>>>,
     mut f: impl FnMut(&Node<T>) -> Ordering,
 ) -> Result<(&Node<T>, usize), usize> {
@@ -274,7 +332,7 @@ fn binary_search_by<T: Debug>(
         Ordering::Greater => binary_search_by(&node.child[0], f),
     }
 }
-fn get<T: Debug>(tree: &Option<Box<Node<T>>>, mut index: usize) -> Option<&Node<T>> {
+fn get<T>(tree: &Option<Box<Node<T>>>, mut index: usize) -> Option<&Node<T>> {
     binary_search_by(tree, |node| {
         let current = len(&node.child[0]);
         let res = current.cmp(&index);
@@ -301,6 +359,55 @@ mod tests {
                 .collect::<Vec<_>>();
             let expected = (0..n).collect::<Vec<_>>();
             assert_eq!(result, expected);
+        }
+    }
+
+    #[test]
+    fn test_push_back() {
+        let mut result = AvlTree::new();
+        let mut expected = Vec::new();
+        for i in 0..10 {
+            result.push_back(i);
+            expected.push(i);
+            assert_eq!(&result.iter().copied().collect::<Vec<_>>(), &expected);
+        }
+    }
+
+    #[test]
+    fn test_push_front() {
+        let mut result = AvlTree::new();
+        let mut expected = Vec::new();
+        for i in 0..10 {
+            result.push_front(i);
+            expected.insert(0, i);
+            assert_eq!(&result.iter().copied().collect::<Vec<_>>(), &expected);
+        }
+    }
+
+    #[test]
+    fn test_pop_back() {
+        for n in 0..10 {
+            let mut result = (0..n).collect::<AvlTree<_>>();
+            let mut expected = (0..n).collect::<Vec<_>>();
+            assert_eq!(result.pop_back(), expected.pop());
+            assert_eq!(&result.iter().copied().collect::<Vec<_>>(), &expected);
+        }
+    }
+
+    #[test]
+    fn test_pop_front() {
+        for n in 0..10 {
+            let mut result = (0..n).collect::<AvlTree<_>>();
+            let mut expected = (0..n).collect::<Vec<_>>();
+            assert_eq!(
+                result.pop_front(),
+                if n == 0 {
+                    None
+                } else {
+                    Some(expected.remove(0))
+                }
+            );
+            assert_eq!(&result.iter().copied().collect::<Vec<_>>(), &expected);
         }
     }
 
@@ -359,6 +466,19 @@ mod tests {
     }
 
     #[test]
+    fn test_partition_point_insert() {
+        for n in 0..=10 {
+            for i in 0..=n {
+                let mut result = (0..n).collect::<AvlTree<_>>();
+                let mut expected = (0..n).collect::<Vec<_>>();
+                result.partition_point_insert(|&x| i <= x, n);
+                expected.insert(i, n);
+                assert_eq!(result.iter().copied().collect::<Vec<_>>(), expected);
+            }
+        }
+    }
+
+    #[test]
     fn test_remove_at() {
         for n in 0..=10 {
             for i in 0..n {
@@ -385,19 +505,6 @@ mod tests {
     }
 
     #[test]
-    fn test_index() {
-        for n in 0..=10 {
-            for i in 0..n {
-                let result = (0..n).collect::<AvlTree<_>>();
-                let expected = (0..n).collect::<Vec<_>>();
-                let result = result[i];
-                let expected = expected[i];
-                assert_eq!(result, expected);
-            }
-        }
-    }
-
-    #[test]
     fn test_binary_search() {
         for n in 0..=10 {
             for i in 0..=2 * n + 1 {
@@ -418,6 +525,47 @@ mod tests {
                 let expected = (1..=2 * n).step_by(2).collect::<Vec<_>>();
                 let result = result.binary_search_by_key(&(i * 10), |x| x * 10);
                 let expected = expected.binary_search_by_key(&(i * 10), |x| x * 10);
+                assert_eq!(result, expected);
+            }
+        }
+    }
+
+    #[test]
+    fn test_binary_search_remove() {
+        for n in 0..=10 {
+            for i in 0..=2 * n + 1 {
+                let mut result = (1..=2 * n).step_by(2).collect::<AvlTree<_>>();
+                let mut expected = (1..=2 * n).step_by(2).collect::<Vec<_>>();
+                let result = result.binary_search_remove(&i);
+                let expected = expected.binary_search(&i).map(|i| (expected.remove(i), i));
+                assert_eq!(result, expected);
+            }
+        }
+    }
+
+    #[test]
+    fn test_binary_search_remove_by_key() {
+        for n in 0..=10 {
+            for i in 0..=2 * n + 1 {
+                let mut result = (1..=2 * n).step_by(2).collect::<AvlTree<_>>();
+                let mut expected = (1..=2 * n).step_by(2).collect::<Vec<_>>();
+                let result = result.binary_search_remove_by_key(&(i * 10), |x| x * 10);
+                let expected = expected
+                    .binary_search_by_key(&(i * 10), |x| x * 10)
+                    .map(|i| (expected.remove(i), i));
+                assert_eq!(result, expected);
+            }
+        }
+    }
+
+    #[test]
+    fn test_index() {
+        for n in 0..=10 {
+            for i in 0..n {
+                let result = (0..n).collect::<AvlTree<_>>();
+                let expected = (0..n).collect::<Vec<_>>();
+                let result = result[i];
+                let expected = expected[i];
                 assert_eq!(result, expected);
             }
         }
