5d2f606c309f10ba7a919b2dda4fb6d1c07987c3
Kana <ngtkana@gmail.com>
2022-01-02 15:35:21 +0900

    add median_heap

--- crates/removable_heap/src/lib.rs
+++ crates/removable_heap/src/lib.rs
@@ -13,13 +13,204 @@
 //!
 //! - Yukicoder No.738 - 平らな農地
 //!   - 問題: <https://yukicoder.me/problems/no/738>
-//!   - 提出 (33 ms): <https://yukicoder.me/submissions/727723>
-//!   - 解法: メディアンで切って、[`RemovableHeap`] ２本とそれぞれの要素総和を管理します。
-//!     メディアン版ライブラリにしたいですが、きれいな方法が思いつかず……
+//!   - 提出 (38 ms): <https://yukicoder.me/submissions/727771>
+//!   - 難易度: ほぼ貼るだけ
 //!   - 制約: N, Q ≤ 100,000
+//!   - コメント: 固定個数中央値は、最初の時点で右、左、右、左と入れるか、
+//!   入れ終わったときに [`balance`](MedianHeap::balance) を呼ぶかがよいです。
 //!
 
-use std::{collections::BinaryHeap, fmt::Debug, hash::Hash, iter::FromIterator};
+use std::{
+    cmp::{Ordering, Reverse},
+    collections::BinaryHeap,
+    fmt::Debug,
+    hash::Hash,
+    iter::FromIterator,
+};
+
+pub trait Handler<T> {
+    fn push_left(&mut self, value: T);
+    fn pop_left(&mut self, value: T);
+    fn push_right(&mut self, value: T);
+    fn pop_right(&mut self, value: T);
+}
+struct Nop;
+impl<T> Handler<T> for Nop {
+    fn push_left(&mut self, _value: T) {}
+    fn pop_left(&mut self, _value: T) {}
+    fn push_right(&mut self, _value: T) {}
+    fn pop_right(&mut self, _value: T) {}
+}
+
+#[derive(Clone)]
+pub struct MedianHeap<T, H> {
+    left: RemovableHeap<T>,
+    right: RemovableHeap<Reverse<T>>,
+    handler: H,
+}
+impl<T, H> Debug for MedianHeap<T, H>
+where
+    T: Copy + Ord + Hash + Debug,
+    H: Handler<T>,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("MedianHeap")
+            .field("left", &self.left)
+            .field(
+                "right",
+                &self
+                    .right
+                    .collect_sorted_vec()
+                    .into_iter()
+                    .rev()
+                    .map(|rev| rev.0)
+                    .collect::<Vec<_>>(),
+            )
+            .finish()
+    }
+}
+impl<T> Default for MedianHeap<T, Nop>
+where
+    T: Copy + Ord + Hash,
+{
+    fn default() -> Self {
+        Self {
+            left: RemovableHeap::default(),
+            right: RemovableHeap::default(),
+            handler: Nop,
+        }
+    }
+}
+impl<T> MedianHeap<T, Nop>
+where
+    T: Copy + Ord + Hash,
+{
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+impl<T, H> MedianHeap<T, H>
+where
+    T: Copy + Ord + Hash,
+    H: Handler<T>,
+{
+    pub fn with_handler(handler: H) -> Self {
+        Self {
+            left: RemovableHeap::default(),
+            right: RemovableHeap::default(),
+            handler,
+        }
+    }
+    pub fn is_empty(&self) -> bool {
+        self.left.is_empty() && self.right.is_empty()
+    }
+    pub fn len(&self) -> usize {
+        self.left.len() + self.right.len()
+    }
+    pub fn left_len(&self) -> usize {
+        self.left.len()
+    }
+    pub fn right_len(&self) -> usize {
+        self.right.len()
+    }
+    pub fn push_left(&mut self, elm: T) {
+        self.handler.push_left(elm);
+        self.left.push(elm);
+        self.settle();
+    }
+    pub fn push_right(&mut self, elm: T) {
+        self.handler.push_right(elm);
+        self.right.push(Reverse(elm));
+        self.settle();
+    }
+    pub fn peek_left(&self) -> Option<T> {
+        self.left.peek()
+    }
+    pub fn peek_right(&self) -> Option<T> {
+        self.right.peek().map(|rev| rev.0)
+    }
+    pub fn pop_left(&mut self) -> Option<T> {
+        let ans = self.left.pop();
+        self.settle();
+        ans
+    }
+    pub fn pop_right(&mut self) -> Option<T> {
+        let ans = self.right.pop().map(|rev| rev.0);
+        self.settle();
+        ans
+    }
+    pub fn move_left(&mut self) {
+        let elm = self.right.pop().unwrap().0;
+        self.handler.pop_right(elm);
+        self.handler.push_left(elm);
+        self.left.push(elm);
+        self.settle();
+    }
+    pub fn move_right(&mut self) {
+        let elm = self.left.pop().unwrap();
+        self.handler.pop_left(elm);
+        self.handler.push_right(elm);
+        self.right.push(Reverse(elm));
+        self.settle();
+    }
+    pub fn remove_left_unchecked(&mut self, elm: T) {
+        if self.left.peek().map_or(false, |lmax| elm <= lmax) {
+            self.handler.pop_left(elm);
+            self.left.remove_unchecked(elm);
+            self.settle();
+        } else {
+            self.handler.pop_right(elm);
+            self.right.remove_unchecked(Reverse(elm));
+            self.settle();
+            self.move_right();
+        }
+    }
+    pub fn remove_right_unchecked(&mut self, elm: T) {
+        if self.left.peek().map_or(false, |lmax| elm <= lmax) {
+            self.handler.pop_left(elm);
+            self.left.remove_unchecked(elm);
+            self.settle();
+        } else {
+            self.handler.pop_right(elm);
+            self.right.remove_unchecked(Reverse(elm));
+            self.settle();
+            self.move_right();
+        }
+    }
+    pub fn balance(&mut self, mut f: impl FnMut(usize, usize) -> Ordering) {
+        loop {
+            match f(self.left_len(), self.right_len()) {
+                Ordering::Less => self.move_left(),
+                Ordering::Equal => break,
+                Ordering::Greater => self.move_right(),
+            }
+        }
+    }
+    pub fn handler(&self) -> &H {
+        &self.handler
+    }
+    pub fn collect_sorted_vec(&self) -> Vec<T> {
+        let mut left = self.left.collect_sorted_vec();
+        let right = self.right.collect_sorted_vec();
+        left.extend(right.into_iter().rev().map(|rev| rev.0));
+        left
+    }
+    fn settle(&mut self) {
+        while !self.left.is_empty()
+            && !self.right.is_empty()
+            && self.left.peek().unwrap() > self.right.peek().unwrap().0
+        {
+            let elm = self.right.pop().unwrap().0;
+            self.handler.pop_right(elm);
+            self.handler.push_left(elm);
+            self.left.push(elm);
+            let elm = self.left.pop().unwrap();
+            self.handler.pop_left(elm);
+            self.handler.push_right(elm);
+            self.right.push(Reverse(elm));
+        }
+    }
+}
 
 /// 本体です。
 #[derive(Clone)]
@@ -209,6 +400,41 @@ mod tests {
             }
             assert_eq!(&heap.collect_sorted_vec(), &sorted);
             assert_eq!(heap.len(), sorted.len());
+        }
+    }
+
+    #[test]
+    fn test_median_heap() {
+        let mut sorted = Vec::new();
+        let mut heap = MedianHeap::default();
+        let mut rng = StdRng::seed_from_u64(42);
+        for _ in 0..200 {
+            let x = rng.gen_range(0..10);
+            match rng.gen_range(0..2) {
+                0 => {
+                    sorted.push(x);
+                    sorted.sort_unstable();
+                    heap.push_left(x);
+                }
+                1 => {
+                    if let Ok(i) = sorted.binary_search(&x) {
+                        sorted.remove(i);
+                        heap.remove_left_unchecked(x);
+                    }
+                }
+                _ => unreachable!(),
+            }
+            dbg!(&heap, &sorted);
+            assert_eq!(&heap.collect_sorted_vec(), &sorted);
+            assert_eq!(heap.len(), sorted.len());
+
+            if !sorted.is_empty() {
+                let i = rng.gen_range(0..sorted.len());
+                let expected = sorted[i];
+                heap.balance(|l, _r| l.cmp(&i));
+                assert_eq!(heap.peek_right().unwrap(), expected);
+            }
+            dbg!(&heap, &sorted);
         }
     }
 }
