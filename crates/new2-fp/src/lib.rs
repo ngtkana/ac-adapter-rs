10dd8b32d4482f42dc438ea1e1c4f8bbe8743fe7
Kana <ngtkana@gmail.com>
2022-01-07 00:02:30 +0900

    finish new2_fp

--- crates/new2-fp/src/lib.rs
+++ crates/new2-fp/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     fmt::{Debug, Display},
     hash::Hash,
-    iter::{Product, Sum},
+    iter::{successors, Product, Sum},
     marker::PhantomData,
     mem::swap,
     ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
@@ -19,6 +19,12 @@ pub trait Mod {
 /// [`Mod`] を実装した型 `$m` と、型エイリアス `$f = Fp<$m>` を定義します。
 #[macro_export]
 macro_rules! define_fp {
+    ($mod:expr) => {
+        $crate::define_fp! { type $mod }
+    };
+    ($mod:expr, $f:ident, $m:ident) => {
+        $crate::define_fp! { type $mod, $f, $m }
+    };
     ($pub:vis type $mod:expr) => {
         $crate::define_fp! { $pub type $mod, F, M }
     };
@@ -39,7 +45,7 @@ macro_rules! define_fp {
 ///
 /// ```
 /// use new2_fp::{fp, define_fp};
-/// define_fp! { type 13 }
+/// define_fp!(13);
 ///
 /// // リテラル
 /// let a: F = fp!(6);
@@ -57,7 +63,7 @@ macro_rules! define_fp {
 ///
 /// ```
 /// use new2_fp::{fp, define_fp};
-/// define_fp! { type 13 }
+/// define_fp!(13);
 ///
 /// assert_eq!(fp!(2; 3), F::new(2) / F::new(3));
 /// ```
@@ -283,10 +289,10 @@ macro_rules! forward_ops {
                 self.$fn_assign(*rhs);
             }
         }
-        impl<M: Mod> $trait for Fp<M> {
+        impl<M: Mod, T: Into<Fp<M>>> $trait<T> for Fp<M> {
             type Output = Fp<M>;
-            fn $fn(mut self, rhs: Fp<M>) -> Self::Output {
-                self.$fn_assign(rhs);
+            fn $fn(mut self, rhs: T) -> Self::Output {
+                self.$fn_assign(rhs.into());
                 self
             }
         }
@@ -296,10 +302,10 @@ macro_rules! forward_ops {
                 self.$fn(*rhs)
             }
         }
-        impl<M: Mod> $trait<Fp<M>> for &Fp<M> {
+        impl<M: Mod, T: Into<Fp<M>>> $trait<T> for &Fp<M> {
             type Output = Fp<M>;
-            fn $fn(self, rhs: Fp<M>) -> Self::Output {
-                (*self).$fn(rhs)
+            fn $fn(self, rhs: T) -> Self::Output {
+                (*self).$fn(rhs.into())
             }
         }
         impl<M: Mod> $trait<&Fp<M>> for &Fp<M> {
@@ -337,10 +343,181 @@ impl<'a, M: Mod> Product<&'a Self> for Fp<M> {
     }
 }
 
+/// 階乗を順に返すイテレータを生成します。
+///
+/// # Examples
+///
+/// ```
+/// use new2_fp::{fact_iter, define_fp, fp};
+/// define_fp!(13);
+///
+/// let mut fact = fact_iter::<M>();
+/// assert_eq!(fact.next(), Some(fp!(1)));
+/// assert_eq!(fact.next(), Some(fp!(1)));
+/// assert_eq!(fact.next(), Some(fp!(2)));
+/// assert_eq!(fact.next(), Some(fp!(6)));
+/// assert_eq!(fact.next(), Some(fp!(24)));
+/// ```
+pub fn fact_iter<M: Mod>() -> impl Iterator<Item = Fp<M>> {
+    (1..).scan(Fp::new(1), |state, x| {
+        let ans = *state;
+        *state *= x;
+        Some(ans)
+    })
+}
+
+/// 階乗とその逆数を前計算します。
+///
+/// # Examples
+///
+/// ```
+/// use new2_fp::{fact_build, define_fp, fp};
+/// define_fp!(13);
+///
+/// let fact = fact_build::<M>(3);
+/// assert_eq!(fact.0[0], vec![fp!(1), fp!(1), fp!(2)]);
+/// assert_eq!(fact.0[1], vec![fp!(1), fp!(1), fp!(1; 2)]);
+/// ```
+///
+#[allow(clippy::missing_panics_doc)]
+pub fn fact_build<M: Mod>(n: usize) -> FactTable<M> {
+    FactTable(if n == 0 {
+        [Vec::new(), Vec::new()]
+    } else {
+        let fact = fact_iter::<M>().take(n).collect::<Vec<_>>();
+        let mut fact_inv = vec![fact.last().unwrap().inv(); n];
+        (1..n).rev().for_each(|i| fact_inv[i - 1] = fact_inv[i] * i);
+        [fact, fact_inv]
+    })
+}
+
+/// [`fact_build`] の戻り値型で、階乗とその逆数のテーブルを管理しています。
+///
+/// # Examples
+///
+/// ```
+/// use new2_fp::{fact_build, define_fp, fp};
+/// define_fp!(13);
+///
+/// let fact = fact_build::<M>(4);
+///
+/// assert_eq!(fact.0[0], vec![fp!(1), fp!(1), fp!(2), fp!(6)]);
+/// assert_eq!(fact.0[1], vec![fp!(1), fp!(1), fp!(1; 2), fp!(1; 6)]);
+/// ```
+///
+///
+#[derive(Clone, Debug, Default, Hash, PartialEq)]
+pub struct FactTable<M: Mod>(pub [Vec<Fp<M>>; 2]);
+impl<M: Mod> FactTable<M> {
+    /// k ≤ n < len ならば binom(n, k) を返します。さもなくばパニックします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use new2_fp::{fact_build, define_fp, fp};
+    /// define_fp!(13);
+    /// let fact = fact_build::<M>(5);
+    ///
+    /// assert_eq!(fact.binom(4, 0), fp!(1));
+    /// assert_eq!(fact.binom(4, 1), fp!(4));
+    /// assert_eq!(fact.binom(4, 2), fp!(6));
+    /// ```
+    pub fn binom(&self, n: usize, k: usize) -> Fp<M> {
+        assert!(n < self.0[0].len());
+        assert!(k <= n);
+        self.0[0][n] * self.0[1][k] * self.0[1][n - k]
+    }
+    /// i + j < len ならば binom(i + j, i) を返します。さもなくばパニックします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use new2_fp::{fact_build, define_fp, fp};
+    /// define_fp!(13);
+    /// let fact = fact_build::<M>(5);
+    ///
+    /// assert_eq!(fact.binom2(0, 4), fp!(1));
+    /// assert_eq!(fact.binom2(1, 3), fp!(4));
+    /// assert_eq!(fact.binom2(2, 2), fp!(6));
+    /// ```
+    pub fn binom2(&self, i: usize, j: usize) -> Fp<M> {
+        self.binom(i + j, i)
+    }
+    /// 0 ≤ k ≤ n < len ならば 1 / binom(n, k) を返します。さもなくばパニックします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use new2_fp::{fact_build, define_fp, fp};
+    /// define_fp!(13);
+    /// let fact = fact_build::<M>(5);
+    ///
+    /// assert_eq!(fact.binom_inv(4, 0), fp!(1; 1));
+    /// assert_eq!(fact.binom_inv(4, 1), fp!(1; 4));
+    /// assert_eq!(fact.binom_inv(4, 2), fp!(1; 6));
+    /// ```
+    pub fn binom_inv(&self, n: u64, k: u64) -> Fp<M> {
+        let n = n as usize;
+        let k = k as usize;
+        assert!(n < self.0[0].len());
+        assert!(k <= n);
+        self.0[1][n] * self.0[0][k] * self.0[0][n - k]
+    }
+    /// 0 ≤ n < len ならば binom(n, k) を返します。さもなくばパニックします。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use new2_fp::{fact_build, define_fp, fp};
+    /// define_fp!(13);
+    /// let fact = fact_build::<M>(5);
+    ///
+    /// // `binom` と同じ
+    /// assert_eq!(fact.binom_or_zero(4, 0), fp!(1));
+    /// assert_eq!(fact.binom_or_zero(4, 1), fp!(4));
+    /// assert_eq!(fact.binom_or_zero(4, 2), fp!(6));
+    ///
+    /// // `k` が範囲外なると `0` を返します。
+    /// assert_eq!(fact.binom_or_zero(4, 5), fp!(0));
+    /// assert_eq!(fact.binom_or_zero(4, -1), fp!(0));
+    /// ```
+    pub fn binom_or_zero(&self, n: usize, k: isize) -> Fp<M> {
+        assert!(n < self.0[0].len() as usize);
+        if (0..=n as isize).contains(&k) {
+            self.binom(n, k as usize)
+        } else {
+            Fp::new(0)
+        }
+    }
+}
+
+/// 二項係数 binom(n, k) を、n ごとにまとめて返すイテレータを生成します。
+///
+/// # Examples
+///
+/// ```
+/// use new2_fp::{binom_iter, define_fp, fp};
+/// define_fp!(13);
+///
+/// let mut binom = binom_iter::<M>();
+/// assert_eq!(binom.next(), Some(vec![fp!(1)]));
+/// assert_eq!(binom.next(), Some(vec![fp!(1), fp!(1)]));
+/// assert_eq!(binom.next(), Some(vec![fp!(1), fp!(2), fp!(1)]));
+/// assert_eq!(binom.next(), Some(vec![fp!(1), fp!(3), fp!(3), fp!(1)]));
+/// ```
+pub fn binom_iter<M: Mod>() -> impl Iterator<Item = Vec<Fp<M>>> {
+    successors(Some(vec![Fp::new(1)]), |last| {
+        let mut crr = last.clone();
+        crr.push(Fp::new(0));
+        crr[1..].iter_mut().zip(last).for_each(|(x, &y)| *x += y);
+        Some(crr)
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    define_fp! { type 13 }
+    define_fp!(13);
 
     #[test]
     #[allow(unused_imports)]
@@ -366,6 +543,12 @@ mod tests {
         assert_eq!(a, F::new(1));
         let mut a = F::new(6);
         a += &Fp::new(8);
+        assert_eq!(a, F::new(1));
+        let mut a = F::new(6);
+        a += 8;
+        assert_eq!(a, F::new(1));
+        let mut a = F::new(6);
+        a += 8_usize;
         assert_eq!(a, F::new(1));
     }
 
@@ -438,7 +621,7 @@ mod tests {
     #[test]
     #[allow(clippy::op_ref)]
     fn test_fmt() {
-        define_fp! { type 998244353 }
+        define_fp!(998244353);
         for num in 1..100_u64 {
             for den in 1..100_u64 {
                 let x: F = fp!(num; den);
