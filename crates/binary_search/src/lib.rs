4de131d547621b6e8a7ce4432e30a72507aeb304
Kana <ngtkana@gmail.com>
2022-01-14 00:52:57 +0900

    add exponential_search_f64g

--- crates/binary_search/src/lib.rs
+++ crates/binary_search/src/lib.rs
@@ -1,3 +1,81 @@
+use std::f64::{INFINITY, NEG_INFINITY};
+
+pub fn exponential_search_f64(mut f: impl FnMut(f64) -> bool) -> f64 {
+    let mut lower;
+    let mut upper;
+    if f(0.0) {
+        if !f(-1.0) {
+            lower = -1.0;
+            upper = -0.5;
+            while !f(upper) {
+                lower = upper;
+                upper *= -upper;
+                if upper == 0.0 {
+                    return 0.0;
+                }
+            }
+        } else {
+            lower = -2.0;
+            upper = -1.0;
+            while f(lower) {
+                upper = lower;
+                lower *= -lower;
+                if lower == NEG_INFINITY {
+                    return NEG_INFINITY;
+                }
+            }
+        }
+        while lower < upper * 2.0 {
+            let mid = lower * (upper / lower).sqrt();
+            if f(mid) {
+                upper = mid;
+            } else {
+                lower = mid;
+            }
+        }
+        debug_assert_eq!(lower, 2.0 * upper);
+    } else {
+        if f(1.0) {
+            lower = 0.5;
+            upper = 1.0;
+            while f(lower) {
+                upper = lower;
+                lower *= lower;
+            }
+        } else {
+            lower = 1.0;
+            upper = 2.0;
+            while !f(upper) {
+                lower = upper;
+                upper *= upper;
+                if upper == INFINITY {
+                    return INFINITY;
+                }
+            }
+        }
+        while lower * 2.0 < upper {
+            let mid = lower * (upper / lower).sqrt();
+            if f(mid) {
+                upper = mid;
+            } else {
+                lower = mid;
+            }
+        }
+        debug_assert_eq!(lower * 2.0, upper);
+    }
+    loop {
+        let mid = lower + (upper - lower) / 2.0;
+        if lower == mid || mid == upper {
+            return upper;
+        }
+        if f(mid) {
+            upper = mid;
+        } else {
+            lower = mid;
+        }
+    }
+}
+
 pub fn binary_search_u8(mut lower: u8, mut upper: u8, mut f: impl FnMut(u8) -> bool) -> u8 {
     assert!(lower < upper);
     assert!(!f(lower) && f(upper));
@@ -91,7 +169,11 @@ pub fn exponential_search_i8(mut f: impl FnMut(i8) -> bool) -> Option<i8> {
 mod tests {
     use super::*;
     use rand::{prelude::StdRng, Rng, SeedableRng};
-    use std::{collections::HashMap, mem::swap};
+    use std::{
+        collections::HashMap,
+        f64::{EPSILON, MAX, MIN_POSITIVE},
+        mem::swap,
+    };
 
     #[test]
     fn test_exponential_search_u8() {
@@ -111,6 +193,43 @@ mod tests {
         }
         let result = exponential_search_i8(|_| false);
         assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_exponential_search_f64() {
+        // let mut rng = StdRng::seed_from_u64(42);
+        // for _ in 0..2000 {
+        //     let expected = if rng.gen_bool(0.5) { 1.0 } else { -1.0 }
+        //         * 2_f64.powf(rng.gen_range(-512.0..512.0));
+        //     let mut count = 0;
+        //     let result = exponential_search_f64(|x| {
+        //         count += 1;
+        //         expected <= x
+        //     });
+        //     assert!(count <= 72);
+        //     assert_eq!(result, expected);
+        // }
+
+        assert_eq!(MIN_POSITIVE, MIN_POSITIVE);
+
+        for &(threshold, expected) in &[
+            // 正確に計算できるもの
+            (0.0, 0.0),
+            (-0.0, 0.0),
+            (INFINITY, INFINITY),
+            (NEG_INFINITY, NEG_INFINITY),
+            (EPSILON, EPSILON),
+            (MAX.sqrt(), MAX.sqrt()),
+            (-MAX.sqrt(), -MAX.sqrt()),
+            (MIN_POSITIVE, MIN_POSITIVE),
+            (-MIN_POSITIVE, -MIN_POSITIVE),
+            // できないもの
+            (MAX.sqrt() * 1.000000000001, INFINITY),
+            (-MAX.sqrt() * 1.000000000001, NEG_INFINITY),
+        ] {
+            let result = exponential_search_f64(|x| threshold <= x);
+            assert_eq!(result, expected);
+        }
     }
 
     #[test]
