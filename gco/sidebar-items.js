a84089635d4ce02d490f97c97eb3e91838e562fc
ngtkana <ngtkana@users.noreply.github.com>
2023-03-08 14:48:45 +0000

    deploy: a2704526fd68b116fab5cb098f26d0ef30dfee11

--- gco/sidebar-items.js
+++ gco/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["Gco","2 次以下の劣モジュラー Graph cut 最適化問題を解きます。"],["GcoResult","[`Gco::solve`] の返すオブジェクトです。"]]};
+window.SIDEBAR_ITEMS = {"struct":[["Gco","A solver of graph cut optimization problems."],["GcoResult","The minimum value and and an argmin of $f$."]]};
