a92e035388e4f907f019a6a633ca6c6a9741b833
ngtkana <ngtkana@users.noreply.github.com>
2023-03-30 13:08:43 +0000

    deploy: 6690822c55e8207dbcb848b6786b568f0253aebb

--- implementors/core/convert/trait.TryFrom.js
+++ implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"grid":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.1/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"grid/enum.Dihedral.html\" title=\"enum grid::Dihedral\">Dihedral</a>"]]
+"grid":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.68.1/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"grid/enum.Dihedral.html\" title=\"enum grid::Dihedral\">Dihedral</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
