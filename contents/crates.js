85220da26a115a2ba8e620e0e476aeeef99cf5be
ngtkana <ngtkana@gmail.com>
2023-04-02 17:17:59 +0900

    use js to generate crate list

---
+++ contents/crates.js
@@ -0,0 +1,245 @@
+const crates = [
+    {
+        "name": "accum"
+    },
+    {
+        "name": "advance"
+    },
+    {
+        "name": "avl_tree"
+    },
+    {
+        "name": "bfs"
+    },
+    {
+        "name": "bfs01"
+    },
+    {
+        "name": "binary"
+    },
+    {
+        "name": "bitutils"
+    },
+    {
+        "name": "bitvec"
+    },
+    {
+        "name": "box_segtree"
+    },
+    {
+        "name": "bsgs"
+    },
+    {
+        "name": "cht"
+    },
+    {
+        "name": "cipolla"
+    },
+    {
+        "name": "convex_hull"
+    },
+    {
+        "name": "dbg"
+    },
+    {
+        "name": "dfs"
+    },
+    {
+        "name": "dial"
+    },
+    {
+        "name": "dijkstra_radix_heap"
+    },
+    {
+        "name": "dinic"
+    },
+    {
+        "name": "dual_segtree"
+    },
+    {
+        "name": "elim"
+    },
+    {
+        "name": "erato"
+    },
+    {
+        "name": "euclid"
+    },
+    {
+        "name": "filter_position"
+    },
+    {
+        "name": "fp"
+    },
+    {
+        "name": "gco"
+    },
+    {
+        "name": "grid"
+    },
+    {
+        "name": "gridnei"
+    },
+    {
+        "name": "gss"
+    },
+    {
+        "name": "heap_slope_trick"
+    },
+    {
+        "name": "heap_tricks"
+    },
+    {
+        "name": "hld"
+    },
+    {
+        "name": "hopkarp"
+    },
+    {
+        "name": "hungarian"
+    },
+    {
+        "name": "inversion_number"
+    },
+    {
+        "name": "lagrange"
+    },
+    {
+        "name": "lazy_segbeats"
+    },
+    {
+        "name": "lazy_segtree"
+    },
+    {
+        "name": "lin_ineq"
+    },
+    {
+        "name": "low_link"
+    },
+    {
+        "name": "make_graph"
+    },
+    {
+        "name": "max_flow"
+    },
+    {
+        "name": "mincost_flow"
+    },
+    {
+        "name": "monotone_minima"
+    },
+    {
+        "name": "newton"
+    },
+    {
+        "name": "next_permutation"
+    },
+    {
+        "name": "ngtio"
+    },
+    {
+        "name": "not_nan"
+    },
+    {
+        "name": "numeric_search"
+    },
+    {
+        "name": "open"
+    },
+    {
+        "name": "ordtools"
+    },
+    {
+        "name": "partially_persistent_union_find"
+    },
+    {
+        "name": "poly"
+    },
+    {
+        "name": "prefix_sum"
+    },
+    {
+        "name": "radix_heap"
+    },
+    {
+        "name": "randtools"
+    },
+    {
+        "name": "rational"
+    },
+    {
+        "name": "rbtree"
+    },
+    {
+        "name": "reverse"
+    },
+    {
+        "name": "scc"
+    },
+    {
+        "name": "segbeats"
+    },
+    {
+        "name": "segbeats_task3"
+    },
+    {
+        "name": "segtree"
+    },
+    {
+        "name": "seq"
+    },
+    {
+        "name": "skew-heap"
+    },
+    {
+        "name": "sort_tree"
+    },
+    {
+        "name": "space"
+    },
+    {
+        "name": "span"
+    },
+    {
+        "name": "sparse_table"
+    },
+    {
+        "name": "splay_tree"
+    },
+    {
+        "name": "suffix_array"
+    },
+    {
+        "name": "swag"
+    },
+    {
+        "name": "tree_fold"
+    },
+    {
+        "name": "trial"
+    },
+    {
+        "name": "trie"
+    },
+    {
+        "name": "two_sat"
+    },
+    {
+        "name": "uf_checklist"
+    },
+    {
+        "name": "union_find"
+    },
+    {
+        "name": "vec_lines"
+    },
+    {
+        "name": "wavelet_matrix"
+    },
+    {
+        "name": "z_algorithm"
+    },
+    {
+        "name": "zeta"
+    }
+]
