b234de47069a8232d90b07186c23af466969ae39
ngtkana <ngtkana@gmail.com>
2023-04-05 21:29:51 +0900

    thin dbg

--- crates/dbg/src/lib.rs
+++ crates/dbg/src/lib.rs
@@ -1,30 +1,4 @@
-//! Provides print-debugging utilities.
-//!
-//! # Macros
-//!
-//! Prints arguments to STDERR with code position information.
-//!
-//! - [`lg`]
-//!
-//!
-//! # Structs
-//!
-//! * [`BitSlice`] to format a slice of Boolean vales. Implements both `Debug` and `Display`.
-//!
-//!
-//! # Functions
-//!
-//! * [`table()`] to format a two-dimensional table. Returns [`Table`], which implements `Debug`, and
-//! allows customization of formats via [`by`](Table::by) method.
-//!
-
-mod bitslice;
-mod table;
-
-pub use {
-    bitslice::BitSlice,
-    table::{table, Table},
-};
+//! Provides a macro [lg]
 
 #[macro_export]
 macro_rules! lg {
