fa875ce262e468d566d6b23edffbdd19f300df1f
ngtkana <ngtkana@gmail.com>
2023-05-01 00:28:54 +0900

    add input

---
+++ crates/input/src/lib.rs
@@ -0,0 +1,61 @@
+/// Provids utility on stdin.
+use std::{
+    cell::Cell,
+    convert::TryFrom,
+    io::{stdin, BufRead, BufReader, Lines, Stdin},
+    str::FromStr,
+    sync::{Mutex, Once},
+};
+
+type Server = Mutex<Lines<BufReader<Stdin>>>;
+static ONCE: Once = Once::new();
+pub struct Lazy(Cell<Option<Server>>);
+unsafe impl Sync for Lazy {}
+fn line() -> String {
+    static SYNCER: Lazy = Lazy(Cell::new(None));
+    ONCE.call_once(|| {
+        SYNCER
+            .0
+            .set(Some(Mutex::new(BufReader::new(stdin()).lines())));
+    });
+    unsafe {
+        (*SYNCER.0.as_ptr())
+            .as_ref()
+            .unwrap()
+            .lock()
+            .unwrap()
+            .next()
+            .unwrap()
+            .unwrap()
+    }
+}
+pub trait ForceFromStr: FromStr {
+    fn force_parse(s: &str) -> Self;
+}
+impl<T, E> ForceFromStr for T
+where
+    T: FromStr<Err = E>,
+    E: std::fmt::Debug,
+{
+    fn force_parse(s: &str) -> Self {
+        s.parse().unwrap()
+    }
+}
+/// Read a line from stdin and parse to [T; N].
+pub fn input_array<T: ForceFromStr, const N: usize>() -> [T; N]
+where
+    T: std::fmt::Debug,
+{
+    <[_; N]>::try_from(input_vec()).unwrap()
+}
+/// Read a line from stdin and parse to Vec<T>.
+pub fn input_vec<T: ForceFromStr>() -> Vec<T> {
+    line()
+        .split_whitespace()
+        .map(T::force_parse)
+        .collect::<Vec<_>>()
+}
+/// Read a line from stdin and parse to T.
+pub fn input<T: ForceFromStr>() -> T {
+    T::force_parse(&line())
+}
