942a91add8dbc0e4d79295adf2b31a5d32f5a642
ngtkana <ngtkana@users.noreply.github.com>
2023-09-28 19:40:22 +0000

    deploy: a201a8b897f11ebcfe96b348b0b060d8989c35f3

--- fps/sidebar-items.js
+++ fps/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["fps_inv","fps_sqrt"]};
+window.SIDEBAR_ITEMS = {"fn":["fps_deriv","fps_exp","fps_int","fps_inv","fps_log","fps_pow","fps_sqrt"],"macro":["fps"]};
--- implementors/core/str/traits/trait.FromStr.js
+++ implementors/core/str/traits/trait.FromStr.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rational":[["impl&lt;T: <a class=\"trait\" href=\"rational/trait.Signed.html\" title=\"trait rational::Signed\">Signed</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"rational/struct.Rational.html\" title=\"struct rational::Rational\">Rational</a>&lt;T&gt;"]]
+"rational":[["impl&lt;T: <a class=\"trait\" href=\"rational/trait.Signed.html\" title=\"trait rational::Signed\">Signed</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"rational/struct.Rational.html\" title=\"struct rational::Rational\">Rational</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
