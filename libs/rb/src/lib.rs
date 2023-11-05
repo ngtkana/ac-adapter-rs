b61082b305ffd8d16cd49183736854203d3847ca
Nagata Kana <ngtkana@gmail.com>
2023-11-05 22:31:54 +0900

    Add insert/remove of Multiset

--- libs/rb/src/lib.rs
+++ libs/rb/src/lib.rs
@@ -1,5 +1,11 @@
-#![warn(missing_docs)]
 //! Containers for storing data in a red-black tree.
 
 /// Balancing algorithm of red-black trees.
 mod balance;
+
+/// Multimap based on red-black trees.
+mod multimap;
+
+pub use multimap::Multimap;
+pub use multimap::MultimapOp;
+pub use multimap::Multiset;
