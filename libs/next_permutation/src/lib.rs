2897c41547e9c99c1902f9e00e4208c29bac2394
Nagata Kana <ngtkana@gmail.com>
2023-11-21 00:03:56 +0900

    next_permutation

--- libs/next_permutation/src/lib.rs
+++ libs/next_permutation/src/lib.rs
@@ -1,257 +1,226 @@
-//! スライスの next-permutation アルゴリズムと、それによるイテレータを提供します。
-//!
-//! # `next_permutation` 系
-//!
-//! 次の置換が存在する場合は、受け取った可変スライス `state` をそれに変化させ、`true` を返します。
-//! さもなくば、`state` には何も起こさず、`false` を返します。
-//!
-//! ## Examples
-//!
-//! ```
-//! # use next_permutation::next_permutation;
-//! let mut a = vec![1, 0, 2, 1];
-//! assert_eq!(
-//!     next_permutation(&mut a).map(|a| a.to_vec()),
-//!     Some(vec![1, 1, 0, 2])
-//! );
-//! assert_eq!(a.as_slice(), &[1, 1, 0, 2]);
-//! ```
-//!
-//! # `permutations` 系
-//!
-//! 受け取ったベクターの置換をすべて辞書順返すイテレータを返します。
-//! 引数 `state` は必ずしもソートされていなくてもよいです。
-//! （この関数の実装の最初に、自動的にソートされます。）
-//!
-//! ## Examples
-//!
-//! ```
-//! # use next_permutation::permutations;
-//! let a = vec![1, 0, 0];
-//! let perms = permutations(a).collect::<Vec<_>>();
-//! assert_eq!(perms, vec![vec![0, 0, 1], vec![0, 1, 0], vec![1, 0, 0]]);
-//! ```
-//!
-//! # [`iter_by_next`] について
-//!
-//! [`next_permutation`] から [`permutations`] を作る操作を一般化したものです
-//!
-//! ## Examples
-//!
-//! 直積全探索の例です。
-//!
-//! ```
-//! # use next_permutation::iter_by_next;
-//! let n = 3;
-//! let value_lim = 3;
-//!
-//! let result = iter_by_next(vec![0; n], |state: &mut [_]| {
-//!     // `state` に型注釈が必要です。
-//!     state.iter().rposition(|&x| x + 1 != value_lim).map(|i| {
-//!         state[i] += 1;
-//!         state[i + 1..].iter_mut().for_each(|x| *x = 0);
-//!         state
-//!     })
-//! })
-//! .collect::<Vec<_>>();
-//!
-//! let mut expected = Vec::new();
-//! for i in 0..value_lim {
-//!     for j in 0..value_lim {
-//!         for k in 0..value_lim {
-//!             expected.push(vec![i, j, k]);
-//!         }
-//!     }
-//! }
-//! assert_eq!(result, expected);
-//! ```
+//! A library to enumerate permutations, shuffles, and partitions.
 
-use std::borrow::BorrowMut;
-use std::cmp::Ordering;
-use std::iter::once;
-use std::iter::repeat;
-
-/// 遷移関数からイテレータを作ります。
-pub fn iter_by_next<T, U: ?Sized>(
-    init: T,
-    mut next: impl FnMut(&mut U) -> Option<&mut U>,
-) -> impl Iterator<Item = T>
-where
-    T: BorrowMut<U> + Clone,
-    U: ToOwned<Owned = T>,
-{
-    once(init.clone()).chain(repeat(()).scan(init, move |state, ()| {
-        next(state.borrow_mut()).map(|state| state.to_owned())
-    }))
+/// Returns the next permutation of `a` in lexicographic order.
+pub fn next_permutation<T: Ord>(a: &mut [T]) -> bool {
+    if a.is_empty() {
+        return false;
+    }
+    let Some(i) = (0..a.len() - 1).rfind(|&i| a[i] < a[i + 1]) else {
+        return false;
+    };
+    let j = a.iter().rposition(|x| x > &a[i]).unwrap();
+    a.swap(i, j);
+    a[i + 1..].reverse();
+    true
+}
+/// Calls `f` for each permutation of `a` in lexicographic order.
+pub fn for_each_permutation<T: Ord, F: FnMut(&[T])>(a: &mut [T], mut f: F) {
+    a.sort();
+    while {
+        f(a);
+        next_permutation(a)
+    } {}
+}
+/// Returns all permutations of `a` in lexicographic order.
+pub fn permutations<T: Ord + Clone>(a: Vec<T>) -> Vec<Vec<T>> {
+    let mut a = a;
+    let mut result = Vec::new();
+    for_each_permutation(&mut a, |a| result.push(a.to_vec()));
+    result
+}
+/// Returns the next $(K, N - K)$-shuffle of `a` in lexicographic order.
+pub fn next_shuffle<T: Ord>(a: &mut [T], k: usize) -> bool {
+    let n = a.len();
+    if n == k {
+        return false;
+    }
+    let (left, right) = a.split_at_mut(k);
+    let Some(mut i) = left.iter().rposition(|x| x < right.last().unwrap()) else {
+        return false;
+    };
+    let mut j = right.iter().position(|x| &left[i] < x).unwrap();
+    std::mem::swap(&mut left[i], &mut right[j]);
+    i += 1;
+    j += 1;
+    let swap_len = (k - i).min(n - k - j);
+    left[k - swap_len..].swap_with_slice(&mut right[j..j + swap_len]);
+    left[i..].rotate_left(k.saturating_sub(i + swap_len));
+    right[j..].rotate_right((n - k).saturating_sub(j + swap_len));
+    true
+}
+/// Calls `f` for each $(K, N - K)$-shuffle of `a` in lexicographic order.
+pub fn for_each_shuffle<T: Ord, F: FnMut(&[T])>(a: &mut [T], k: usize, mut f: F) {
+    a.sort();
+    while {
+        f(&a[..k]);
+        next_shuffle(a, k)
+    } {}
+}
+/// Returns all $(K, N - K)$-shuffles of `a` in lexicographic order.
+pub fn shuffles<T: Ord + Clone>(a: Vec<T>, k: usize) -> Vec<Vec<T>> {
+    let mut a = a;
+    let mut result = Vec::new();
+    for_each_shuffle(&mut a, k, |a| result.push(a.to_vec()));
+    result
+}
+/// Returns the next pairing (ascending $(2,2,\dots,2)$-shuffle) of `a` in lexicographic order.
+pub fn next_pairing(a: &mut [usize]) -> bool {
+    assert!(a.len() % 2 == 0);
+    assert!(a.len() <= 32);
+    let n = a.len();
+    let mut used = 0_u32;
+    for i in (0..n).rev() {
+        used |= 1 << a[i];
+        if i % 2 == 1 && a[i] + 1 < (used + 1).next_power_of_two().trailing_zeros() as usize {
+            a[i] = (used >> (a[i] + 1)).trailing_zeros() as usize + a[i] + 1;
+            used ^= 1 << a[i];
+            for i in i + 1..n {
+                a[i] = used.trailing_zeros() as usize;
+                used ^= 1 << a[i];
+            }
+            return true;
+        }
+    }
+    false
+}
+/// Calls `f` for each pairing (ascending $(2,2,\dots,2)$-shuffle) of `a` in lexicographic order.
+pub fn for_each_pairing<F: FnMut(&[usize])>(n: usize, mut f: F) {
+    let mut a = (0..n).collect::<Vec<_>>();
+    while {
+        f(&a);
+        next_pairing(&mut a)
+    } {}
+}
+/// Returns all pairings (ascending $(2,2,\dots,2)$-shuffles) of `a` in lexicographic order.
+pub fn pairings(n: usize) -> Vec<Vec<usize>> {
+    let mut result = Vec::new();
+    for_each_pairing(n, |a| result.push(a.to_vec()));
+    result
+}
+/// Returns the next partition of `a` in lexicographic order.
+pub fn next_partition(a: &mut Vec<usize>) -> bool {
+    let Some(mut sum) = a.pop() else { return false };
+    if a.is_empty() {
+        return false;
+    }
+    while let Some(x) = a.pop() {
+        sum += x;
+        if a.last().map_or(true, |&last| last > x) {
+            a.push(x + 1);
+            a.extend(std::iter::repeat(1).take(sum - x - 1));
+            break;
+        }
+    }
+    true
+}
+/// Calls `f` for each partition of `n` in lexicographic order.
+pub fn for_each_partition<F: FnMut(&[usize])>(n: usize, mut f: F) {
+    let mut a = vec![1; n];
+    while {
+        f(&a);
+        next_partition(&mut a)
+    } {}
+}
+/// Returns all partitions of `n` in lexicographic order.
+pub fn partitions(n: usize) -> Vec<Vec<usize>> {
+    let mut result = Vec::new();
+    for_each_partition(n, |a| result.push(a.to_vec()));
+    result
+}
+/// Returns the previous partition of `a` in lexicographic order.
+pub fn prev_partition(a: &mut Vec<usize>) -> bool {
+    let Some(i) = a.iter().rposition(|&x| x != 1) else { return false };
+    let max = a[i] - 1;
+    let mut sum = a.split_off(i).into_iter().sum::<usize>();
+    while sum >= max {
+        a.push(max);
+        sum -= max;
+    }
+    if sum > 0 {
+        a.push(sum);
+    }
+    true
+}
+/// Calls `f` for each partition of `n` in the reverse of the lexicographic order.
+pub fn for_each_partition_rev<F: FnMut(&[usize])>(n: usize, mut f: F) {
+    if n == 0 {
+        f(&[]);
+        return;
+    }
+    let mut a = vec![n];
+    while {
+        f(&a);
+        prev_partition(&mut a)
+    } {}
+}
+/// Returns all partitions of `n` in the reverse of the lexicographic order.
+pub fn partitions_rev(n: usize) -> Vec<Vec<usize>> {
+    let mut result = Vec::new();
+    for_each_partition_rev(n, |a| result.push(a.to_vec()));
+    result
 }
 
-/// 要素の [`Ord`] による比較により、受け取ったスライスを次の置換に遷移します。
-///
-/// # Returns
-///
-/// 次の置換が存在した場合、`true` を返します。
-///
-/// # Effects
-///
-/// 次の置換が存在した場合、次の置換に遷移します。
-/// さもなくば何も起こりません。
-pub fn next_permutation<T: Ord>(state: &mut [T]) -> Option<&mut [T]> {
-    next_permutation_by(state, Ord::cmp)
-}
-
-pub fn next_permutation_by_key<T, U, F>(state: &mut [T], mut f: F) -> Option<&mut [T]>
-where
-    F: FnMut(&T) -> U,
-    U: Ord,
-{
-    next_permutation_by(state, |x, y| f(x).cmp(&f(y)))
-}
-
-/// 引数の比較関数により比較して、受け取ったスライスを次の置換に遷移します。
-///
-/// # Returns
-///
-/// 次の置換が存在した場合、`true` を返します。
-///
-/// # Effects
-///
-/// 次の置換が存在した場合、次の置換に遷移します。
-/// さもなくば何も起こりません。
-pub fn next_permutation_by<T>(
-    state: &mut [T],
-    mut cmp: impl FnMut(&T, &T) -> Ordering,
-) -> Option<&mut [T]> {
-    (0..state.len() - 1)
-        .rfind(|&i| cmp(&state[i], &state[i + 1]) == Ordering::Less)
-        .map(|i| {
-            state[i + 1..].reverse();
-            let j = i
-                + 1
-                + state[i + 1..]
-                    .iter()
-                    .position(|x| cmp(&state[i], x) == (Ordering::Less))
-                    .unwrap();
-            state.swap(i, j);
-            state
-        })
-}
-
-/// 要素を射影して、受け取ったベクターの置換をすべて辞書順返すイテレータを返します。
-///
-/// 引数 `state` は必ずしもソートされていなくてもよいです。
-/// （この関数の実装の最初に、自動的にソートされます。）
-pub fn permutations<T>(mut state: Vec<T>) -> impl Iterator<Item = Vec<T>>
-where
-    T: Clone + Ord,
-{
-    state.sort();
-    iter_by_next(state, next_permutation)
-}
-
-/// 要素の [`Ord`] による比較により、
-/// 受け取ったベクターの置換をすべて辞書順返すイテレータを返します。
-///
-/// 引数 `state` は必ずしもソートされていなくてもよいです。
-/// （この関数の実装の最初に、自動的にソートされます。）
-pub fn permutations_by_key<T, U, F>(mut state: Vec<T>, f: F) -> impl Iterator<Item = Vec<T>>
-where
-    T: Clone,
-    U: Ord,
-    F: FnMut(&T) -> U + Copy,
-{
-    state.sort_by_key(f);
-    iter_by_next(state, move |state| next_permutation_by_key(state, f))
-}
-
-/// 引数の比較関数により比較して、受け取ったベクターの置換をすべて辞書順返すイテレータを返します。
-///
-/// 引数 `state` は必ずしもソートされていなくてもよいです。
-/// （この関数の実装の最初に、自動的にソートされます。）
-pub fn permutations_by<T, C>(mut state: Vec<T>, cmp: C) -> impl Iterator<Item = Vec<T>>
-where
-    T: Clone,
-    C: FnMut(&T, &T) -> Ordering + Copy,
-{
-    state.sort_by(cmp);
-    iter_by_next(state, move |state| next_permutation_by(state, cmp))
-}
-
-#[cfg(tests)]
+#[cfg(test)]
 mod tests {
     use super::*;
     use itertools::Itertools;
 
-    fn make_perms() -> Vec<Vec<usize>> {
-        vec![
-            vec![0, 1, 1, 2],
-            vec![0, 1, 2, 1],
-            vec![0, 2, 1, 1],
-            vec![1, 0, 1, 2],
-            vec![1, 0, 2, 1],
-            vec![1, 1, 0, 2],
-            vec![1, 1, 2, 0],
-            vec![1, 2, 0, 1],
-            vec![1, 2, 1, 0],
-            vec![2, 0, 1, 1],
-            vec![2, 1, 0, 1],
-            vec![2, 1, 1, 0],
-        ]
-    }
-
-    #[test]
-    fn test_next_permutation() {
-        let perms = make_perms();
-        for v in perms.windows(2) {
-            let mut from = v[0].clone();
-            let to = &v[1];
-            next_permutation(&mut from);
-            assert_eq!(&from, to);
-        }
-    }
-
-    #[test]
-    fn test_next_permutation_by_key() {
-        let perms = make_perms();
-        for v in perms.windows(2) {
-            let mut from = v[0].clone();
-            let to = &v[1];
-            next_permutation_by_key(&mut from, |x| x);
-            assert_eq!(&from, to);
-        }
-    }
-
-    #[test]
-    fn test_next_permutation_by() {
-        let perms = make_perms();
-        for v in perms.windows(2) {
-            let mut from = v[0].clone();
-            let to = &v[1];
-            next_permutation_by(&mut from, |x, y| x.cmp(&y));
-            assert_eq!(&from, to);
-        }
-    }
-
     #[test]
     fn test_permutations() {
-        let a = vec![1, 0, 1, 2];
-        let perms = permutations(a).collect_vec();
-        let expected = make_perms();
-        assert_eq!(expected, perms);
+        for n in 0..=5 {
+            let result = permutations((0..n).collect::<Vec<_>>());
+            assert!(result.iter().tuple_windows().all(|(a, b)| a < b));
+            assert_eq!(result.len(), (1..=n).product());
+        }
     }
 
     #[test]
-    fn test_permutations_by_key() {
-        let a = vec![1, 0, 1, 2];
-        let perms = permutations_by_key(a, |x| x).collect_vec();
-        let expected = make_perms();
-        assert_eq!(expected, perms);
+    fn test_shuffles() {
+        for n in 0..=5 {
+            for k in 0..=n {
+                let result = shuffles((0..n).collect::<Vec<_>>(), k);
+                for result in &result {
+                    assert!(result[..k].iter().tuple_windows().all(|(a, b)| a < b));
+                    assert!(result[k..].iter().tuple_windows().all(|(a, b)| a < b));
+                }
+                assert!(result.iter().tuple_windows().all(|(a, b)| a < b));
+                assert_eq!(
+                    result.len(),
+                    (1..=n).product::<usize>()
+                        / (1..=k).product::<usize>()
+                        / (1..=(n - k)).product::<usize>()
+                );
+            }
+        }
     }
 
     #[test]
-    fn test_permutations_by() {
-        let a = vec![1, 0, 1, 2];
-        let perms = permutations_by(a, |x, y| x.cmp(&y)).collect_vec();
-        let expected = make_perms();
-        assert_eq!(expected, perms);
+    fn test_pairings() {
+        for n in (0..=10).step_by(2) {
+            let result = pairings(n);
+            assert!(result.iter().tuple_windows().all(|(a, b)| a < b));
+            for result in &result {
+                assert!(result.chunks_exact(2).all(|x| x[0] < x[1]));
+                assert!(result.iter().step_by(2).tuple_windows().all(|(a, b)| a < b));
+            }
+            assert_eq!(result.len(), (1..=n).step_by(2).product::<usize>());
+        }
+    }
+
+    #[test]
+    fn test_partitions() {
+        let n = 10;
+        let mut p = vec![1; n];
+        for step in 2..n {
+            for i in 0..n - step {
+                p[i + step] += p[i];
+            }
+        }
+        for n in 0..10 {
+            let result = partitions(n);
+            assert!(result.iter().tuple_windows().all(|(a, b)| a < b));
+            assert_eq!(result.len(), p[n]);
+            assert_eq!(result, partitions_rev(n).into_iter().rev().collect_vec());
+        }
     }
 }
