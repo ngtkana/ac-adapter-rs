5278e5fa031937e3a92d59981f55f1206edc6365
Nagata Kana <ngtkana@gmail.com>
2024-11-12 19:10:49 +0900

    migrte slicemore into riff

--- libs/riff/src/lib.rs
+++ libs/riff/src/lib.rs
@@ -1,5 +1,9 @@
 //! Future and otherworldly Rust features.
 
+use std::cmp::Ordering;
+use std::cmp::Ordering::Equal;
+use std::cmp::Ordering::Greater;
+use std::cmp::Ordering::Less;
 use std::collections::BinaryHeap;
 
 /// Removes and returns the "top" element in a vector if the predicate
@@ -56,6 +60,42 @@ impl<T: Ord> PopIf for BinaryHeap<T> {
     }
 }
 
+/// Method versions of functions.
+pub trait BinarySearch<T> {
+    fn partition_point<F: FnMut(&T) -> bool>(&self, pred: F) -> usize;
+    fn lower_bound_by<F: FnMut(&T) -> Ordering>(&self, mut f: F) -> usize {
+        self.partition_point(|x| f(x) < Equal)
+    }
+    fn upper_bound_by<F: FnMut(&T) -> Ordering>(&self, mut f: F) -> usize {
+        self.partition_point(|x| f(x) <= Equal)
+    }
+    fn lower_bound_by_key<B: Ord, F: FnMut(&T) -> B>(&self, b: &B, mut f: F) -> usize {
+        self.lower_bound_by(|x| f(x).cmp(b))
+    }
+    fn upper_bound_by_key<B: Ord, F: FnMut(&T) -> B>(&self, b: &B, mut f: F) -> usize {
+        self.upper_bound_by(|x| f(x).cmp(b))
+    }
+    fn lower_bound(&self, x: &T) -> usize
+    where
+        T: Ord,
+    {
+        self.lower_bound_by(|p| p.cmp(x))
+    }
+    fn upper_bound(&self, x: &T) -> usize
+    where
+        T: Ord,
+    {
+        self.upper_bound_by(|p| p.cmp(x))
+    }
+}
+
+impl<T> BinarySearch<T> for [T] {
+    fn partition_point<F: FnMut(&T) -> bool>(&self, mut pred: F) -> usize {
+        self.binary_search_by(|x| if pred(x) { Less } else { Greater })
+            .unwrap_err()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -97,5 +137,20 @@ mod tests {
         assert_eq!(run(vec![0, 3]), (vec![0, 3], None));
         assert_eq!(run(vec![1, 2]), (vec![1], Some(2)));
         assert_eq!(run(vec![1, 3]), (vec![1, 3], None));
+    }
+
+    #[test]
+    fn test_vec_binary_search() {
+        let vec = vec![10, 12];
+        assert_eq!(vec.lower_bound(&9), 0);
+        assert_eq!(vec.lower_bound(&10), 0);
+        assert_eq!(vec.lower_bound(&11), 1);
+        assert_eq!(vec.lower_bound(&12), 1);
+        assert_eq!(vec.lower_bound(&13), 2);
+        assert_eq!(vec.upper_bound(&9), 0);
+        assert_eq!(vec.upper_bound(&10), 1);
+        assert_eq!(vec.upper_bound(&11), 1);
+        assert_eq!(vec.upper_bound(&12), 2);
+        assert_eq!(vec.upper_bound(&13), 2);
     }
 }
