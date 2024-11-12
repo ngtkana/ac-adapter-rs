d3e46c4fc8654b8a046eaab7b4389c63f2513289
Nagata Kana <ngtkana@gmail.com>
2024-11-12 19:42:10 +0900

    refactor numeric_traits out as a new module

--- libs/riff/src/lib.rs
+++ libs/riff/src/lib.rs
@@ -3,10 +3,12 @@
 mod binary_search;
 mod bitmask_iterators;
 mod change_min_max;
+mod numeric_traits;
 mod pop_if;
 
 pub use binary_search::BinarySearch;
 pub use bitmask_iterators::bitmask_combinations;
 pub use bitmask_iterators::bitmask_subsets;
 pub use change_min_max::ChangeMinMax;
+pub use numeric_traits::Unsigned;
 pub use pop_if::PopIf;
