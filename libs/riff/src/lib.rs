3e7614376addaf423b991138814e3383ea4d4272
Nagata Kana <ngtkana@gmail.com>
2025-05-13 02:04:22 +0900

    add lazy_lock

--- libs/riff/src/lib.rs
+++ libs/riff/src/lib.rs
@@ -3,6 +3,7 @@
 mod bitmask_iterators;
 mod bitmask_operations;
 mod change_min_max;
+mod lazy_lock;
 mod numeric_traits;
 mod pop_if;
 mod slice_accum;
@@ -13,6 +14,7 @@ pub use bitmask_iterators::bitmask_combinations;
 pub use bitmask_iterators::bitmask_subsets;
 pub use bitmask_operations::i2powm1;
 pub use change_min_max::ChangeMinMax;
+pub use lazy_lock::LazyLock;
 pub use numeric_traits::Unsigned;
 pub use pop_if::PopIf;
 pub use slice_accum::SliceAccum;
