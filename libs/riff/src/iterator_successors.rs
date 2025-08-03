c8d99a75c24788b90f2be5d9afbf791248c68bb9
Nagata Kana <ngtkana@gmail.com>
2025-08-03 17:04:44 +0900

    imporove docs

--- libs/riff/src/iterator_successors.rs
+++ libs/riff/src/iterator_successors.rs
@@ -1,24 +1,26 @@
-/// An extension trait for iterators that provides a method to generate successors
-///
-/// # Specification
-///
-/// Returns an iterator that generates the sequence $b_0, b_1, \ldots$ where:
-///
-/// * self: $a_0, a_1, \ldots, a_{n - 1}$
-/// * first: $b_0$
-/// * succ: $f(a_i, b_i) = b_{i + 1}$
-///
-///
-/// # Example
-///
-/// ```
-/// use riff::IteratorSuccessors;
-/// let a = (1..=3)
-///     .successors(Some([0]), |x, &[y]| Some([x + y]))
-///     .collect::<Vec<_>>();
-/// assert_eq!(a.as_slice(), &[[0], [1], [3], [6]]);
-/// ```
+/// `successors` for `Iterator`
 pub trait IteratorSuccessors: Iterator {
+    /// Creates an iterator like [`std::iter::successors`], but unlike it, uses an iterator to
+    /// calculate the next element.
+    ///
+    /// # Specification
+    ///
+    /// Returns an iterator that generates the sequence $b_0, b_1, \ldots$ where:
+    ///
+    /// * `self`: $a_0, a_1, \ldots, a_{n - 1}$
+    /// * `first`: $b_0$
+    /// * `succ`: $f(a_i, b_i) = b_{i + 1}$
+    ///
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use riff::IteratorSuccessors;
+    /// let a = (1..=3)
+    ///     .successors(Some([0]), |x, &[y]| Some([x + y]))
+    ///     .collect::<Vec<_>>();
+    /// assert_eq!(a.as_slice(), &[[0], [1], [3], [6]]);
+    /// ```
     fn successors<T, F>(self, first: Option<T>, succ: F) -> IterSuccessors<Self, T, F>
     where
         Self: Sized,
