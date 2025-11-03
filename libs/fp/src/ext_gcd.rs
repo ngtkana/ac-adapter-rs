422058fefeceada9e7211f3b773da7ab36b92e33
Nagata Kana <ngtkana@gmail.com>
2025-11-04 04:10:40 +0900

    refactor fp

--- libs/fp/src/ext_gcd.rs
+++ libs/fp/src/ext_gcd.rs
@@ -1,20 +1,20 @@
 /// Return the multiplicative inverse of `x` modulo `P`.
 /// # Requirements
-/// - $P$ is odd and prime ($P \ge 2^{31}$)
+/// - $P$ is odd and prime ($P ≥ 2³¹$)
 /// - $x < P$
 /// # Returns
-/// $x^{-1} \mod P$
+/// $x⁻¹ \mod P$
 /// # Postconditions
-/// - $0 \le \text{result} < P$
+/// - $0 ≤ \text{result} < P$
 pub(crate) fn mod_inv<const P: u64>(x: u64) -> u64 {
     debug_assert!(P % 2 == 1);
     debug_assert!(P < 1 << 31);
     debug_assert!(x < P);
     mod_inv_signed(x as i64, P as i64) as u64
 }
-/// Returns $a^{-1} \mod m$.
+/// Returns $a⁻¹ \mod m$.
 /// # Requirements
-/// - $m \gt 0, 0 \lt a \lt m$
+/// - $m > 0, 0 < a < m$
 fn mod_inv_signed(a: i64, m: i64) -> i64 {
     debug_assert!(a > 0);
     debug_assert!(m > 0);
