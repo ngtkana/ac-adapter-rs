5414789cb5349b1c65f6aa414af60e665feb77ed
Kana Nagata <ngtkana@gmail.com>
2025-11-06 20:30:15 +0900

    reversible-list reverse

--- libs/reversible-list/src/reversible_list.rs
+++ libs/reversible-list/src/reversible_list.rs
@@ -44,6 +44,17 @@ impl<T: Debug> ReversibleList<T> {
         self.root = merge2(l, r);
         c.data
     }
+    #[lg_recur]
+    pub fn reverse(&mut self, start: usize, end: usize) {
+        assert!(start <= end && end <= self.len());
+        let (lc, r) = split2_by_index(self.root.take(), end);
+        let (l, mut c) = split2_by_index(lc, start);
+        if let Some(c) = c.as_deref_mut() {
+            c.rev ^= true;
+        }
+        let lc = merge2(l, c);
+        self.root = merge2(lc, r);
+    }
 }
 
 struct Marker<T> {
@@ -60,7 +71,7 @@ impl<T: std::fmt::Debug> NodeMarker for Marker<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::node::debug::{collect, display, validate};
+    use crate::reversible_list_core::debug::{collect, display, validate};
     use rand::Rng;
     use rand::{SeedableRng, rngs::StdRng};
 
@@ -68,6 +79,7 @@ mod tests {
     enum Query {
         Insert { index: usize, value: i32 },
         Remove { index: usize },
+        Reverse { start: usize, end: usize },
     }
 
     #[test]
@@ -82,13 +94,26 @@ mod tests {
             let mut vec = vec![];
             #[allow(clippy::explicit_counter_loop)]
             for qid in 1..=q {
-                let query = if rng.gen_ratio(n as u32, len_max) {
-                    let index = rng.gen_range(0..n);
-                    Query::Remove { index }
-                } else {
-                    let index = rng.gen_range(0..=n);
-                    let value = rng.gen_range(0..value_lim);
-                    Query::Insert { index, value }
+                let query = match rng.gen_range(0..3) {
+                    0 => {
+                        let mut start = rng.gen_range(0..=n + 1);
+                        let mut end = rng.gen_range(0..=n);
+                        if start > end {
+                            (start, end) = (end, start - 1);
+                        }
+                        Query::Reverse { start, end }
+                    }
+                    1..=2 => {
+                        if rng.gen_ratio(n as u32, len_max) {
+                            let index = rng.gen_range(0..n);
+                            Query::Remove { index }
+                        } else {
+                            let index = rng.gen_range(0..=n);
+                            let value = rng.gen_range(0..value_lim);
+                            Query::Insert { index, value }
+                        }
+                    }
+                    _ => unreachable!(),
                 };
                 eprintln!("Query #{tid}.{qid}: {query:?}");
                 match query {
@@ -102,6 +127,10 @@ mod tests {
                         let expected = vec.remove(index);
                         assert_eq!(result, expected);
                         n -= 1;
+                    }
+                    Query::Reverse { start, end } => {
+                        rlist.reverse(start, end);
+                        vec[start..end].reverse();
                     }
                 }
                 eprintln!("{}", display(rlist.root.as_deref()));
