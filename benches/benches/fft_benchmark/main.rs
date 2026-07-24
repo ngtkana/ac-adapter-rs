f490ed42df2428cdeb8f22a40231a04ec7113040
Kana Nagata <ngtkana@gmail.com>
2026-07-24 15:05:02 +0900

    refactor bench

--- benches/benches/fft_benchmark/main.rs
+++ benches/benches/fft_benchmark/main.rs
@@ -1,12 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use fp::fp;
+use fp::fpu;
 use fp_fft::fft;
 
 const P: u64 = 998_244_353;
 
 fn fft_bench_2_23(c: &mut Criterion) {
-    c.bench_function("fft_1<<23", |b| {
-        let data: Vec<_> = (0..1 << 23).map(|i| fp::<P>(i as u64)).collect();
+    c.bench_function("fft_1^23", |b| {
+        let data: Vec<_> = (0..1 << 23).map(fpu::<P>).collect();
 
         b.iter(|| {
             let mut work = black_box(data.clone());
