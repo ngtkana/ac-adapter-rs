7be13d04a7bbadf17c74d082344e55013146cfac
ngtkana <ngtkana@users.noreply.github.com>
2026-08-27 13:21:32 +0000

    deploy: aa096c051ca17e9370f645145be534005e709f70

--- rustdoc/graph/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["kosaraju"],"struct":["Csr","Iter","LastMut"]};
--- rustdoc/trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["graph",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"graph/struct.Iter.html\" title=\"struct graph::Iter\">Iter</a>&lt;'_, T&gt;"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[342]}
