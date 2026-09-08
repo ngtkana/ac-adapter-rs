7a1e4fd242a63bf1f99678d681112ad9f07180ba
ngtkana <ngtkana@users.noreply.github.com>
2026-09-08 19:48:07 +0000

    deploy: 26e4a18fce0ff807a71870314e23c5f36a5fa841

--- rustdoc/search.desc/io_writer/io_writer-desc-0-.js
+++ rustdoc/search.desc/io_writer/io_writer-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("io_writer", 0, "…\nFlush を遅延した版の <code>print</code> です。\nFlush を遅延した版の <code>println</code> です。")
+searchState.loadedDescShard("io_writer", 0, "呼び出しのたびに flush しない出力マクロ。\nバッファリングして遅延 flush する <code>print!</code> …\nバッファリングして遅延 flush する <code>println!</code> …")
