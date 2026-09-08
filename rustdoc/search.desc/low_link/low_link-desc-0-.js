9eb528d1bc06dca634c9e7e42d5ff908c93a260b
ngtkana <ngtkana@users.noreply.github.com>
2026-09-08 19:55:18 +0000

    deploy: cd91a0135c17665e8dfdf73ee55ca5566c4b4c8e

--- rustdoc/search.desc/low_link/low_link-desc-0-.js
+++ rustdoc/search.desc/low_link/low_link-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("low_link", 0, "Low-link を計算します。\nLow-link を計算する構造体です。\n…\n…\nビルドします。\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\n頂点 <code>x</code> が関節点なら <code>true</code> を返します。\n頂点 <code>x</code>, <code>y</code> …\n未ビルドの空グラフを構築します。\n…")
+searchState.loadedDescShard("low_link", 0, "無向グラフの low-link …\n無向グラフの low-link を管理する構造体。\n未ビルドのグラフに無向辺 ${i, j}$ …\n…\n…\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\n頂点 <code>x</code> …\n頂点 <code>x</code>, <code>y</code> …\n頂点数 …\n…")
